c8ef1c902ca627e3148763979542fbd82c0deffe
G00FY <G00FBALL@users.noreply.github.com>
2017-10-11 20:30:07 -0400

    corrected chance

--- Changes/index.php
+++ Changes/index.php
@@ -107,7 +107,7 @@
     <p><img src="http://musicfamily.org/realm/Factions/picks/VeteranFigurineArtifact.png" alt="Veteran Figureine" align="middle"><b> Veteran Figureine</b></p>
     <p><b>Description</b>: The warrior of a thousand battles, ultimate champion of the Realms.
     <p><b>Requirement</b>: R90+
-    <p><b>Chance</b>: 5%
+    <p><b>Chance</b>: (1,000,000 seconds This Game)%
     <p><b>Effect</b>: Passive effect: allows Mercenaries to benefit from all challenges.
 <br/>
 <br/>
