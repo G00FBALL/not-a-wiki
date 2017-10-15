1fbf841369e5c30880ae92dd5fb3481f622352a7
G00FY <G00FBALL@users.noreply.github.com>
2017-10-15 11:47:31 -0400

    Update index.php

--- R100Plus/index.php
+++ R100Plus/index.php
@@ -1,1 +1,195 @@
-Under Construction
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN"
+        "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml" lang="en" xml:lang="en">
+<meta http-equiv="Content-Type" content="text/html; charset=windows-1252">
+<?php include "../scripts/header.html"; ?>
+<br/>
+<h6><img src="http://musicfamily.org/realm/Factions/picks/TopPageResearch.png" alt="Spellcraft" align="middle"></h6>
+<br/>
+<br/>
+    <p><b>NOTE</b>: Updated October 14th 2017
+    <p><b>NOTE</b>: We will be adding builds as we get them.<br/>
+<br/>
+<div class="shlisting">
+    <p><b>Production Builds</b></p>
+    <div class="category">
+        <div class="shelementwhole">
+            <p onclick="shohid($(this));"><b> <a href="#" onclick="return false;">Demonline Titans 0-1e33 gems <font color="DarkGoldenRod"> Neutral</font></a></b></p>
+            <div class="autohide">
+                <p><b>Author</b>: Siku
+                <p><b>Faction</b>: Titan/Dragon
+                <p><b>Bloodline</b>: Demon
+                <p><b>Range</b>: 0 gems - 1e33 (1 Dc) gems
+                <p><b>Research Template</b>
+                <p>S105,S150,S215,S270,S330,S500,
+                <p>C135,C150,C250,C330,C340,C400,C460,
+                <p>D55,D150,D200,D245,D250,D320,
+                <p>E135,E145,E275,E330,E400,E460,
+                <p>A105,A120,A150,A250,A305,A400,
+                <p>W120,W150,W180,W250,W260,W330,W400
+                <p><b>Notes</b>: Requires excavations to set up for demon lineage.
+         </div>
+    </div>
+        <div class="shelementwhole">
+            <p onclick="shohid($(this));"><b> <a href="#" onclick="return false;">Elfline Faceless1 e30-1e90 gems<font color="DarkGoldenRod"> Neutral</font></a></b></p>
+            <div class="autohide">
+                <p><b>Author</b>: Necrodoom
+                <p><b>Faction</b>: Faceless/Dragon
+                <p><b>Bloodline</b>: Elf
+                <p><b>Range</b>: 1e30 (1 No) gems - 1e90 (1 NoVg)(?) gems
+                <p><b>Research Template</b>
+                <p>S215,S250,S270,S305,S330,S1450,S1500,
+                <p>C250,C330,C340,C400,C1300,C1325,
+                <p>D55,D245,D260,D320,D1125,D1275,D1375,
+                <p>E135,E145,E350,E460,E1225,E1425,
+                <p>A105,A120,A150,A250,A305,A400,
+                <p>W150,W180,W275,W400,W1375,W1400
+                <p><b>Notes</b>: Requires excavations to set up for elf lineage.
+         </div>
+    </div>
+        <div class="shelementwhole">
+            <p onclick="shohid($(this));"><b> <a href="#" onclick="return false;">Thor 1e69-1e129 gems<font color="DarkGoldenRod"> Neutral</font></a></b></p>
+            <div class="autohide">
+                <p><b>Author</b>: Lapare
+                <p><b>Faction</b>: Neutral Mercenary
+                <p><b>Bloodline</b>: Titan
+                <p><b>A2950</b>: Dragon
+                <p><b>Range</b>: 1e69 (1 DVg) gems - 1e129 (1 Dqag) gems
+                <p><b>Requiments</b>: Veteran Figurine
+                <p><b>Merc Template</b>
+                <p>EL3,EL7,TT4,TT6,DD6,FC2,FC7,DN2,DN8,DG3,DG4,DG9,
+                <p>SP:Dragon's Breath,SP:God's Hand
+                <p><b>Research Template</b>
+                <p>S215,S270,S305,S330,S2875,S3200,
+                <p>C150,C340,C400,C590,C1300,C3000,
+                <p>D25,D55,D245,D260,D320,D1275,
+                <p>E135,E260,E350,E1225,E1425,E3250,
+                <p>A120,A590,A1200,A1500,A2950,A3400,
+                <p>W180,W260,W400,W1275,W3050,W3150
+                <p><b>Notes</b>: 
+                <p>Replace C150 with C1500 if higher.
+                <p>Replace E350 with E480 if higher (takes about 1 hour of buildup in R for E480 to be higher).
+                <p>Replace C400 with C1500 if high enough (100%+)
+                <p>May need excavations for spell tiers to set up at low gems
+         </div>
+    </div>
+        <div class="shelementwhole">
+            <p onclick="shohid($(this));"><b> <a href="#" onclick="return false;">Ragnarok 1e90-1.778e138 gems<font color="DarkGoldenRod"> Neutral</font></a></b></p>
+            <div class="autohide">
+                <p><b>Author</b>: Lapare
+                <p><b>Faction</b>: Neutral Mercenary
+                <p><b>Bloodline</b>: Titan
+                <p><b>A2950</b>: Dragon
+                <p><b>Requiments</b>: Veteran Figurine, 51.8B mana generated this R (for GB11)
+                <p><b>Merc Template</b>
+                <p>EL3,EL7,TT4,TT6,DD6,FC2,DD7,DN2,DN8,DG3,DG4,DG9,
+                <p>SP:Dragon's Breath,SP:Grand Balance
+                <p><b>Research Template</b>
+                <p>S215,S270,S305,S330,S2875,S3200,
+                <p>C300,C340,C400,C590,C1300,C3000,
+                <p>D25,D55,D245,D260,D320,D1275,
+                <p>E135,E260,E350,E1225,E1425,E3250,
+                <p>A120,A590,A1200,A1500,A2950,A3400,
+                <p>W180,W260,W400,W1275,W3050,W3150
+                <p><b>Notes</b>: 
+                <p>Replace E350 with E480 if higher (takes about 1 hour of buildup in R for E480 to be higher).
+                <p>Replace C400 with C1500 if high enough (100%+)
+         </div>
+    </div>
+            </div>
+                    <br/>
+    <p><b>Special Builds</b></p>
+    <div class="category">
+        <div class="shelementwhole">
+            <p onclick="shohid($(this));"><b> <a href="#" onclick="return false;">Balance Elemental (Mana Builder for GB11)<font color="DarkGoldenRod"> Neutral</font></a></b></p>
+            <div class="autohide">
+                    <p><b>Author</b>: Lapare
+                    <p><b>Requiments</b>: Veteran Figurine
+                    <p><b>Range</b>: 1e90 (1 NoVg gems)(?)
+                    <p><b>Faction</b>: Neutral Mercenary
+                    <p><b>Bloodline</b>: Dragon
+                    <p><b>A2950</b>: Elf
+                    <p><b>Merc Template</b>
+                    <p>EL7,AN3,AN6,TT3,DD4,DD6,FC2,FC7,DN2,DN8,DG6,DG9,
+                    <p>SP:God's Hand,SP:Fairy Chanting
+                    <p><b>Research Template</b>
+                    <p>S215,S305,S400,S1450,S1500,S3200,
+                    <p>C250,C400,C590,C1300,C3000,C3100,
+                    <p>D25,D200,D245,D1275,D1375,D2775,
+                    <p>E135,E260,E320,E1225,E1425,E3250,
+                    <p>A120,A305,A1200,A1500,A2950,A3400,
+                    <p>W275,W400,W560,W1375,W1400,W3050
+        </div>
+    </div>
+        <div class="shelementwhole">
+            <p onclick="shohid($(this));"><b> <a href="#" onclick="return false;">Magic of Rivendell (Elf lineage level pusher)<font color="DarkGoldenRod"> Neutral</font></a></b></p>
+            <div class="autohide">
+                    <p><b>Author</b>: Lapare
+                    <p><b>Requiments</b>: Veteran Figurine
+                    <p><b>Range</b>: 1e90 (1 NoVg) gems(?)
+                    <p><b>Faction</b>: Neutral Mercenary
+                    <p><b>Bloodline</b>: Elf
+                    <p><b>A2950</b>: Dragon
+                    <p><b>Merc Template</b>
+                    <p>EL1,EL7,AN5,AN6,GB7,DD3,DD6,FC2,FC7,DN2,DG6,DG9,
+                    <p>SP:Dragon's Breath,SP:Lightning Strike
+                    <p><b>Research Template</b>
+                    <p>S215,S305,S330,S400,S1450,S1500,
+                    <p>C250,C340,C400,C590,C1300,C3000,
+                    <p>D25,D200,D245,D1275,D1375,D2775,
+                    <p>E135,E260,E320,E1225,E1425,E3250,
+                    <p>A120,A305,A1200,A1500,A2950,A3400,
+                    <p>W260,W275,W400,W1375,W3050,W3150
+                    <p><b>Notes</b>: Gets level 32 in under a hour without any buff to W275 or S400
+        </div>
+    </div>
+        <div class="shelementwhole">
+            <p onclick="shohid($(this));"><b> <a href="#" onclick="return false;">Elven Burial (W275 Buff Build)<font color="DarkGoldenRod"> Neutral</font></a></b></p>
+            <div class="autohide">
+                    <p><b>Author</b>: Lapare
+                    <p><b>Requiments</b>: Veteran Figurine
+                    <p><b>Range</b>: 1e90 (1 NoVg) gems(?)
+                    <p><b>Faction</b>: Neutral Mercenary
+                    <p><b>Bloodline</b>: Undead
+                    <p><b>A2950</b>: Elf
+                    <p><b>Merc Template</b>
+                    <p>FR9,EL3,EL7,TT6,DD4,FC2,FC5,FC7,DN2,DG3,DG6,DG9,
+                    <p>SP:Dragon's Breath,SP:God's Hand
+                    <p><b>Research Template</b>
+                    <p>S215,S305,S330,S400,S1450,S1500,
+                    <p>C340,C590,C1300,C1325,C3000,C3100,
+                    <p>D25,D245,D250,D1275,D1375,D2775,
+                    <p>E135,E590,E1225,E1425,E3250,E3300,
+                    <p>A120,A590,A1200,A1500,A2950,A3400,
+                    <p>W275,W400,W560,W1400,W3050,W3150
+        </div>
+    </div>
+        <div class="shelementwhole">
+            <p onclick="shohid($(this));"><b> <a href="#" onclick="return false;">400 Shades of Spells (S400 buff build)<font color="DarkRed"> Evil</font></a></b></p>
+            <div class="autohide">
+                    <p><b>Author</b>: Lapare
+                    <p><b>Requiments</b>: Veteran Figurine
+                    <p><b>Range</b>: 1e90 (1 NoVg) gems(?)
+                    <p><b>Faction</b>: Evil Mercenary
+                    <p><b>Bloodline</b>: Undead
+                    <p><b>A2950</b>: Elf
+                    <p><b>M13</b>: UD7
+                    <p><b>Merc Template</b>
+                    <p>EL3,EL7,AN2,AN5,TT6,DD5,DD7,DN2,DN8,DG3,DG4,DG9,
+                    <p>SP:Night Time,SP:Grand Balance
+                    <p><b>Research Template</b>
+                    <p>S50,S215,S305,S400,S1450,S1500,
+                    <p>C250,C340,C590,C1300,C1500,C3000,
+                    <p>D25,D200,D245,D1275,D1375,D2775,
+                    <p>E10,E135,E145,E350,E1225,E3250,
+                    <p>A120,A410,A590,A1500,A2950,A3400,
+                    <p>W180,W205,W400,W525,W3050,W3150
+                    <p><b>Notes</b>: Estimated run time 10-14 days to unlock Spell Master trophy.
+        </div>
+    </div>
+</div>
+</div>
+<?php include "../scripts/footer.html"; ?>
+
+
+
