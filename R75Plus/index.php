8580cbb44347f2436660aefcae7a82d4448652f2
G00FY <G00FBALL@users.noreply.github.com>
2017-10-15 18:16:31 -0400

    Update index.php

--- R75Plus/index.php
+++ R75Plus/index.php
@@ -242,7 +242,8 @@
                     <p>Build comes from this <a target="_blank" href="http://www.kongregate.com/forums/8945-realm-grinder/topics/922600-3-0-build-megathread-under-construction-please-post-builds-to-add?page=1"><b>forum</b></a> post</p>
             </div>
         </div>
-    <br/>
+         </div>
+   <br/>
             
               <p><b>Lineage Leveling</b></p>
     <div class="category">
