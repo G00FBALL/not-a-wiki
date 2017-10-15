8559b28cb45a71a3d4b868b36c6ba63e4c5da168
G00FY <G00FBALL@users.noreply.github.com>
2017-10-15 18:40:19 -0400

    Update index.php

--- R75Plus/index.php
+++ R75Plus/index.php
@@ -298,7 +298,8 @@
                 <p>W180,W275,W400,W1375,W3050,W3150
             </div>
         </div>
-       <div class="shelementwhole">
+          </div>
+     <div class="shelementwhole">
             <p onclick="shohid($(this));"><b> <a href="#" onclick="return false;">A Diverse Lineage Level 25 R82+<font color="DarkGoldenRod"> Neutral</font></a></b></p>
             <div class="autohide">
                 <p><b>Author</b>: Lapare
