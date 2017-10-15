e7d1e9cdb0fff4bd51077583a5c8951ea0d2e9b4
G00FY <G00FBALL@users.noreply.github.com>
2017-10-15 09:46:25 -0400

    Update index.php

--- R90Plus/index.php
+++ R90Plus/index.php
@@ -30,6 +30,26 @@
                 <p><b>Notes</b>: Requires excavations to set up for demon lineage.
          </div>
     </div>
+<div class="shlisting">
+    <p><b>Production Builds</b></p>
+    <div class="category">
+        <div class="shelementwhole">
+            <p onclick="shohid($(this));"><b> <a href="#" onclick="return false;">Demonline Titans<font color="DarkGoldenRod"> Neutral</font></a></b></p>
+            <div class="autohide">
+                <p><b>Author</b>: Siku
+                <p><b>Faction</b>: Titan/Dragon
+                <p><b>Bloodline</b>: Demon
+                <p><b>Range</b>: 0 gems - 1e33 (1 Dc) gems
+                <p><b>Research Template</b>
+                <p>S105,S150,S215,S270,S330,S500,
+                <p>C135,C150,C250,C330,C340,C400,C460,
+                <p>D55,D150,D200,D245,D250,D320,
+                <p>E135,E145,E275,E330,E400,E460,
+                <p>A105,A120,A150,A250,A305,A400,
+                <p>W120,W150,W180,W250,W260,W330,W400
+                <p><b>Notes</b>: Requires excavations to set up for demon lineage.
+         </div>
+    </div>
 </div>
                 </div>
 <?php include "../scripts/footer.html"; ?>
