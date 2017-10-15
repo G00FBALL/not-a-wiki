c55aba5984380f33e8a94d20d49bf064f5c7047e
G00FY <G00FBALL@users.noreply.github.com>
2017-10-15 15:24:11 -0400

    Update index.php

--- R40-R46/index.php
+++ R40-R46/index.php
@@ -96,7 +96,7 @@
             </div>
         </div>
         <div class="shelementwhole">
-            <p onclick="shohid($(this));"><b><a href="#" onclick="return false;"> Dragon Stage 4 <font color="Blue">Good</font></a></b></p>
+            <p onclick="shohid($(this));"><b><a href="#" onclick="return false;"> Dragon Stage 4 <font color="DarkGoldenRod">Neutral</font></a></b></p>
             <div class="autohide">
                 <p><b>Author</b>: Artcrow
                 <p><b>Faction</b>: Titan
