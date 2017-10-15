a10837a5293245a14cca76792dbbd305009da913
G00FY <G00FBALL@users.noreply.github.com>
2017-10-15 08:52:09 -0400

    fixed error, updated builds

--- R75Plus/index.php
+++ R75Plus/index.php
@@ -19,13 +19,35 @@
             <p onclick="shohid($(this));"><b> <a href="#" onclick="return false;">Demonline Titans<font color="DarkGoldenRod"> Neutral</font></a></b></p>
             <div class="autohide">
                 <p><b>Author</b>: Siku
-                <p><b>Note</b>: Using Dragless: Get to 1e90+  (1 NoVg) gems, Excavate to 8000 for the Obsidian Shard.
-                <p><b>Faction</b>: Neutral Mercenary
-                <p><b>Bloodline</b>: Goblin
-                <p><b>Merc Template</b>
-                <p>FR9,EL3,EL7,AN5,GB4,GB7,TT6,FC7,DN2,DW2,DG3,DG4,
-                <p>SP:Fairy Chanting,SP:God's Hand
-                <p>Build comes from this <a target="_blank" href="https://docs.google.com/document/d/1dihhpSBa14_qsmDC9qZlhl5dWIgE9KQlobCqPtSqbsI/edit"><b>Google Docs</b></a> post</p>
+                <p><b>Faction</b>: Titan/Dragon
+                <p><b>Bloodline</b>: Demon
+                <p><b>Range</b>: 0 gems - 1e33 (1 Dc) gems
+                <p><b>Research Template</b>
+                <p>S105,S150,S215,S270,S330,S500,
+                <p>C135,C150,C250,C330,C340,C400,C460,
+                <p>D55,D150,D200,D245,D250,D320,
+                <p>E135,E145,E275,E330,E400,E460,
+                <p>A105,A120,A150,A250,A305,A400,
+                <p>W120,W150,W180,W250,W260,W330,W400
+                <p><b>Note</b>: Requires excavations to set up for demon lineage.
+                </div>
+        </div>
+        </div>
+    <div class="category">
+        <div class="shelementwhole">
+            <p onclick="shohid($(this));"><b> <a href="#" onclick="return false;">Undeadline Faceless<font color="DarkGoldenRod"> Neutral</font></a></b></p>
+            <div class="autohide">
+                <p><b>Author</b>: TemperGrace
+                <p><b>Faction</b>: Faceless/Dragon
+                <p><b>Bloodline</b>: Undead
+                <p><b>Range</b>: 1e30 (1 No) gems - 1e90(1e92?) (1 NoVg) gems
+                <p><b>Research Template</b>
+                <p>S135,S215,S270,S305,S330,S1450,S1500,
+                <p>C150,C250,C340,C400,C1300,C1325,
+                <p>D55,D245,D260,D320,D1125,D1275,D1375,
+                <p>E135,E145,E320,E350,E1225,E1425,
+                <p>A105,A120,A150,A305,A330,A400,
+                <p>W150,W180,W275,W290,W400,W1375
             </div>
         </div>
         </div>
