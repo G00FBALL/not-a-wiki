7495fe25d5874954965af1ba828377b35df93c8c
G00FY <G00FBALL@users.noreply.github.com>
2017-10-15 15:04:14 -0400

    Update index.php

--- R40-R46/index.php
+++ R40-R46/index.php
@@ -45,19 +45,20 @@
                 </div>
         </div>
         <div class="shelementwhole">
-                    <p onclick="shohid($(this));"><b><a href="#" onclick="return false;"> Titan (SS2 Alternative)<font color="DarkGoldenRod"> Neutral</font></a></b></p>
+                    <p onclick="shohid($(this));"><b><a href="#" onclick="return false;"> Titan (SS2)<font color="DarkGoldenRod"> Neutral</font></a></b></p>
                                 <div class="autohide">
                 <p><b>Author</b>: Artcrow
                 <p><b>Faction</b>: Titan
                 <p><b>Bloodline</b>: Druid
-                <p><b>Range</b>: 1e28 (10 Oc) gems - ?
-                <p>S105,S150,S215,S330,S500,
+                <p><b>Range</b>: 1e27 (1 Oc) gems - ?
+                <p>S150,S215,S270,S330,S500,
                 <p>C150,C251,C330,C340,C375,C400,
                 <p>D55,D200,D245,D290,D320,
-                <p>E135,E145,E275,E320,E400,
+                <p>E135,E145,E275,E320,E460,
                 <p>A105,A120,A150,A250,A400,
                 <p>W150,W180,W290,W320,W330,W400
-                <p><b>Notes</b>: Requires 3 hours to unlock SS6. Not recommended for runs of over 9 hours (beaten by the original SS2 build). Swap S500 with S400 if higher.
+                <p><b>Notes</b>: Requires 6 hours to unlock SS6. Swap S500 with S400 if higher.
+                <p>Also used for stage 4 unlock at e31 (10 No) gems (requires using S400 with at least 14k bonus, estimated run time 10 hours)
          </div>
     </div>
             </div>
@@ -70,7 +71,9 @@
         <p><b>Author</b>: Artcrow
         <p><b>Faction</b>: Good Mercenary
         <p><b>Bloodline</b>: Druid
-        <p>FR1,FR7,FR9,EL3,AN3,AN5,AN6,GB7,DD6,FC5,FC7,DN2,SP:Fairy Chanting,SP:Hellfire Blast
+                <p><b>Range</b>: ? gems
+        <p>FR1,FR7,FR9,EL3,AN3,AN5,AN6,GB7,DD6,FC5,FC7,DN2,
+        <p>SP:Fairy Chanting,SP:Hellfire Blast
                 <p><b>Notes</b>: 20 million spells cast in 12 hours. S400=12500.
                 <p><b>Notes</b>: 40 million in 20 hours. S400=16000.
                 <p><b>Notes</b>: 55 milion in 24 hours. S400=18000.
@@ -81,6 +84,7 @@
             <div class="autohide">
                 <p><b>Author</b>: Artcrow
                 <p><b>Faction</b>: Fairy/Dwarf
+                <p><b>Range</b>: e31 (10 No) gems+
                 <p><b>Bloodline</b>: Druid
                 <p>S180,S250,S330,S400,S545,S590,
                 <p>C10,C80,C150,C330,C340,C400,C405,
@@ -88,23 +92,23 @@
                 <p>E50,E135,E145,E330,E400,
                 <p>A30,A105,A120,A150,A250,
                 <p>W150,W180,W290,W320,W400
-                <p><b>Notes</b>: Estimated run time 1 day+. Can be tried at e30 gems but may take over 3 days.</p>
+                <p><b>Notes</b>:  Estimated run time 1 day+. Can be tried at e30 gems but may take over 3 days.
             </div>
         </div>
         <div class="shelementwhole">
+            <p onclick="shohid($(this));"><b><a href="#" onclick="return false;"> Dragon Stage 4 <font color="Blue">Good</font></a></b></p>
             <div class="autohide">
                 <p><b>Author</b>: Artcrow
                 <p><b>Faction</b>: Titan
                 <p><b>Bloodline</b>: Druid
                 <p><b>Range</b>: 1e27 (1 Oc) gems - ?
-                <p>S150,S215,S250,S330,S500,
+                <p>S150,S215,S270,S330,S500,
                 <p>C150,C251,C330,C340,C375,C400,
                 <p>D55,D200,D245,D290,D320,
-                <p>E135,E145,E275,E400,E460,
+                <p>E135,E145,E275,E320,E460,
                 <p>A105,A120,A150,A250,A400,
                 <p>W150,W180,W290,W320,W330,W400
-                <p><b>Note</b>: Requires 6 hours to unlock SS6. Swap E400 with E30 if run over 12 hours. Swap S500 with S400 if higher.
-                <p><b>Note</b>:  At e31 (10 No) gems (requires using S400 with at least 14k bonus, estimated run time 10 hours)
+                <p>Requires using S400 with at least 14k bonus, estimated run time 10 hours.
             </div>
         </div>
         <!--<div class="shelementwhole">
