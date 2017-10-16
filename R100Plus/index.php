fbdc75bbafe2ea5bacd0d69e48b6d6478e37ef81
G00FY <G00FBALL@users.noreply.github.com>
2017-10-16 17:27:25 -0400

    updating builds

--- R100Plus/index.php
+++ R100Plus/index.php
@@ -14,34 +14,36 @@
     <p><b>Production Builds</b></p>
     <div class="category">
         <div class="shelementwhole">
-            <p onclick="shohid($(this));"><b> <a href="#" onclick="return false;">Druidline Druids 0-1e27<font color="DarkGoldenRod"> Neutral</font></a></b></p>
+            <p onclick="shohid($(this));"><b> <a href="#" onclick="return false;">Druidline Druids 0 - 1e27 (1 Oc) Gems<font color="DarkGoldenRod"> Neutral</font></a></b></p>
             <div class="autohide">
-                <p><b>Author</b>: ensteffahn
-                <p><b>Faction</b>: Druid
-                <p><b>Bloodline</b>: Druid
-                <p><b>Research Template</b>
-                <p>S3200,
-                <p>C300,C340,
-                <p>D150,D275,
-                <p>E260,E1425,
-                <p>A250,A251,A375,
-                <p>W350,W1375
+                    <p><b>Author</b>: ensteffahn
+                    <p><b>Range</b>: 0 - 1e27 (1 Oc) Gems
+                    <p><b>Faction</b>: Druid
+                    <p><b>Bloodline</b>: Druid
+                    <p><b>Template</b>
+                    <p>S3200,
+                    <p>C300,C340,
+                    <p>D150,D275,
+                    <p>E260,E1425,
+                    <p>A250,A251,A375,
+                    <p>W350,W1375
          </div>
     </div>
         <div class="shelementwhole">
-            <p onclick="shohid($(this));"><b> <a href="#" onclick="return false;">Undeadline Druids ?-1e32 gems<font color="DarkGoldenRod"> Neutral</font></a></b></p>
+            <p onclick="shohid($(this));"><b> <a href="#" onclick="return false;">Undeadline Druids ?-1e32 (100 No) Gems<font color="DarkGoldenRod"> Neutral</font></a></b></p>
             <div class="autohide">
-                <p><b>Author</b>: 123123winz
-                <p><b>Faction</b>: Druid
-                <p><b>Bloodline</b>: Undead
-                <p><b>Research Template</b>
-                <p>S3200,
-                <p>C300,C400,
-                <p>D150,D275,
-                <p>E260,E1425,
-                <p>A251,A375,A400,
-                <p>W350,W1375
-                <p><b>Notes</b>: Takes several minutes to set up.
+                    <p><b>Author</b>: 123123winz
+                    <p><b>Range</b>: ?-1e32 (100 No) Gems
+                    <p><b>Faction</b>: Druid
+                    <p><b>Bloodline</b>: Undead
+                    <p><b>Template</b>
+                    <p>S3200,
+                    <p>C300,C400,
+                    <p>D150,D275,
+                    <p>E260,E1425,
+                    <p>A251,A375,A400,
+                    <p>W350,W1375
+                    <p><b>Notes</b>: Takes several minutes to set up.
          </div>
     </div>
             </div>
@@ -49,13 +51,14 @@
     <p><b>Special Builds</b></p>
     <div class="category">
         <div class="shelementwhole">
-            <p onclick="shohid($(this));"><b> <a href="#" onclick="return false;">Mana Bomb (GB11 unlock build) e30 gems<font color="DarkGoldenRod"> Neutral</font></a></b></p>
+            <p onclick="shohid($(this));"><b> <a href="#" onclick="return false;">Mana Bomb (GB11 Bulid) 1e30 (1 No) Gems<font color="DarkGoldenRod"> Neutral</font></a></b></p>
             <div class="autohide">
                     <p><b>Author</b>: ensteffahn
                     <p><b>Requiments</b>: Ziggurat unlock
+                    <p><b>Range</b>: 1e30 (1 No) Gems
                     <p><b>Faction</b>: Druid
                     <p><b>Bloodline</b>: Dragon
-                    <p><b>Research Template</b>
+                    <p><b>Template</b>
                     <p>S3200,
                     <p>C300,C400,
                     <p>D150,D275,
@@ -67,76 +70,166 @@
     </div>
             </div>
                     <br/>
-    <p><b>Special Builds</b></p>
+    <p><b>Unique Building 2 Quest Unlock</b></p>
     <div class="category">
         <div class="shelementwhole">
-            <p onclick="shohid($(this));"><b> <a href="#" onclick="return false;">Swarming Tower (Fairy UB2) Quest e21 gems<font color="Blue"> Good</font></a></b></p>
+            <p onclick="shohid($(this));"><b> <a href="#" onclick="return false;">Swarming Tower (Fairy) e16 (10 Qa) Gems<font color="Blue"> Good</font></a></b></p>
             <div class="autohide">
                     <p><b>Author</b>: FutureSin
-                    <p><b>Requiments</b>: Veteran Figurine
-                    <p><b>Range</b>: 1e90 (1 NoVg) gems(?)
+                    <p><b>Range</b>: e16 (10 Qa) Gems
                     <p><b>Faction</b>: Fairy
                     <p><b>Bloodline</b>: Undead
-                    <p><b>Research Template</b>
-                    <p>S1,S180,S500,
-                    <p>C10,C1325,
-                    <p>D225,D250,
+                    <p><b>Template</b>
+                    <p>S1,S105,S180,
+                    <p>C10,C400,
+                    <p>D150,D225,
                     <p>E50,E1325,
                     <p>A30,A120,
                     <p>W1375
-                    <p><b>Notes</b>: Estimated time of 20 minutes for unlock.
+                    <p><b>Notes</b>: Estimated time of 2 minutes for unlock.
         </div>
     </div>
         <div class="shelementwhole">
-            <p onclick="shohid($(this));"><b> <a href="#" onclick="return false;">Arboreal City (Elf UB2) Quest ? gems<font color="Blue"> Good</font></a></b></p>
+            <p onclick="shohid($(this));"><b> <a href="#" onclick="return false;">Arboreal City (Elf) 1e27 (1 Oc) Gems<font color="Blue"> Good</font></a></b></p>
             <div class="autohide">
                     <p><b>Author</b>: ensteffahn
+                    <p><b>Range</b>: 1e27 (1 Oc) Gems
                     <p><b>Faction</b>: Elf
                     <p><b>Bloodline</b>: Druid
-                    <p><b>Research Template</b>
-                    <p>S105,S175,
+                    <p><b>Template</b>
+                    <p>S175,S3200,
                     <p>C1,C225,C400,
-                    <p>D50,D200,
+                    <p>D50,D290,
                     <p>E25,E320,
                     <p>A3400,
                     <p>W10,W1375
-                    <p><b>Notes</b>: Estimated time of 5 hours to unlock. Put only All Creation and Tax Collection on autocast once setup is done.
+                    <p><b>Notes</b>: Set all spells that cost less than 30000 mana (except Holy Light tier 6) on bronze, Tax Collections on silver (contingency at 60-70%)
+                    <p><b>Notes</b>: Estimated time of 2-2.5 hours to unlock.
         </div>
     </div>
         <div class="shelementwhole">
-            <p onclick="shohid($(this));"><b> <a href="#" onclick="return false;">Flesh Workshop (Undead UB2) Quest ? gems<font color="DarkRed"> Evil</font></a></b></p>
+            <p onclick="shohid($(this));"><b> <a href="#" onclick="return false;">Holy Site (Angel) 1e20 (100 Qi) Gems<font color="Blue"> Good</font></a></b></p>
             <div class="autohide">
                     <p><b>Author</b>: ensteffahn
+                    <p><b>Range</b>: 1e20 (100 Qi) Gems
+                    <p><b>Faction</b>: Angel
+                    <p><b>Bloodline</b>: Drow
+                    <p><b>Template</b>
+                    <p>S10,S3200,
+                    <p>C25,C330,
+                    <p>D1,D175,D290,
+                    <p>E1325,
+                    <p>A50,A3400,
+                    <p>W10,W1375
+                    <p><b>Notes</b>: Get 400 Royal Exchanges for Druid Lineage, then cast all spells for unlock.
+                    <p><b>Notes</b>: Estimated time of 2 minutes to unlock.
+        </div>
+    </div>
+        <div class="shelementwhole">
+            <p onclick="shohid($(this));"><b> <a href="#" onclick="return false;">Slave Market (Goblin) 1e27 (1 Oc) Gems<font color="DarkRed"> Evil</font></a></b></p>
+            <div class="autohide">
+                    <p><b>Author</b>: ensteffahn
+                    <p><b>Range</b>: 1e27 (1 Oc) Gems
+                    <p><b>Faction</b>: Goblin
+                    <p><b>Bloodline</b>: Druid
+                    <p><b>Template</b>
+                    <p>S50,S105,
+                    <p>C175,C340,
+                    <p>D150,
+                    <p>E1,E145,E225,
+                    <p>A10,A400,
+                    <p>W25,W1375
+                    <p><b>Notes</b>: Estimated time of 2 minutes to unlock.
+        </div>
+    </div>
+        <div class="shelementwhole">
+            <p onclick="shohid($(this));"><b> <a href="#" onclick="return false;">Flesh Workshop (Undead) 1e27 (1 Oc) Gems<font color="DarkRed"> Evil</font></a></b></p>
+            <div class="autohide">
+                    <p><b>Author</b>: ensteffahn
+                    <p><b>Range</b>: 1e27 (1 Oc) Gems
                     <p><b>Faction</b>: Undead
                     <p><b>Bloodline</b>: Drow
-                    <p><b>Research Template</b>
+                    <p><b>Template</b>
                     <p>S225,S3200,
                     <p>C400,
                     <p>D25,D3350,
                     <p>E10,E1325,
                     <p>A1,A175,A3400,
                     <p>W50,W205
-                    <p><b>Notes</b>: Put TC on silver (contingency at 100%), Temporal Flux and all other spells/spell tiers, that cost at most ~2500 mana on bronze. After you have gotten a sufficient amount of spell tiers (most likely SS3 and rest at 6), get to max mana and cast all spells. If you don't get quite enough, wait a bit longer for Temporal Flux to build up more. should take at most 30 minutes.
+                    <p><b>Notes</b>: Set all spells, that cost at most 2500 mana (and Temporal Flux) on bronze, TC on silver (contingency 90%), get to SS3 and tier 6 on the rest, get to max mana and cast all spells.
+                    <p><b>Notes</b>: Estimated time of 30 minutes to unlock.
         </div>
     </div>
         <div class="shelementwhole">
-            <p onclick="shohid($(this));"><b> <a href="#" onclick="return false;">Ziggurat (Druid UB2) Quest e31 gems<font color="DarkGoldenRod"> Neutral</font></a></b></p>
+            <p onclick="shohid($(this));"><b> <a href="#" onclick="return false;">Burning Abyss (Demon) 1e27 (1 Oc) Gems<font color="DarkRed"> Evil</font></a></b></p>
             <div class="autohide">
                     <p><b>Author</b>: ensteffahn
+                    <p><b>Range</b>: 1e27 (1 Oc) Gems
+                    <p><b>Faction</b>: Demon
+                    <p><b>Bloodline</b>: Elf
+                    <p><b>Template</b>
+                    <p>S225,S3200,
+                    <p>C400,
+                    <p>D25,D3350,
+                    <p>E10,E1325,
+                    <p>A1,A175,A3400,
+                    <p>W50,W205
+                    <p><b>Notes</b>: Set all spells, that cost at most 2500 mana (and Temporal Flux) on bronze, TC on silver (contingency 90%), get to SS3 and tier 6 on the rest, get to max mana and cast all spells.
+                    <p><b>Notes</b>: Estimated time of 30 minutes to unlock.
+        </div>
+    </div>
+        <div class="shelementwhole">
+            <p onclick="shohid($(this));"><b> <a href="#" onclick="return false;">Mountain Palace (Titan) 1e24 (1 Sp) Gems<font color="DarkGoldenRod"> Neutral</font></a></b></p>
+            <div class="autohide">
+                    <p><b>Author</b>: ensteffahn
+                    <p><b>Range</b>: 1e24 (1 Sp) Gems
+                    <p><b>Faction</b>: Titan
+                    <p><b>Bloodline</b>: Druid
+                    <p><b>Template</b>
+                    <p>S300,S3200,
+                    <p>C251,C375,C400,
+                    <p>D200,D350,
+                    <p>E275,E1325,
+                    <p>A3400,
+                    <p>W260,W1375
+                    <p><b>Notes</b>: Estimated time of 5 minutes to unlock.
+        </div>
+    </div>
+        <div class="shelementwhole">
+            <p onclick="shohid($(this));"><b> <a href="#" onclick="return false;">Ziggurat (Druid) 1e31 (10 No) Gems<font color="DarkGoldenRod"> Neutral</font></a></b></p>
+            <div class="autohide">
+                    <p><b>Author</b>: ensteffahn
+                    <p><b>Range</b>: 1e31 (10 No) Gems
                     <p><b>Faction</b>: Druid
                     <p><b>Bloodline</b>: Goblin
-                    <p><b>Research Template</b>
+                    <p><b>Template</b>
                     <p>S3200,
                     <p>C300,C340,
                     <p>D150,D275,
                     <p>E135,E260,
                     <p>A251,A375,A400,
                     <p>W350,W1375
+                    <p><b>Notes</b>: Estimated 12 hours to unlock.
+                    <p><b>Notes</b>: Swap C340 with C400 if you own T7 spells, which will also allow unlock in 2 hours.
+        </div>
+    </div>
+        <div class="shelementwhole">
+            <p onclick="shohid($(this));"><b> <a href="#" onclick="return false;">Forbidden Library (Faceless) 1e27 (1 Oc) Gems<font color="DarkGoldenRod"> Neutral</font></a></b></p>
+            <div class="autohide">
+                    <p><b>Author</b>: ensteffahn
+                    <p><b>Range</b>: 1e27 (1 Oc) Gems
+                    <p><b>Faction</b>: Faceless
+                    <p><b>Bloodline</b>: Druid
+                    <p><b>Template</b>
+                    <p>S3200,
+                    <p>C300,C340,
+                    <p>D150,D275,
+                    <p>E260,E290,
+                    <p>A250,A251,A375,
+                    <p>W350,W1375
+                    <p><b>Notes</b>: Estimated 12 hours to unlock.
         </div>
     </div>
 </div>
 </div>
 <?php include "../scripts/footer.html"; ?>
-
-
-
