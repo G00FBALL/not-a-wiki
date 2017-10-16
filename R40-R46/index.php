9ef8849d32c17cbe460f9d1185049b1cd559d8bd
G00FY <G00FBALL@users.noreply.github.com>
2017-10-16 13:35:16 -0400

    Update index.php

--- R40-R46/index.php
+++ R40-R46/index.php
@@ -92,7 +92,7 @@
                 <p>E50,E135,E145,E330,E400,
                 <p>A30,A105,A120,A150,A250,
                 <p>W150,W180,W290,W320,W400
-                <p><b>Notes</b>:  Estimated run time 1 day+. Can be tried at e30 gems but may take over 3 days.
+                <p><b>Notes</b>: Estimated run time 1 day+. Can be tried at e30 gems but may take over 3 days.
             </div>
         </div>
         <div class="shelementwhole">
@@ -108,8 +108,8 @@
                 <p>E135,E145,E275,E320,E460,
                 <p>A105,A120,A150,A250,A400,
                 <p>W150,W180,W290,W320,W330,W400
-                <p>Requires using S400 with at least 14k bonus, estimated run time 10 hours.
-                <p>If you do not buff S400 and S500 is higher use S500.
+                <p><b>Notes</b>: Requires using S400 with at least 14k bonus, estimated run time 10 hours.
+                <p><b>Notes</b>: If you do not buff S400 and S500 is higher use S500.
             </div>
         </div>
         <!--<div class="shelementwhole">
