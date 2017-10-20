81285ebac80a35f53fdd6c166dbe9abd407ee44c
G00FY <G00FBALL@users.noreply.github.com>
2017-10-20 07:03:49 -0400

    fixed error

--- R40-R46/index.php
+++ R40-R46/index.php
@@ -57,7 +57,7 @@
                 <p>E135,E145,E275,E320,E460,
                 <p>A105,A120,A150,A250,A400,
                 <p>W150,W180,W290,W320,W330,W400
-                <p><b>Notes</b>: Requires 6 hours to unlock SS6. Swap S500 with S400 if higher.
+                <p><b>Notes</b>: Requires 6 hours to unlock SS2. Swap S500 with S400 if higher.
                 <p>Also used for stage 4 unlock at e31 (10 No) gems (requires using S400 with at least 14k bonus, estimated run time 10 hours)
          </div>
     </div>
