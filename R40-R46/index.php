021f9fe726de8070f3ede661f2d0d7f12e5585e0
G00FY <G00FBALL@users.noreply.github.com>
2017-10-27 07:00:13 -0400

    updated builds

--- R40-R46/index.php
+++ R40-R46/index.php
@@ -7,7 +7,10 @@
     <h6><img src="http://musicfamily.org/realm/Factions/picks/TopPageResearch.png" alt="Spellcraft" align="middle"></h6>
     <br/>
     <br/>
-        <p><b>NOTE</b>: This page will be updated in the near future in the meantime you can find new builds <a target="_blank" href="http://www.kongregate.com/forums/8945-realm-grinder/topics/922600-3-0-build-megathread-under-construction-please-post-builds-to-add?page=1"><b>here</b></a></p>
+    <p><b>Updated Oct. 26th 2017</b>
+    <p>All builds comes from the  <a target="_blank" href="http://www.kongregate.com/forums/8945-realm-grinder/topics/922600-3-0-build-megathread-under-construction-please-post-builds-to-add?page=1"><b>megathread</b></a>  forum page</p>
+<br/>
+<br/>
         <p><b>Note</b>: It is recommended to get at least 10 No (1e31) gems before starting stage 3 of Dragon Unlocks, 100 No (1e32) will be quicker for unlock stages. </p>
         <p><b>Note</b>: Make sure to get Prismatic Mana trophy as soon as possible! </p>
 <div class="shlisting">
@@ -18,24 +21,25 @@
             <p onclick="shohid($(this));"><b><a href="#" onclick="return false;">Titan (Rush) <font color="DarkGoldenRod"> Neutral</font></a></b></p>
             <div class="autohide">
                 <p><b>Author</b>: Artcrow
+                <p><b>Range</b>: 0 - ?
                 <p><b>Faction</b>: Titan
                 <p><b>Bloodline</b>: Druid
-                <p><b>Range</b>: 0 - ?
                 <p>S135,S150,S270,S330,S500,
                 <p>C150,C251,C330,C340,C375,C400,
                 <p>D55,D200,D245,D290,D320,
                 <p>E135,E145,E275,E330,E400,
                 <p>A105,A120,A250,A330,A400,
                 <p>W150,W180,W290,W320,W330,W400
+                <p><b>Notes</b>: Requires Spell tiers to get past 1e24 (1 Sp) gems. Recommended doing T3 titan spell tiers (Call to Arms, Gem Grinder, Lightning Strike, Spiritual Surge).
             </div>
         </div>
         <div class="shelementwhole">
                     <p onclick="shohid($(this));"><b><a href="#" onclick="return false;"> Titan (GG2/LS2)<font color="DarkGoldenRod"> Neutral</font></a></b></p>
                                 <div class="autohide">
                 <p><b>Author</b>: Artcrow
+                <p><b>Range</b>: ? gems - ?
                 <p><b>Faction</b>: Titan
                 <p><b>Bloodline</b>: Druid
-                <p><b>Range</b>: ? gems - ?
                 <p>S150,S250,S270,S330,S500,
                 <p>C150,C251,C330,C340,C375,C400,
                 <p>D55,D200,D290,D320,D350,
@@ -46,20 +50,56 @@
                 </div>
         </div>
         <div class="shelementwhole">
-                    <p onclick="shohid($(this));"><b><a href="#" onclick="return false;"> Titan (SS2)<font color="DarkGoldenRod"> Neutral</font></a></b></p>
+                    <p onclick="shohid($(this));"><b><a href="#" onclick="return false;"> Titan (SS2) (Stage 4 Unlock)<font color="DarkGoldenRod"> Neutral</font></a></b></p>
                                 <div class="autohide">
                 <p><b>Author</b>: Artcrow
+                <p><b>Range</b>: 1e27 (1 Oc) gems - ?
                 <p><b>Faction</b>: Titan
                 <p><b>Bloodline</b>: Druid
-                <p><b>Range</b>: 1e27 (1 Oc) gems - ?
-                <p>S150,S215,S270,S330,S500,
+                <p>S150,S215,S250,S330,S500,
                 <p>C150,C251,C330,C340,C375,C400,
                 <p>D55,D200,D245,D290,D320,
                 <p>E135,E145,E275,E320,E460,
                 <p>A105,A120,A150,A250,A400,
                 <p>W150,W180,W290,W320,W330,W400
-                <p><b>Notes</b>: Requires 6 hours to unlock SS2. Swap S500 with S400 if higher.
-                <p>Also used for stage 4 unlock at e31 (10 No) gems (requires using S400 with at least 14k bonus, estimated run time 10 hours)
+                <p><b>Notes</b>: Requires 6 hours to unlock SS2.
+                <p><b>Notes</b>: Swap S500 with S400 if higher.
+                <p><b>Notes</b>: Swap E460 with E30 if Run if 12 hours run or more.
+                <p><b>Notes</b>: Also used for stage 4 unlock at e3? (?) gems (requires using S400 with at least 14k bonus, estimated run time ? hours)
+                </div>
+        </div>
+        <div class="shelementwhole">
+                    <p onclick="shohid($(this));"><b><a href="#" onclick="return false;"> Spider Sanctuary<font color="DarkRed"> Evil</font></a></b></p>
+                                <div class="autohide">
+                <p><b>Author</b>: Artcrow
+                <p><b>Range</b>: 1e30 (1 No) -? Gems
+                <p><b>Faction</b>: Goblin/Drow
+                <p><b>Bloodline</b>: Druid
+                <p>S50,S215,S305,S330,S400,
+                <p>C120,C135,C150,C340,C400,
+                <p>D55,D200,D245,D320,D400,
+                <p>E30,E80,E135,E145,E225,E400,
+                <p>A10,A105,A120,A150,A400,
+                <p>W25,W180,W205,W400,W405,W520,W525
+                <p><b>Notes</b>: Replace C135 with C200 if stronger.
+                <p><b>Notes</b>: Run for 3 days to unlock T3 spells.
+                </div>
+        </div>
+        <div class="shelementwhole">
+                    <p onclick="shohid($(this));"><b><a href="#" onclick="return false;"> Goblin Banks (Stage 4 Unlock?)<font color="DarkRed"> Evil</font></a></b></p>
+                                <div class="autohide">
+                <p><b>Author</b>: Artcrow
+                <p><b>Requirements</b>: T3 Goblin/Drow spells
+                <p><b>Range</b>: 1e30 (1 No) -? Gems
+                <p><b>Faction</b>: Goblin/Drow
+                <p><b>Bloodline</b>: Druid
+                <p>S50,S215,S305,S330,S400,
+                <p>C150,C175,C340,C400,C500,
+                <p>D55,D200,D245,D250,D320,
+                <p>E1,E80,E135,E145,E225,E330,
+                <p>A105,A120,A150,A400,A410,
+                <p>W25,W150,W180,W205,W400,W520,W525
+                <p><b>Notes</b>: Requires 1.5-2 days to set up.
          </div>
     </div>
             </div>
@@ -67,14 +107,14 @@
     <p><b>Unlock Builds</b></p>
     <div class="category">
         <div class="shelementwhole">
-                    <p onclick="shohid($(this));"><b><a href="#" onclick="return false;"> Dragon Stage 2 + S400 buff<font color="Blue"> Good</font></a></b></p>
-                                <div class="autohide">
-        <p><b>Author</b>: Artcrow
-        <p><b>Faction</b>: Good Mercenary
-        <p><b>Bloodline</b>: Druid
+            <p onclick="shohid($(this));"><b><a href="#" onclick="return false;"> Dragon Stage 2 + S400 buff<font color="Blue"> Good</font></a></b></p>
+            <div class="autohide">
+                <p><b>Author</b>: Artcrow
                 <p><b>Range</b>: ? gems
-        <p>FR1,FR7,FR9,EL3,AN3,AN5,AN6,GB7,DD6,FC5,FC7,DN2,
-        <p>SP:Fairy Chanting,SP:Hellfire Blast
+                <p><b>Faction</b>: Good Mercenary
+                <p><b>Bloodline</b>: Druid
+                <p>FR1,FR7,FR9,EL3,AN3,AN5,AN6,GB7,DD6,FC5,FC7,DN2,
+                <p>SP:Fairy Chanting,SP:Hellfire Blast
                 <p><b>Notes</b>: 20 million spells cast in 12 hours. S400=12500.
                 <p><b>Notes</b>: 40 million in 20 hours. S400=16000.
                 <p><b>Notes</b>: 55 milion in 24 hours. S400=18000.
@@ -84,8 +124,8 @@
             <p onclick="shohid($(this));"><b><a href="#" onclick="return false;"> Dragon Stage 3 <font color="Blue">Good</font></a></b></p>
             <div class="autohide">
                 <p><b>Author</b>: Artcrow
-                <p><b>Faction</b>: Fairy/Dwarf
                 <p><b>Range</b>: e31 (10 No) gems+
+                <p><b>Faction</b>: Fairy/Dwarf
                 <p><b>Bloodline</b>: Druid
                 <p>S180,S250,S330,S400,S545,S590,
                 <p>C10,C80,C150,C330,C340,C400,C405,
@@ -100,10 +140,11 @@
             <p onclick="shohid($(this));"><b><a href="#" onclick="return false;"> Dragon Stage 4 <font color="DarkGoldenRod">Neutral</font></a></b></p>
             <div class="autohide">
                 <p><b>Author</b>: Artcrow
+                <p><b>Requirements</b>: S400 with at least 14k.
+                <p><b>Range</b>: 1e31+?
                 <p><b>Faction</b>: Titan
                 <p><b>Bloodline</b>: Druid
-                <p><b>Range</b>: 1e31+
-                <p>S150,S215,S270,S330,S400,
+                <p>S150,S215,S250,S330,S500,
                 <p>C150,C251,C330,C340,C375,C400,
                 <p>D55,D200,D245,D290,D320,
                 <p>E135,E145,E275,E320,E460,
@@ -112,23 +153,6 @@
                 <p><b>Notes</b>: Requires using S400 with at least 14k bonus, estimated run time 10 hours.
             </div>
         </div>
-        <!--<div class="shelementwhole">
-            <p onclick="shohid($(this));"><b><a href="#" onclick="return false;">Stage 4 Grow Dragon <font color="DarkGoldenRod">Neutral</font></a></b></p>
-            <div class="autohide">
-                <p><b>Author</b>: Fonzoland + Curunur
-                <p><b>Faction</b>: Titan
-                <p><b>Bloodline</b>: Druid
-                <p><b>Range</b>: <b>10 No (1e31) gems</b>
-                <p>S150,S270,S330,S400,S500,
-                <p>C150,C251,C330,C340,C375,C400,
-                <p>D55,D200,D290,D320,D350,
-                <p>E135,E145,E275,E330,E460,
-                <p>A105,A120,A150,A250,A330,
-                <p>W150,W180,W290,W320,W330,W400
-                <p><b>Note</b>: Use this build after getting to <b>1e31 gems</b>
-                <p><b>Notes</b>: Gets stage 4 unlock almost instantly.
-            </div>
-        </div>--!>
             </div>
     </div>
 <?php include "../scripts/footer.html"; ?>
