b37d10aaefa685c21bdf80ab7436868862283e83
G00FY <G00FBALL@users.noreply.github.com>
2017-10-27 05:24:10 -0400

    Updated builds

--- R60-R75/index.php
+++ R60-R75/index.php
@@ -7,8 +7,8 @@
 <h6><img src="http://musicfamily.org/realm/Factions/picks/TopPageResearch.png" alt="Spellcraft" align="middle"></h6>
 <br/>
 <br/>
-    <p><b>NOTE</b>: This page will be updated in the near future in the meantime you can find new builds <a target="_blank" href="http://www.kongregate.com/forums/8945-realm-grinder/topics/922600-3-0-build-megathread-under-construction-please-post-builds-to-add?page=1"><b>here</b></a></p>
-    <p><b>NOTE</b>: We will be adding builds as we get them.<br/>
+    <p><b>Updated Oct. 27th 2017</b>
+    <p>All builds comes from the  <a target="_blank" href="http://www.kongregate.com/forums/8945-realm-grinder/topics/922600-3-0-build-megathread-under-construction-please-post-builds-to-add?page=1"><b>megathread</b></a>  forum page</p>
 <br/>
 <div class="shlisting">
     <p><b>Production Builds</b></p>
@@ -17,9 +17,9 @@
             <p onclick="shohid($(this));"><b> <a href="#" onclick="return false;">Titan Starter (R60-R69)<font color="DarkGoldenRod"> Neutral</font></a></b></p>
             <div class="autohide">
                 <p><b>Author</b>: Wlerin
+                <p><b>Range</b>: 0 - 1e33 (1 Dc) Gems
                 <p><b>Faction</b>: Titan/Dragon
                 <p><b>Bloodline</b>: Druid
-                <p><b>Range</b>: 0 - 1e33 (1 Dc) Gems
                 <p><b>Template</b>
                 <p>S105,S200,S270,S330,S500,
                 <p>C150,C250,C330,C340,C400,C460,
@@ -33,16 +33,18 @@
             <p onclick="shohid($(this));"><b> <a href="#" onclick="return false;">Undeadline Faceless (R60-R69)<font color="DarkGoldenRod"> Neutral</font></a></b></p>
             <div class="autohide">
                 <p><b>Author</b>: Shophaune
+                <p><b>Requirements</b>: Undead Lineage level 5+
+                <p><b>Range</b>: 1e30 (1 No) - 1e80 (100 QiVg) Gems
                 <p><b>Faction</b>: Faceless/Dragon
                 <p><b>Bloodline</b>: Undead
-                <p><b>Range</b>: 1e30 (1 No) - 1e80 (100 QiVg) Gems
                 <p><b>Template</b>
                 <p>S215,S270,S305,S330,S1450,S1500,
                 <p>C250,C340,C400,C1300,C1325,
                 <p>D245,D260,D320,D1125,D1275,D1375,
-                <p>E135,E145,E320,E1225,E1425,
+                <p>E135,E145,E460,E1225,E1425,
                 <p>A105,A120,A150,A305,A400,
                 <p>W150,W180,W275,W400,W1375
+                <p><b>Notes</b>: May need excavations to get 400 Royal Exchanges for lineage.
          </div>
     </div>
 <br/>
@@ -50,26 +52,26 @@
             <p onclick="shohid($(this));"><b> <a href="#" onclick="return false;">Titan Starter (R70-R75)<font color="DarkGoldenRod"> Neutral</font></a></b></p>
             <div class="autohide">
                 <p><b>Author</b>: Wlerin
+                <p><b>Range</b>: 0 - 1e33 (1 Dc) Gems
                 <p><b>Faction</b>: Titan/Dragon
                 <p><b>Bloodline</b>: Druid
-                <p><b>Range</b>: 0 - 1e33 (1 Dc) Gems
                 <p><b>Template</b>
                 <p>S105,S200,S270,S300,S330,S500,
                 <p>C150,C250,C330,C340,C400,C460,C500,
                 <p>D55,D150,D200,D245,D250,D320,
-                <p>E135,E145,E275,E320,E330,E400,
+                <p>E135,E145,E275,E320,E330,E400
                 <p>A105,A120,A250,A305,A330,A400,
                 <p>W120,W150,W180,W260,W320,W330,W400
-                <p><b>Notes</b>: Replace D250 with D350 if higher (e9 (1 B) gems+).
+                <p><b>Notes</b>: Replace D250 with D350 if higher (e9 (1 B) gems+)
          </div>
     </div>
         <div class="shelementwhole">
             <p onclick="shohid($(this));"><b> <a href="#" onclick="return false;">Undeadline Faceless (R70-R75)<font color="DarkGoldenRod"> Neutral</font></a></b></p>
             <div class="autohide">
                 <p><b>Author</b>: TemperGrace
+                <p><b>Range</b>: 1e30 (1 No) - 1e90 (1 NoVg) Gems
                 <p><b>Faction</b>: Faceless/Dragon
                 <p><b>Bloodline</b>: Undead
-                <p><b>Range</b>: 1e30 (1 No) - 1e90 (1 NoVg) Gems
                 <p><b>Template</b>
                 <p>S135,S215,S270,S305,S330,S1450,S1500,
                 <p>C150,C250,C340,C400,C1300,C1325,
@@ -77,14 +79,71 @@
                 <p>E135,E145,E320,E350,E1225,E1425,
                 <p>A105,A120,A150,A305,A330,A400,
                 <p>W150,W180,W275,W290,W400,W1375
+         </div>
+    </div>
+         </div>
+        <br/>
+    <p><b>Special Builds</b></p>
+    <div class="category">
+        <div class="shelementwhole">
+            <p onclick="shohid($(this));"><b> <a href="#" onclick="return false;">DC4<font color="DarkGoldenRod"> Neutral</font></a></b></p>
+            <div class="autohide">
+                <p><b>Author</b>: Necrodoom
+                <p><b>Range</b>: 1e51 (1 Sxd) Gems
+                <p><b>Faction</b>: Faceless/Dragon
+                <p><b>Bloodline</b>: Druid
+                <p><b>Template</b>
+                <p>S105,S375,S400,S500,S1450,S1500,
+                <p>C250,C340,C400,C1300,C1325,
+                <p>D200,D250,D290,D1125,D1275,D1375,
+                <p>E135,E145,E320,E1225,E1425,
+                <p>A120,A300,A305,A400,A1325,
+                <p>W150,W180,W250,W400,W1375
+                <p><b>Notes</b>: Estimated run time is 1.5-2 hours.
+         </div>
+    </div>
+        <div class="shelementwhole">
+            <p onclick="shohid($(this));"><b> <a href="#" onclick="return false;">UL5/Spell Cataclysm/DC5<font color="DarkGoldenRod"> Neutral</font></a></b></p>
+            <div class="autohide">
+                <p><b>Author</b>: Necrodoom
+                <p><b>Requirements</b>: DC4
+                <p><b>Range</b>: 1e54 (1 Spd) Gems
+                <p><b>Faction</b>: Faceless/Dragon
+                <p><b>Bloodline</b>: Undead
+                <p><b>Template</b>
+                <p>S215,S270,S305,S330,S1450,S1500,
+                <p>C250,C340,C400,C1300,C1325,
+                <p>D245,D260,D320,D1125,D1275,D1375,
+                <p>E135,E145,E320,E1225,E1425,
+                <p>A105,A120,A150,A305,A400,
+                <p>W150,W180,W275,W400,W1375
+                <p><b>Notes</b>: Assuming starting build with 0 excavations, you will need 1500 excavations for UL5, 2600 for Spell Cataclysm, and 3100 for DC5.
+         </div>
+    </div>
+        <div class="shelementwhole">
+            <p onclick="shohid($(this));"><b> <a href="#" onclick="return false;">Dwarf Lineage level 10<font color="DarkGoldenRod"> Neutral</font></a></b></p>
+            <div class="autohide">
+                <p><b>Author</b>: Necrodoom
+                <p><b>Requirements</b>: DC4
+                <p><b>Range</b>: 1e66 (1 Vg) Gems
+                <p><b>Faction</b>: Faceless/Dragon
+                <p><b>Bloodline</b>: Dwarf
+                <p><b>Template</b>
+                <p>S215,S270,S305,S330,S1450,S1500,
+                <p>C250,C340,C400,C1300,C1325,
+                <p>D245,D260,D320,D1125,D1275,D1375,
+                <p>E135,E145,E290,E1225,E1425,
+                <p>A105,A120,A150,A305,A400,
+                <p>W150,W180,W275,W400,W1375
+                <p><b>Notes</b>: Excavate until Dwarf Lineage level 10 is gotten, which allows to get Undead Lineage to level 10 easily via Ancient Heirloom (Which will be almost certainly gotten on or before getting to this build). No point going further as it is of little benefit.
             </div>
           </div>
         </div>
         <br/>
         <br/>
+    <div class="category">
         <p onclick="shohid($(this));"><b> <a href="#" onclick="return false;">Old(not updated) and/or obsolete</a></b></p>
         <div class="category autohide">
-        <div class="shelementwhole">
             <div class="shelementwhole">
                 <p onclick="shohid($(this));"><b><a href="#" onclick="return false;">Titan Start <font color="DarkGoldenRod">Neutral</font></a></b></p>
                 <div class="autohide">
@@ -116,7 +175,6 @@
                     <p><b>Notes</b>: Excavating for 400 Royal Exchanges when running this build for the first time in an R might be needed.
             </div>
         </div>
-        <div class="shelementwhole">
             <div class="shelementwhole">
                 <p onclick="shohid($(this));"><b><a href="#" onclick="return false;">Titan Start <font color="DarkGoldenRod">Neutral</font></a></b></p>
                 <div class="autohide">
@@ -132,7 +190,6 @@
                     <p>W150,W180,W250,W290,W320,W330,W400
             </div>
         </div>
-        <div class="shelementwhole">
             <div class="shelementwhole">
                 <p onclick="shohid($(this));"><b><a href="#" onclick="return false;">Faceless Rush <font color="DarkGoldenRod">Neutral</font></a></b></p>
                 <div class="autohide">
@@ -165,7 +222,6 @@
                     <p><b>Notes</b>: Spend excavations to buy the 400 RE for undead lineage.
             </div>
         </div>
-        <div class="shelementwhole">
             <div class="shelementwhole">
                 <p onclick="shohid($(this));"><b><a href="#" onclick="return false;">W275 Buff <font color="DarkGoldenRod">Neutral</font></a></b></p>
                 <div class="autohide">
@@ -182,7 +238,6 @@
                     <p><b>Notes</b>: Build estimated to require 3 hours until DB5 is purchased. Sunforce assistants helps.
             </div>
         </div>
-        <div class="shelementwhole">
             <div class="shelementwhole">
                 <p onclick="shohid($(this));"><b><a href="#" onclick="return false;">S400 Buff <font color="DarkRed">Evil</font></a></b></p>
                 <div class="autohide">
@@ -202,13 +257,7 @@
             </div>
         </div>
     </div>
-            </div>
-        </div>
     </div>
-            </div>
-        </div>
         </div>
 
 <?php include "../scripts/footer.html"; ?>
-
-
