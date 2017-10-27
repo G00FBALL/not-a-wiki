17647eaea9a0b87cc0ba9122e0650d44991e9574
Necrodoom <doomed.war@gmail.com>
2017-10-27 20:21:23 +0300

    Update angel spell challenge reward

--- Angel/index.php
+++ Angel/index.php
@@ -37,4 +37,5 @@
 <p><b>In-Game Challenge Final Reward</b></p>
 <p><img src="http://musicfamily.org/realm/Factions/picks/God'sThroneChallenges.png" alt="God's Throne" align="middle"> <b>God's Throne</b></p>
 <p><b>Effect</b>: Mana regeneration is increased by 50% while God's Hand is active.</p>
+<p><b>Effect</b>: Also increases offline spell cast amount by multiplicatively based on your offline mana regeneration (10 * log10(1 + x)).
 <?php include "../scripts/footer.html"; ?>
