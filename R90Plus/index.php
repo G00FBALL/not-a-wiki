1961d2ce733221cae1dabfb5f9d75c2e0850ae17
G00FY <G00FBALL@users.noreply.github.com>
2017-10-31 17:27:22 -0400

    add build

--- R90Plus/index.php
+++ R90Plus/index.php
@@ -336,6 +336,33 @@
                     <p><b>Notes</b>: Alternative of the above S400 build, that requires Dragon Breath to cast x2 blue breath to be better. To do so, you have to export/import the save until Dragon's Breath is casting x2 blue every time the spell recasts. To be better than above build, you must maintain x2 blue at least 40% of the time.
                     <p><b>Notes</b>: Estimated run time 5-7 to 8-10 days (Depending on maintained blue x2 time) to unlock Spell Master trophy.
                     <p><b>Notes</b>: Make sure you can commit to this build if you want to use it over the other.
+        </div>
+     </div>
+  </div>
+                    <br/>
+    <p><b>Halloween Builds</b></p>
+    <div class="category">
+        <div class="shelementwhole">
+            <p onclick="shohid($(this));"><b> <a href="#" onclick="return false;">UNSC (2 day Spell Master)<font color="DarkGoldenRod"> Neutral</font></a></b></p>
+            <div class="autohide">
+                    <p><b>Author</b>: Necrodoom
+                    <p><b>Requirements</b>: Veteran Figurine, Vampire L25, Ghost L25
+                    <p><b>Range</b>:1e120 (1 NoTg) Gems(?)
+                    <p><b>Faction</b>: Evil Mercenary
+                    <p><b>Bloodline</b>: Druid
+                    <p><b>A2950</b>: Elf
+                    <p><b>13th Upgrade</b>: UD7
+                    <p><b>Template</b>
+                    <p>EL3,EL7,AN5,TT6,DD6,FC3,FC7,DN2,DN8,DG3,DG4,DG9,
+                    <p>SP:Grand Balance,SP:God's Hand,
+                    <p>S50,S215,S305,S400,S1450,S1500,
+                    <p>C250,C340,C590,C1300,C3000,C3100,
+                    <p>D25,D200,D245,D1275,D1375,D2775,
+                    <p>E135,E145,E320,E350,E1225,E3250,
+                    <p>A120,A410,A590,A1500,A2950,A3400,
+                    <p>W400,W525,W560,W1400,W3050,W3150
+                    <p><b>Notes</b>: Use Vampire to buy dragon lineage and then go Ghost.
+                    <p><b>Notes</b>: Estimated time 2 days to unlock Spell Master trophy.
          </div>
     </div>
             </div>
