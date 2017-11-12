79ac54844710531e24af6ddc7e58fba362de65cf
G00FY <G00FBALL@users.noreply.github.com>
2017-11-12 06:02:22 -0500

    updating NU slots

--- ResearchNonUniqueUpgrades/index.php
+++ ResearchNonUniqueUpgrades/index.php
@@ -5,116 +5,36 @@
 <h6><a href="/realm/ResearchUpgrades/"><img src="http://musicfamily.org/realm/Factions/picks/ResearchTopPage.png"></a></h6>
 <h6>Unique Building Research Upgrade Slots</h6>
 <p><b>Spellcraft</b>
-<p><b>S10</b> - <b>For</b> Angel - <B>Research Name</B> Channeling</p>
-<p><B>Requirement</B>: 2000 Heaven's Gate</P>
-<p><B>Cost</B>: 28.83 Uqag (2.883E127)</P>
-<p><B>Effect</B>: Increase the production of all other buildings by 110% per Heaven's Domain you own.</p>
-<br/>
-<br/>
-<p><B>S2875</B>:  - <b>For</b> Mercenary - <B>Research Name</B> Scholarship</p>    
-<p><B>Requirement</B>: 12000 Freemason's Halls.
-<p><B>Cost</B>: 7.26 Qiqag (7.26e168) 
-<p><B>Effect</B>: Increases the production of Non-Unique buildings by 0.4% per Unique building.
-<p><b>----------------------------------</b></p>
+<p><b>S2875</b> - <b>For</b>: Mercenary - <b>Research Name</b>: Scholarship 
+<p><b>Requirement</b>: 11000 Freemason's Halls.  
+<p><b>Cost</b>: 7.26 Qiqag (7.26e168) 
+<p><b>Effect</b>: Increases the production of Non-Unique buildings by 40% per Unique building.
+<p><b>----------------------------------</b>
 <p><b>Craftsmanship</b>
-<p><b>C10</b> - <b>For</b> Fairy - <B>Research Name</B> Cultivation</p>
-<p><B>Requirement</B>: 2500 Farms
-<p><B>Cost</B>: 28.83 Uqag (2.883E127)</p>
-<p><B>Effect</B>: Increases production of Blacksmith and Inn by 60% per Enchanted field that you have.</p>
-<br/>
-<br/>
-<p><b>C460</b> - <b>For</b> Any (Prestige Neutral) - <B>Research Name</B> Distribution</p>
-<p><B>Requirement</B>: 1M Clicks (this game)</p>
-<p><B>Cost</B>: 502.3 SpSxg (5.023E206)</p>
-<p><B>Effect</B>: Increase the production of non-unique buildings based on the amount of clicks you made in this game.</p>
-<p><B>Formula</B>: 35 * x^0.5%, where x is your Treasure Clicks (This Game) stat.</p>
-<p><b>----------------------------------</b></p>
-<p><b>Divine</b>
-<p><b>D10</b> - <b>For</b> Demon - <B>Research Name</B> Inflame
-<p><B>Requirement</B>: 2,000 Infernal Realms
-<p><B>Cost</B>: 28.83 Uqag (2.883E127)
-<p><B>Effect</B>: Increase the production of all other buildings by 110% per Infernal Realm you own.
-<br/>
-<br/>
-<p><b>D260</b> - <b>For</b> Faceless - <B>Research Name</B> Mutation
-<p><B>Requirement</B>: 3,000 Sunken Cities
-<p><B>Cost</B>: 3.039 SxQig (3.039E171)
-<p><B>Effect</B>: Increase the production of all other buildings by 95% per Sunken Cities you own.
-<p><b>----------------------------------</b></p>
+<p><b>C460</b> - <b>For</b>: All Factions (Prestige Neutral) - <b>Research Name</b>: Distribution 
+<p><b>Requirement</b>: 1M Clicks (This game) 
+<p><b>Cost</b>: 502.3 SpSxg (5.023E206) 
+<p><b>Effect</b>: Increase the production of non-unique buildings based on the amount of clicks you made in this game. 
+<p><b>Formula</b>: 35 * x^0.5%, where x is your Treasure Clicks (This Game) stat.
+<p><b>----------------------------------</b>
 <p><b>Economics</b>
-<p><b>E10</b> - <b>For</b> Undead - <B>Research Name</B> Recycling</p>
-<p><B>Requirement</B>: 2,000 Undercities
-<p><B>Cost</B>: 28.83 Uqag (2.883E127)
-<p><B>Effect</B>: Increase the production of all other buildings by 95% per Undercity you own.</p>
-<br/>
-<br/>
-<p><b>E260</b> - <b>For</b> Druid - <B>Research Name</B> Sifting
-<p><B>Requirement</B>: 3,000 Stonehenge Circles
-<p><B>Cost</B>: 3.039 SxQig (3.039E171)
-<p><B>Effect</B>: Increase the production of all other buildings by 80% per Stonehenge Circle you own.
-<br/>
-<br/>
-<p><b>E410</b> - <b>For</b> Dwarf - <B>Research Name</B> Marketing
-<p><B>Requirement</B>: 5000 Blacksmiths
-<p><B>Cost</B>: 787.7 QaSxg (7.877E197)
-<p><B>Effect</B>: Increase production of all other buildings by 85% per Dwarven Forge you own.
-<br/>
-<br/>
-<p><b>E480</b> - <b>For</b> Any - <B>Research Name</B> Undercutting
-<p><B>Requirement</B>: 15000 max mana
-<p><B>Cost</B>: 1.67 NoSxg (1.67E210)
-<p><B>Effect</B>: Increase the production of non-unique buildings based on total spells cast.
-<p><B>Formula</B>: 0.65 * x^0.65%, where x is your Spells Cast (Total) stat.
-<p><b>----------------------------------</b></p>
-<p><b>Alchemy</b>
-<p><b>A10</b> - <b>For</b> Goblin - <B>Research Name</B> Transmutation</p>
-<p><B>Requirement</B>: 2250 Goblin Banks
-<p><B>Cost</B>: 28.83 Uqag (2.883E127)
-<p><B>Effect</B>: Increase the production of all other buildings by 85% per Goblin Bank you own.</p>
-<br/>
-<br/>
-<p><b>A410</b> - <b>For</b> Drow - <B>Research Name</B> Poisons
-<p><B>Requirement</B>: 4000 Sanctuaries
-<p><B>Cost</B>: 787.7 QaSxg (7.877E197)
-<p><B>Effect</B>: Increase the production of all other buildings by 95% per Spider Sanctuary you own.
-<br/>
-<br/>
-<p><b>A1200</B>: - <b>For</b>  Neutral,Dragon - <B>Research Name</B> Melting
-<p><B>Requirement</B>: 5000 Iron Stronghold, Affiliated Dragon
-<p><B>Requirement</B>: Post Ascension Only
-<p><B>Effect</B>: Increase the production of all other buildings per Iron Strongholds owned
-<p><B>Formula</B>: 0.35 * x, where x is iron strongholds owned
-<p><b>----------------------------------</b></p>
+<p><b>E480</b> - <b>For</b>: All Factions - <b>Research Name</b>: Undercutting 
+<p><b>Requirement</b>: 15000 max mana 
+<p><b>Cost</b>: 1.67 NoSxg (1.67E210) 
+<p><b>Effect</b>: Increase the production of non-unique buildings based on total spells cast. 
+<p><b>Formula</b>: 0.65 * x^0.65%, where x is your Spells Cast (Total) stat.
+<p><b>----------------------------------</b>
 <p><b>Warfare</b>
-<p><b>W10</b> - <b>For</b> Elf - <B>Research Name</B> Dueling</p>
-<p><B>Requirement</B>: 2000 Warrior Barracks
-<p><B>Cost</B>: 28.83 Uqag (2.883E127)
-<p><B>Effect</B>: Increase the production of all other buildings by 90% per Elven Training Grounds you own.</p>
+<p><b>W120</b> - <b>For</b>: All Factions - <b>Research Name</b>: Betrayal 
+<p><b>Requirement</b>: 250 base assistants 
+<p><b>Cost</b>: 676 Spqag (6.76E146) 
+<p><b>Effect</b>: Increase the production of Non-Unique buildings based on the amount of assistants you own. 
+<p><b>Formula</b>: round(1.5 * x^1.5)%, where x is the amount of assistants you own.
 <br/>
 <br/>
-<p><b>W120</b> - <b>For</b> Any - <B>Research Name</B> Betrayal
-<p><B>Requirement</B>: 250 base assistants
-<p><B>Cost</B>: 676 Spqag (6.76E146)
-<p><B>Effect</B>: Increase the production of non-unique buildings based on the amount of assistants you own.
-<p><B>Formula</B>: round(1.5 * x^1.5)%, where x is the amount of assistants you own.
-<br/>
-<br/>
-<p><b>W260</b> - <b>For</b> Titan - <B>Research Name</B> Athletics
-<p><B>Requirement</B>: 3,000 Olympian Halls
-<p><B>Cost</B>: 3.039 SxQig (3.039E171)
-<p><B>Effect</B>: Increase the production of all other buildings by 100% per Olympian Hall you own.
-<br/>
-<br/>
-<p><b>W350</b> - <b>For</b> Druid - <B>Research Name</B> Survival
-<p><B>Requirement</B>: Research Creeping (A300) & Synthesis (A270).
-<p><B>Cost</B>: 21.42 USxg (2.142E187)
-<p><B>Effect</B>: Increase the production of all other buildings based on the production of your Stonehenge Circles.
-<p><B>Formula</B>: round(50 * log^1.3(1 + x))%, where x is the production of your Stonehenge Circles.
-<br/>
-<br/>
-<p><b>W1275</b> - <b>For</b> Any - <B>Research Name</B> Authority
-<p><B>Requirement</B>: 43200 Call to Arms Cast Time in a single game.
-<p><B>Requirement</B>: Post Ascension Only
-<p><B>Effect</B>: Increase the production of Non-Unique buildings based on the percentage of trophies you unlocked.
-<p><B>Formula</B>: 0.2 * x^2, where x is percent of trophies unlocked
+<p><b>W1275</b> - <b>For</b>: Any - <b>Research Name</b>: Authority
+<p><b>Requirement</b>: 43200s (12 hours) Call to Arms Cast Time (This game).
+<p><b>Cost</b>: 555.1 Dvg (5.551E71)
+<p><b>Effect</b>: Increase the production of Non-Unique buildings based on the percentage of trophies you unlocked.
+<p><b>Formula</b>: 0.2 * x^2, where x is percent of trophies unlocked.
 <?php include "../scripts/footer.html"; ?>
