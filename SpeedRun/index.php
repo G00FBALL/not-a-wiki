6b53172332fe57ea95558a9fde90621844c94788
G00FY <G00FBALL@users.noreply.github.com>
2017-11-13 07:01:37 -0500

    updated Speedrun

--- SpeedRun/index.php
+++ SpeedRun/index.php
@@ -1,31 +1,23 @@
-<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN"
-"http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
-<html xmlns="http://www.w3.org/1999/xhtml" lang="en" xml:lang="en">
-<head>
-<title>Speed Run Guide</title>
-<?php include "../scripts/header.html"; ?>
-<h3>Speed Run</h3>
-<div id="myimage22"></div>
-<p><b>Speed Run Trophy Requirement</b></p>
-<p>Reach 1 M (1E6) coins in less than 5 minutes, without using Gem Power. (Can be done at R0)
-<p><b>Step by Step Guide</b></p>
-<p> 1.Abdicate</p>
-<p> 2.Buy 8 Farms</p>
-<p> 3.Buy Crop Rotation</p>
-<p> 4.Buy your 16th Farm</p>
-<p> 5.Buy an Inn</p>
-<p> 6.Buy up to your 18th Farm and 3rd Inn</p>
-<p> 7.Buy a Blacksmith</p>
-<p> 8.19th Farm, 4th Inn, 2nd Blacksmith</p>
-<p> 9.25th Farm</p>
-<p>10.Irrigation</p>
-<p>11.30th Farm, 7th Inn, 5th Blacksmith</p>
-<p>12.Proof of Evil Deed</p>
-<p>13.6 Slave Pens and 6 Orcish Arenas</p>
-<p>14.Cast Blood Frenzy</p>
-<p>15.Buy a Witch Conclave</p>
-<p>16.9th Slave Pen, 9the Orcish Arena, 4th Witch Conclave</p>
-<p>17.Cast Tax Collection</p>
-<p>18.Buy a Dark Temple</p>
-<p>19.Cast Tax Collection</p>
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN"
+"http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml" lang="en" xml:lang="en">
+<title>Speed Run Guide</title>
+<?php include "../scripts/header.html"; ?>
+<h3>Speed Run</h3>
+<p><b>Speed Run Trophy Requirement</b></p>
+<p>Reach 1 M (1E6) coins in less than 5 minutes, without using Gem Power. (Can be done at R0)
+<p><b>Step by Step Guide</b></p>
+<p><b>Bye all upgrades as they become available</b></p>
+<p><b>1</b>. Abdicate</p>
+<p><b>Buy all upgrades as they become available</b></p>
+<p><b>2</b>. Buy 25 Farms</p>
+<p><b>3</b>. Buy 15 Inns</p>
+<p><b>4</b>. Buy 15 Blacksmiths</p>
+<p><b>Bye all upgrades as they become available</b></p>
+<p><b>5</b>. Buy Proof of Evil Deed</p>
+<p><b>6</b>. Buy 6 Slave Pens and 6 Orcish Arenas</p>
+<p><b>Bye all upgrades as they become available</b></p>
+<p><b>7</b>. Cast Blood Frenzy and Call to Arms</p>
+<p><b>8</b>. Buy 6 Witch Conclaves</p>
+<p><b>9</b>. Buy 1 Dark Temple</p>
 <?php include "../scripts/footer.html"; ?>
