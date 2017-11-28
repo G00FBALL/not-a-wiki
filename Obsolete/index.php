9aef816f8f5dddc4ffc6670b740403bc10486d0d
G00FY <G00FBALL@users.noreply.github.com>
2017-11-27 21:28:28 -0500

    added obsolete builds

--- Obsolete/index.php
+++ Obsolete/index.php
@@ -2706,49 +2706,6 @@
                 </div>
             </div>
             <div class="shelementwhole">
-                <p onclick="shohid($(this));"><b><a href="#" onclick="return false;">NU Angel R40+</a></b></p>
-                <div class="autohide">
-                    <p>Medium length grower build.
-                    <p><b>Range</b>: to 1 Sp (E24) gems
-                    <p><b>Bloodline</b>: Druid
-                    <p><b>Spellcraft</b>: 10, 105/400, 150, 330/460, 500
-                    <p><b>Craftsmanship</b>: 25, 80, 105, 135, 150, 330, 460
-                    <p><b>Divine</b>: 55, 150, 200, 250/175, 290/480, 320
-                    <p><b>Economics</b>: 145, 330/30, 400, 410, 200/480
-                    <p><b>Alchemy</b>: 50, 105, 120, 150, 250
-                    <p><b>Warfare</b>: 120, 150, 320, 330, 400
-                    <p><b>Notes</b>:
-                    <p>Autocast God's Hand and Tax Collection for buildup. Add Diamond Pickaxe for S460. A long income phase following a shorter buildup is recommended.
-                    <p><b>12+ hours</b>: D175 E480.
-                    <p><b>24+ hours</b>: S400 S460 D175 E30 E480.
-                    <p>S400: Take S400 if it provides more than 2500 max mana.
-                    <p>S460: S460 growth varies inversely with spell duration. It takes 18 hours to catch up to S330 if God's Hand duration is 40 seconds. 9 hours if God's Hand duration is 20 seconds.
-                    <p>E480: E480 should be higher than E200 by 150,000 total spells cast.
-                    <p>D480: Use D480 if your time between the 3 alignments will be relatively balanced during the run.
-                    <p>C460 requires clicks to grow (it can grow past C120 quickly.) Take C120 and C500 as alternatives to C460 and C135 if you don't want to click.</p>
-                </div>
-            </div>
-            <div class="shelementwhole">
-                <p onclick="shohid($(this));"><b><a href="#" onclick="return false;">NU Druid R43+</a></b></p>
-                <div class="autohide">
-                    <p><b>Description</b>: This build relies on previously buffing time spent as evil, spells cast, and possibly mana produced. Very high production following a run for the Archwizard trophy.
-                    <p><b>Requires</b>: Several days spent as evil. Drow challenge 5 completed. Best with 10 billion mana produced.
-                    <p><b>Buildup runs</b>: The Works by Deworx or Ascended Goblins (Druid bloodline) in this section.
-                    <p><b>Bloodline</b>: Demon
-                    <p><b>Upgrades</b>:
-                    <p><b>Spellcraft</b>: 105, 150, 330, 400, 500/200
-                    <p><b>Craftsmanship</b>: 135/105, 150, 330, 340, 460
-                    <p><b>Divine</b>: 55, 150/245, 200, 275, 320
-                    <p><b>Economics</b>: 135, 145, 200, 260, 330, 480
-                    <p><b>Alchemy</b>: 105, 120, 150, 270/305/375, 330, 400
-                    <p><b>Warfare</b>: 120, 150, 320, 330, 350
-                    <p><b>Notes</b>:
-                    <p>Upgrade selections assume E480 is high and S400 is higher than S500.
-                    <p>D150 and A270 are recommended. Only take A305 if C105 and D150 are not taken. A375 can replace A270 if S400 is less than 7000 (7000 as a crossover point is a guess.)
-                    <p>Taking the time for a buildup run is not recommended before R43.
-                </div>
-            </div>
-            <div class="shelementwhole">
                 <p onclick="shohid($(this));"><b><a href="#" onclick="return false;">The Sorcerer's Apprentice R34+</a></b></p>
                 <div class="autohide">
                     <p>Most of the Archwizard builds out there were made before Prestige challenges were introduced. This one has been adjusted to work with the challenges. I started with "The Works" build and replaced some upgrades with production upgrades (to get more Goblin Banks) and added C250. My results were quite good - I finished my Archwizard run in less than 9 days, 6 hours. I started this at R39 with a gem count of 1.876e148. I had around a 4k max mana bonus to start with on S400. I do not have any of the gifts and did not use timewarps. I did scry a little (maybe a couple dozen times over the course of the run), but I'm not sure whether that would make a noticeable difference in the length of the run. I stopped casting combo strike when my mana regen exceeded my max mana. I think I might have been able to get away with fewer production upgrades, since I got to the required number of Goblin Banks in 12 minutes.
@@ -2815,6 +2772,9 @@
                     <p>Edited by non-author: Fixed range, and information on build.</p>
                 </div>
             </div>
+        </div>
+       <p onclick="shohid($(this));"><b> <a href="#" onclick="return false;">R40-R46</a></b></p>
+        <div class="category autohide">
             <div class="shelementwhole">
                 <p onclick="shohid($(this));"><b><a href="#" onclick="return false;">The Works Ascension+</a></b></p>
                 <div class="autohide">
@@ -2837,6 +2797,180 @@
                     <p><b>Alchemy</b>: : <b>120</b>, <b>270</b>, 250, <b>400</b>, [Drow: 410] [Optional 250 -> <u>305</u> and 120 -> 150]
                     <p><b>Warfare</b>: : 25, 150, <b>205</b>, <b>520</b>, 525 [Dark Light of Bondelnar <u>400</u>, <b>135</b>]
                     <p>Edited by Deworx (Added Post Ascension, improved the Build)</p>
+                </div>
+            </div>
+            <div class="shelementwhole">
+                <p onclick="shohid($(this));"><b><a href="#" onclick="return false;">NU Angel R40+</a></b></p>
+                <div class="autohide">
+                    <p>Medium length grower build.
+                    <p><b>Range</b>: to 1 Sp (E24) gems
+                    <p><b>Bloodline</b>: Druid
+                    <p><b>Spellcraft</b>: 10, 105/400, 150, 330/460, 500
+                    <p><b>Craftsmanship</b>: 25, 80, 105, 135, 150, 330, 460
+                    <p><b>Divine</b>: 55, 150, 200, 250/175, 290/480, 320
+                    <p><b>Economics</b>: 145, 330/30, 400, 410, 200/480
+                    <p><b>Alchemy</b>: 50, 105, 120, 150, 250
+                    <p><b>Warfare</b>: 120, 150, 320, 330, 400
+                    <p><b>Notes</b>:
+                    <p>Autocast God's Hand and Tax Collection for buildup. Add Diamond Pickaxe for S460. A long income phase following a shorter buildup is recommended.
+                    <p><b>12+ hours</b>: D175 E480.
+                    <p><b>24+ hours</b>: S400 S460 D175 E30 E480.
+                    <p>S400: Take S400 if it provides more than 2500 max mana.
+                    <p>S460: S460 growth varies inversely with spell duration. It takes 18 hours to catch up to S330 if God's Hand duration is 40 seconds. 9 hours if God's Hand duration is 20 seconds.
+                    <p>E480: E480 should be higher than E200 by 150,000 total spells cast.
+                    <p>D480: Use D480 if your time between the 3 alignments will be relatively balanced during the run.
+                    <p>C460 requires clicks to grow (it can grow past C120 quickly.) Take C120 and C500 as alternatives to C460 and C135 if you don't want to click.</p>
+                </div>
+            </div>
+        <div class="shelementwhole">
+            <p onclick="shohid($(this));"><b><a href="#" onclick="return false;">Titan (Rush) <font color="DarkGoldenRod"> Neutral</font></a></b></p>
+            <div class="autohide">
+                <p><b>Author</b>: Artcrow
+                <p><b>Range</b>: 0 - ?
+                <p><b>Faction</b>: Titan
+                <p><b>Bloodline</b>: Druid
+                <p>S135,S150,S270,S330,S500,
+                <p>C150,C251,C330,C340,C375,C400,
+                <p>D55,D200,D245,D290,D320,
+                <p>E135,E145,E275,E330,E400,
+                <p>A105,A120,A250,A330,A400,
+                <p>W150,W180,W290,W320,W330,W400
+                <p><b>Notes</b>: Requires Spell tiers to get past 1e24 (1 Sp) gems. Recommended doing T3 titan spell tiers (Call to Arms, Gem Grinder, Lightning Strike, Spiritual Surge).
+            </div>
+        </div>
+            <div class="shelementwhole">
+                <p onclick="shohid($(this));"><b><a href="#" onclick="return false;">NU Druid R43+</a></b></p>
+                <div class="autohide">
+                    <p><b>Description</b>: This build relies on previously buffing time spent as evil, spells cast, and possibly mana produced. Very high production following a run for the Archwizard trophy.
+                    <p><b>Requires</b>: Several days spent as evil. Drow challenge 5 completed. Best with 10 billion mana produced.
+                    <p><b>Buildup runs</b>: The Works by Deworx or Ascended Goblins (Druid bloodline) in this section.
+                    <p><b>Bloodline</b>: Demon
+                    <p><b>Upgrades</b>:
+                    <p><b>Spellcraft</b>: 105, 150, 330, 400, 500/200
+                    <p><b>Craftsmanship</b>: 135/105, 150, 330, 340, 460
+                    <p><b>Divine</b>: 55, 150/245, 200, 275, 320
+                    <p><b>Economics</b>: 135, 145, 200, 260, 330, 480
+                    <p><b>Alchemy</b>: 105, 120, 150, 270/305/375, 330, 400
+                    <p><b>Warfare</b>: 120, 150, 320, 330, 350
+                    <p><b>Notes</b>:
+                    <p>Upgrade selections assume E480 is high and S400 is higher than S500.
+                    <p>D150 and A270 are recommended. Only take A305 if C105 and D150 are not taken. A375 can replace A270 if S400 is less than 7000 (7000 as a crossover point is a guess.)
+                    <p>Taking the time for a buildup run is not recommended before R43.
+                </div>
+            </div>
+        <div class="shelementwhole">
+                    <p onclick="shohid($(this));"><b><a href="#" onclick="return false;"> Titan (GG2/LS2)<font color="DarkGoldenRod"> Neutral</font></a></b></p>
+                                <div class="autohide">
+                <p><b>Author</b>: Artcrow
+                <p><b>Range</b>: ? gems - ?
+                <p><b>Faction</b>: Titan
+                <p><b>Bloodline</b>: Druid
+                <p>S150,S250,S270,S330,S500,
+                <p>C150,C251,C330,C340,C375,C400,
+                <p>D55,D200,D290,D320,D350,
+                <p>E135,E145,E275,E400,E460,
+                <p>A105,A120,A250,A330,A400,
+                <p>W150,W180,W290,W320,W330,W400
+                <p><b>Notes</b>: Swap A330 with A150 if 1 hour run or more.
+                </div>
+        </div>
+        <div class="shelementwhole">
+                    <p onclick="shohid($(this));"><b><a href="#" onclick="return false;"> Titan (SS2) (Stage 4 Unlock)<font color="DarkGoldenRod"> Neutral</font></a></b></p>
+                                <div class="autohide">
+                <p><b>Author</b>: Artcrow
+                <p><b>Range</b>: 1e27 (1 Oc) gems - ?
+                <p><b>Faction</b>: Titan
+                <p><b>Bloodline</b>: Druid
+                <p>S150,S215,S250,S330,S500,
+                <p>C150,C251,C330,C340,C375,C400,
+                <p>D55,D200,D245,D290,D320,
+                <p>E135,E145,E275,E320,E460,
+                <p>A105,A120,A150,A250,A400,
+                <p>W150,W180,W290,W320,W330,W400
+                <p><b>Notes</b>: Requires 6 hours to unlock SS2.
+                <p><b>Notes</b>: Swap S500 with S400 if higher.
+                <p><b>Notes</b>: Swap E460 with E30 if Run if 12 hours run or more.
+                <p><b>Notes</b>: Also used for stage 4 unlock at e3? (?) gems (requires using S400 with at least 14k bonus, estimated run time ? hours)
+                </div>
+        </div>
+        <div class="shelementwhole">
+                    <p onclick="shohid($(this));"><b><a href="#" onclick="return false;"> Spider Sanctuary<font color="DarkRed"> Evil</font></a></b></p>
+                                <div class="autohide">
+                <p><b>Author</b>: Artcrow
+                <p><b>Range</b>: 1e30 (1 No) -? Gems
+                <p><b>Faction</b>: Goblin/Drow
+                <p><b>Bloodline</b>: Druid
+                <p>S50,S215,S305,S330,S400,
+                <p>C120,C135,C150,C340,C400,
+                <p>D55,D200,D245,D320,D400,
+                <p>E30,E80,E135,E145,E225,E400,
+                <p>A10,A105,A120,A150,A400,
+                <p>W25,W180,W205,W400,W405,W520,W525
+                <p><b>Notes</b>: Replace C135 with C200 if stronger.
+                <p><b>Notes</b>: Run for 3 days to unlock T3 spells.
+                </div>
+        </div>
+        <div class="shelementwhole">
+                    <p onclick="shohid($(this));"><b><a href="#" onclick="return false;"> Goblin Banks (Stage 4 Unlock?)<font color="DarkRed"> Evil</font></a></b></p>
+                                <div class="autohide">
+                <p><b>Author</b>: Artcrow
+                <p><b>Requirements</b>: T3 Goblin/Drow spells
+                <p><b>Range</b>: 1e30 (1 No) -? Gems
+                <p><b>Faction</b>: Goblin/Drow
+                <p><b>Bloodline</b>: Druid
+                <p>S50,S215,S305,S330,S400,
+                <p>C150,C175,C340,C400,C500,
+                <p>D55,D200,D245,D250,D320,
+                <p>E1,E80,E135,E145,E225,E330,
+                <p>A105,A120,A150,A400,A410,
+                <p>W25,W150,W180,W205,W400,W520,W525
+                <p><b>Notes</b>: Requires 1.5-2 days to set up.
+         </div>
+    </div>
+        <div class="shelementwhole">
+            <p onclick="shohid($(this));"><b><a href="#" onclick="return false;"> Dragon Stage 2 + S400 buff<font color="Blue"> Good</font></a></b></p>
+            <div class="autohide">
+                <p><b>Author</b>: Artcrow
+                <p><b>Range</b>: ? gems
+                <p><b>Faction</b>: Good Mercenary
+                <p><b>Bloodline</b>: Druid
+                <p>FR1,FR7,FR9,EL3,AN3,AN5,AN6,GB7,DD6,FC5,FC7,DN2,
+                <p>SP:Fairy Chanting,SP:Hellfire Blast
+                <p><b>Notes</b>: 20 million spells cast in 12 hours. S400=12500.
+                <p><b>Notes</b>: 40 million in 20 hours. S400=16000.
+                <p><b>Notes</b>: 55 milion in 24 hours. S400=18000.
+        </div>
+        </div>
+        <div class="shelementwhole">
+            <p onclick="shohid($(this));"><b><a href="#" onclick="return false;"> Dragon Stage 3 <font color="Blue">Good</font></a></b></p>
+            <div class="autohide">
+                <p><b>Author</b>: Artcrow
+                <p><b>Range</b>: e31 (10 No) gems+
+                <p><b>Faction</b>: Fairy/Dwarf
+                <p><b>Bloodline</b>: Druid
+                <p>S180,S250,S330,S400,S545,S590,
+                <p>C10,C80,C150,C330,C340,C400,C405,
+                <p>D55,D200,D245,D290,D320,
+                <p>E50,E135,E145,E330,E400,
+                <p>A30,A105,A120,A150,A250,
+                <p>W150,W180,W290,W320,W400
+                <p><b>Notes</b>: Estimated run time 1 day+. Can be tried at e30 gems but may take over 3 days.
+            </div>
+        </div>
+        <div class="shelementwhole">
+            <p onclick="shohid($(this));"><b><a href="#" onclick="return false;"> Dragon Stage 4 <font color="DarkGoldenRod">Neutral</font></a></b></p>
+            <div class="autohide">
+                <p><b>Author</b>: Artcrow
+                <p><b>Range</b>: 1e31+?
+                <p><b>Requirement</b>: S400 with at least 14k (Estimated run time 10 hours)
+                <p><b>Faction</b>: Titan
+                <p><b>Bloodline</b>: Druid
+                <p>S150,S250,S270,S330,S400,
+                <p>C150,C251,C330,C340,C375,C400,
+                <p>D55,D200,D290,D320,D350,
+                <p>E135,E145,E275,E400,E460,
+                <p>A105,A120,A250,A330,A400,
+                <p>W150,W180,W290,W320,W330,W400
                 </div>
             </div>
         </div>
