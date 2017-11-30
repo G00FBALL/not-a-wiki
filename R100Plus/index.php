585aef8aeb4ea14ce1eca29a2462ac3f4ee8938e
G00FY <G00FBALL@users.noreply.github.com>
2017-11-30 06:54:18 -0500

    added link

--- R100Plus/index.php
+++ R100Plus/index.php
@@ -78,6 +78,7 @@
             <p onclick="shohid($(this));"><b> <a href="#" onclick="return false;">R110+ e90+ Gems</a></b></p>
             <div class="autohide">
     <p>All builds come from the <a target="_blank" href="https://www.kongregate.com/forums/8945-realm-grinder/topics/922600-3-0-build-megathread-will-be-updated-to-3-0-2-on-sunday?page=14#posts-11718140"><b>here</b></a></p>
+    <p>More <a target="_blank" href="https://www.kongregate.com/forums/8945-realm-grinder/topics/922600-3-0-build-megathread-updated-to-3-0-2?page=16#posts-11746797"><b>info</b></a> on these builds</p>
                     <p>I tested a bunch of high gem A2 builds with buffs. The tests should be reasonably accurate. I took several shortcuts and all the builds have less Tax Collections cast and Faction coins gained than they would in actual runs.
                     <br/><br/>
                     <p><b>R111</b>
