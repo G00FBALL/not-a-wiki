d5a6b6f190bf791c7e8e011c43b25925ca218563
G00FY <G00FBALL@users.noreply.github.com>
2017-12-07 16:29:54 -0500

    edit speedrun info

--- SpeedRun/index.php
+++ SpeedRun/index.php
@@ -5,18 +5,22 @@
 <?php include "../scripts/header.html"; ?>
 <h3>Speed Run</h3>
 <p><b>Speed Run Trophy Requirement</b></p>
-<p>Reach 1 M (1E6) coins in less than 5 minutes, without using Gem Power. (Can be done at R0)
+<p>Reach 1 M (1E6) coins in less than 5 minutes, without using Gem Power. (<b>Should be done at R0</b>)
 <p><b>Step by Step Guide</b></p>
 <p><b>Bye all upgrades as they become available</b></p>
+<p><b>Do Not Buy Gem Power</b></p>
 <p><b>1</b>. Abdicate</p>
 <p><b>Buy all upgrades as they become available</b></p>
+<p><b>Do Not Buy Gem Power</b></p>
 <p><b>2</b>. Buy 25 Farms</p>
 <p><b>3</b>. Buy 15 Inns</p>
 <p><b>4</b>. Buy 15 Blacksmiths</p>
 <p><b>Bye all upgrades as they become available</b></p>
+<p><b>Do Not Buy Gem Power</b></p>
 <p><b>5</b>. Buy Proof of Evil Deed</p>
 <p><b>6</b>. Buy 6 Slave Pens and 6 Orcish Arenas</p>
 <p><b>Bye all upgrades as they become available</b></p>
+<p><b>Do Not Buy Gem Power</b></p>
 <p><b>7</b>. Cast Blood Frenzy and Call to Arms</p>
 <p><b>8</b>. Buy 6 Witch Conclaves</p>
 <p><b>9</b>. Buy 1 Dark Temple</p>
