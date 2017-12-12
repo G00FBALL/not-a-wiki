8e141b46f28810916f96d0272fa0416ad3fa2d57
G00FY <G00FBALL@users.noreply.github.com>
2017-12-12 18:24:39 -0500

    Update index.php

--- index.php
+++ index.php
@@ -2,8 +2,8 @@
 "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml" lang="en" xml:lang="en">
 <head>
-<a id="rightbox " href="http://discord.gg/realmgrinder"></a>
 <?php include "musicfamily.org/../scripts/header.html"; ?>
+<a id="rightbox " href="http://discord.gg/realmgrinder"></a>
 <p>Welcome to <b>G00F's not a wiki</b> Realm Grinder <font face="Comic sans MS" size="3">"G00FBALL"</font> Style without all the wiki adverts. 
 <p><b><a target="_blank" href="http://discord.gg/realmgrinder">Realm Grinder Chat on Discord</a></b></p> 
 <p>If you have any suggestions or find any errors send a message to <a target="_blank" href="http://www.kongregate.com/accounts/Igorogi/"><b>Igorogi</b></a> or <a target="_blank" href="http://www.kongregate.com/accounts/G00FBALL/"><b>G00FBALL</b></a> and please include links to where you got the information.</p>
