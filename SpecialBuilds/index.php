8be2b66966626ea772accd57ae733f85a4dc9cd4
Necrodoom <doomed.war@gmail.com>
2017-12-17 13:47:31 +0200

    Theres no reason this build lacks a FC generator

--- SpecialBuilds/index.php
+++ SpecialBuilds/index.php
@@ -23,10 +23,10 @@
                 <p><b>Bloodline</b>: Druid (<b>Ignore if you don't have Bloodline</b>)</p>
                 <p><b>Template</b></p>
                 <p>FR4,EL3,AN5,GB1,UD7,DM8,TT6,DD1,FC7,DN2,DW4,GB7,</p>
-                <p>SP:Fairy Chanting,SP:God's Hand</p>
+                <p>SP:Goblin's Greed,SP:Fairy Chanting</p>
                 <p><b>Note</b>: This build is for getting <b>Coin Pillage</b>,<b>Premeditation</b> and <b>Harlequin</b> at R3 and <b>KYE</b> at R12+</p>
                 <p><b>Coin Pillage</b>: After the build is set up put Tax Collection on Silver auto casting and done.</p>
-                <p><b>Spells</b>: God's Hand, Fairy Chanting</p>
+                <p><b>Spells</b>: Goblin's Greed, Fairy Chanting</p>
                 <p><b>Tier 1 Upgrades</b></p>
                 <p><b>Goblin</b>: (1,1) Strong Currency</p>
                 <p><b>Elf</b>: (1,3) Sylvan Treasure Frills</p>
