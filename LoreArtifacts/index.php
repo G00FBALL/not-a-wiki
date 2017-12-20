8719f934a494d3f638f9e34fe9ec5a11961170ce
G00FY <G00FBALL@users.noreply.github.com>
2017-12-19 20:00:07 -0500

    spelling errors

--- LoreArtifacts/index.php
+++ LoreArtifacts/index.php
@@ -293,13 +293,13 @@
     <p><b>Formula</b>: 0.065 * x^  0.65 where x is amount of time spent as non-mercenary factions (in seconds).
     <p><b>Secret Upgrade Cost</b>: 100 Noqag (1e152)
     <p><b>----------------------------------</b></p>
-    <p><img src="http://musicfamily.org/realm/Factions/picks/VeteranFigurineArtifact.png" alt="Veteran Figureine" align="middle"><b> Veteran Figureine</b></p>
+    <p><img src="http://musicfamily.org/realm/Factions/picks/VeteranFigurineArtifact.png" alt="Veteran Figureine" align="middle"><b> Veteran Figurine</b></p>
     <p><b>Description</b>: The warrior of a thousand battles, ultimate champion of the Realms.
     <p><b>Requirement</b>: R90+
     <p><b>Chance</b>: (x/1,000,000)%, where x is seconds this game
     <p><b>Effect</b>: Passive effect: allows Mercenaries to benefit from all challenges.
     <p><b>----------------------------------</b></p>
-    <p><img src="http://musicfamily.org/realm/Factions/picks/WallChunckArtifact.png" alt="Wall Chunck" align="middle"><b> Wall Chunck</b></p>
+    <p><img src="http://musicfamily.org/realm/Factions/picks/WallChunckArtifact.png" alt="Wall Chunck" align="middle"><b> Wall Chunk</b></p>
     <p><b>Description</b>: A bigger piece of the infamous Ascension Wall.
     <p><b>Requirements</b>: R100+
     <p><b>Effect</b>: Increase the production of all buildings based on their tier.
