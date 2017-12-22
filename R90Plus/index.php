4c54725150d90eca9a854851ff626e9e33373c23
Necrodoom <doomed.war@gmail.com>
2017-12-22 21:58:42 +0200

    No longer obsolete

--- R90Plus/index.php
+++ R90Plus/index.php
@@ -100,7 +100,7 @@
          </div>
     </div>
         <div class="shelementwhole">
-            <p onclick="shohid($(this));"><b> <a href="#" onclick="return false;">Terror From the Deep (Obsolete?)<font color="DarkRed"> Evil</font></a></b></p>
+            <p onclick="shohid($(this));"><b> <a href="#" onclick="return false;">Terror From the Deep<font color="DarkRed"> Evil</font></a></b></p>
             <div class="autohide">
                 <p><b>Author</b>: Lapare
                 <p><b>Requirements</b>: Veteran Figurine, 51.8B mana generated this R (for GB11)
