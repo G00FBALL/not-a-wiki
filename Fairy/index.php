8f2c3227c339066342acec6ece9c29543cf5f575
Necrodoom <doomed.war@gmail.com>
2018-01-01 11:13:45 +0200

    fix fairy 3 formula

--- Fairy/index.php
+++ Fairy/index.php
@@ -31,7 +31,7 @@
 <p><b>Requirements</b>: 18 Reincarnations and Fairy Unique Building
 <p><b>Challenge</b>: Fairy (3,3) Swarm of Fairies gets 60 Assistants in under a minute of a new game.(Abdication or Reincarnation)</p>
 <p><b>Effect</b>: Increase mana regeneration based on the number of buildings you own.
-<p><b>Formula</b>: (0.1x)^2.25, where x is your Buildings Owned (This Game) stat.</p>
+<p><b>Formula</b>: (0.1 * x) ^ 0.225, where x is your Buildings Owned (This Game) stat.</p>
 <p><b>Upgrade</b>: Good for All</h3>
 <p><b>Tips</b>:
 <p>(8190, total, of Farms, Inns and Blacksmiths and you don't have to buy the upgrade itself.)</p>
