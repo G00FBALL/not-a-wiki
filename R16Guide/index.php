f1ce31bbc0925786cf3acfbe3d373526841a083f
Namelessname9 <saarlz104@gmail.com>
2018-01-12 05:40:02 +0200

    New R16 guide
    Swapped the old one with my new one, currently contains all text from the post because it won the community's with an "outstanding" 1-0.

--- R16Guide/index.php
+++ R16Guide/index.php
@@ -3,56 +3,47 @@
 <html xmlns="http://www.w3.org/1999/xhtml" lang="en" xml:lang="en">
 <?php include "../scripts/header.html"; ?>
 <h6>R16 Research Guide</h6>
-<p><b>To unlock Research follow the guide below, Do not buy Prestige Faction<p></b>
-<p>The original post can be found <a target="_blank" href="http://www.kongregate.com/forums/8945-realm-grinder/topics/562011"><b>here</b></a> by <a target="_blank" href="http://www.kongregate.com/accounts/why_amihere"><b> why_amihere</b></a>
-<p>This guide was written at v1.4.39. As of v1.5.42 there is an extra building upgrade tier, which makes the gem requirements both more relaxed and easier to reach. Therefore I would still recommend getting at least close to the lower end of the suggested gem ranges before trying to do things, but stop Mercs when you can’t double your gems in a 1h run.
+<p><b>To unlock Research follow the guide below, do not buy Prestige Faction<p></b>
+<p>The original post can be found <a target="_blank" href="https://www.kongregate.com/forums/8945-realm-grinder/topics/1264578"><b>here</b></a> by <b>Namelessname</b></a>
 <p>This guide is to help you make it through the Research phase of R16 reasonably quickly, without wasting time on upgrades that are too hard to get or take a really long time when they would be much faster later, and without getting lost amid the complexity of it all.
-<p>The original post will not include the list below as it was removed. 
-<p>I have left out all the text from the post, so you might want to look at it.
-<p>A complete list of Research Unlock Requirements can be found <a target="_blank" href="http://musicfamily.org/realm/Research/"> <b>here</b></a>
-<p>A complete list of All Unique Buildings Unlock Requirements can be found <a target="_blank" href="http://musicfamily.org/realm/UniqueBuilding/"> <b>here</b></a>
 <p><b>------------------------------<p></b>
-<p>You should have artifact <b>Vanilla Flavor Juice</b> before starting research
+<p><b>Notes before beginning to unlock research</b>
+<p> you should have the artifact <b>Vanilla Flavor Juice</b>, it is recommended to get it at the start of the R as it is easy to obtain and very powerful
 <p><b>Requirement</b>: +R16
 <p><b>Chance</b>: 20% in the first 5m of a game (Excavate)
 <p><b>Cost</b>: 1 coin
-<p><b>Alignment</b>: Vanilla faction
+<p><b>Alignment</b>: Vanilla faction<p>
+<p>Manage your excavations very carefully upon reaching R16, having excavations available would drastically reduce the time required to purchase new RPs.
+<p>Before trying to unlock Research, you should have 1e69/1Dvg gems, having more would allow for a bit faster and smoother unlock, but might be difficult to achieve without the right Sunforce bonus.
 <p><b>------------------------------<p></b>
-<p><b>RP = Research Points<p></b>
-<p><b>Note</b>: Get to e70-e71 gems before starting to unlock research.
-<p><b>Note</b>: Good Alignment, Elf/Elfline to Merc in under 10-20 minutes.
-<p>You can use <b>Mercenary</b> to get to the required gem level. (Elven Farms build suggested) 
-<p><b>Note</b>: If the upgrade is one of the key points of the run it will be listed outside of brackets/parentheses (). Design your build based on these.
-<p><b>Note</b>: If it is something you should get that run, if you don’t already have it, it will be in brackets: (Mxx)
-<p><b>Note</b>: If it is something you can probably unlock but not vital that run, it will be in square brackets: [Mxx]
-<p>1.1	Unlock the Fairy unique building and the Spellcraft research facility
-<p>1.2  Unlock the Elf unique building and the Craftmenship research facility
-<p>1.3  Unlock the Angel unique building and the Divine research facility
-<p>1.4	Unlock the Goblin unique building and the Economics research facility
-<p>1.5	Unlock the Undead unique building and the Alchemy research facility
-<p>1.6	Unlock the Demon unique building and the Warfare research facility
-<p>2.1	Get to RP55 Using Fairy with Fairy Bloodline
-<p>2.2	S30=4000 Mana (C10=2500 Farms) [A30=750 Fairy Chanting, This R]	Use Fairy with Fairy Bloodline
-<p>2.3	D50 (S10) [C25]	Use Angel with Goblin Bloodline
-<p>2.4	E30 E50 W25 (A10) Use Goblin with Titan Bloodline or Faceless Bloodline 
-<p>2.5	(D10) [A25] [W50] Use Demon with Druid Bloodline or Goblin  Bloodline if hard
-<p>2.6	(E10) Use Undead with Goblin Bloodline 
-<p>2.7	[W10] [E25] [C50] Use Elf with Elf Bloodline
-<p>2.8	[C25] [D55] [A55] Use Angel with Druid Bloodline long run, spam TCs
-<p>3.1	Get to RP80 Using Fairy with Fairy Bloodline get a30 and e50 first
-<p>3.2	C80 Use any Faction? with Goblin Bloodline
-<p>3.3	E80 Use Fairy with Fairy Bloodline or any?/Goblin Bloodline
-<p>4.1	Make A105 visible (RP105 in Alchemy only) Use Fairy with Fairy Bloodline 	
-<p>4.2	Unlock A105 Use Fairy with Goblin Bloodline (if not unlocked on previous run)
-<p>5.1	S50 A120 (A30) [W120] Use Fairy with Faceless Bloodline unlock
-<p>5.2	Get to RP135 Using Elf with Titan Bloodline spam MB, and maybe TC
-<p>6.1	S135 Use Fairy with Goblin Bloodline 	
-<p>6.2	(C105) (C120) (C135) Use Elf with Elf Bloodline
-<p>6.3	[A135] Use Undead with any Bloodline
-<p>6.4	W135 Use Demon with any Bloodline might need to use Mercs with Dwarf 1,2 and Gobline for each alignment to max buildings before this run
-<p>7.1	D25 A50 [D55] [A55] [W50] Use Undead with Druid Bloodline 8 hour minimum
-<p>7.2	D135 (D55) (A55) Use Angel with Druid Bloodline 
-<p>7.3	Earn gems with Elf faction and Elf Bloodline
-<p>7.4	E135 Goblin with Goblin Bloodline needs hours at e71 gems, pretty fast at e75 gems
-<p>7.5	(W120) (W50) (A25) Use Demon with Undead Bloodline 
+<p>So, you got to R16, you got a popup from the advisor, you got the gem requirement, now you are just waiting (im)patiently for me to explain what to do now, unfortunately, I will first explain what exactly are you going to unlock.
+<p>Firstly, <b>Unique Buildings<p></b>
+<p>commonly refered to as UBs, these are an upgrade to a certain building, which is determined by your current faction, which does 2 things, the first is a bonus to that building's production, which scales with a certain stat, and the second is giving you access to Research if you unlocked it for that faction.
+<p>In order to unlock a UB you need to have a certain amount of the building that is going to be upgraded and 1e100 coins, currently you can only unlock the ones for the Vanilla factions, but that will change as you progress through the game. (<a href=http://musicfamily.org/realm/UniqueBuilding>This page</a> has a list of the building requirements for all UBs, and the formulas for their upgrades)
+<p>After you unlocked it, you need to upgrade the building each abdication, this costs coins (1e103 for Vanilla and Neutral factions) and that means you won't be able to buy UBs at the start of the R. (at least for a while)
+<p>Then, once you have the UB for a faction unlocked, you still need to unlock Research for that faction, Vanilla factions skip this step but they do got something else that need to be done...
+<p>The Research tree has 6 branches, 1 for each Vanilla faction, to unlock these branches, you need to pay 1e6 FCs of that faction and 1e121 coins <b>after</b> you bought the UB for that faction, this only needs to be done once.
+<p>So, what I want you to do first is to unlock every branch in the tree, I recommend using a few excavations for Demon, Undead and Angel as it will speed things up by quite a lot, <b>however, be very careful with excavations as you will need quite a lot of them.</b>
+Also, I'd recommend using the following faction-bloodline combinations to unlock the branches: Elf-Elfline, Fairy-Elfline (Fairy's production is enough, but Elfline provides the FCs needed to avoid using excavs), Angel-Druidline, Undead/Demon/Goblin-Titanline. (Demon and Undead have virtually no FC gain nor production, so they need Titanline for production and excavations for FC, Goblin gets FC from their spell so they only need production)<p>
+<p>Alright, now that you unlocked all branches it is time for yet another explanation, this time about <b>Research Points (RPs) and researches</b>.<p>
+<p>Every branch has a level, that level is called RPs, to get more RPs/level it up, you need to pay FCs of the faction that is related to the branch and coins, depending on the level, RPs are retained through all soft resets, and you can only purchase up to a certain cap, which is increased each R, up to 5000 at R100 (-50 because the devs wanted it to be a round number).*ending note 1
+<p>At certain RP amounts for a branch, you will be able to unlock a research, these are refered to by their number and their branch's letter, for example E50=Economics 50.
+<p>Researches are like Merc upgrades, you have a certain amount of slots per branch, and you can pick which ones you want to fill those slots, there will be ways to increase the number of slots you have at each branch, but currently that isn't very important to you. (they also reset each abdication, and have templates, just like Merc)
+<p><img src-"https://i.imgur.com/BH7KYCK.png" width="505" height="461" align="left">The big blocks are where you buy RPs, and where you can see how many RPs and slots you available for that branch, the small blocks to their right are the researches, a red x means the research is unlocked but can't be used as your current faction, a yellow ? means you can't unlock the research as this faction, and a green V means you already chose this research for this abdication.
+<p>There is one more thing about researches, which can either be seen as what's fun about them, or, the opinion that I share, is their biggest problem, that is their requirements, or more specifically, the fact you can't see their requirements in-game, yes, you can see a progress indicator, but this game has so many different stats to track, you would spend more time trying to understand what are the requirements than you would planning a run to fulfill as many as possible at once...
+<p>However, unlike in the game, in <a href="http://musicfamily.org/realm/Researchtree/">this page</a> you can see the requirements for yourself and decide of a best course, but if you feel a bit overwhelmed, here is what I recommend to do:
+<p><b>Step 1:</b> Use Fairyline Fairy to get RPs, use excavs as needed, but be sure you will have enough to use later, the important researches are S1, C10(requires 2500 Farms/EFs), S30(requires 4000 max mana), once you got to the 50s, and mainly 50 in Economics, it's time for step 2. (remember to start a new run every 15 minutes or so to benefit from VFJ, that applies to the rest of the steps and generally whenever you are using a rush-term Vanilla faction until ascension)
+<p><b>Note:</b> If you get stuck, you might need to unlock A30, to get it afk for a few hours (and leave the device on) using an Evil Merc run with FC, HFB, and as much mana regen as possible, leaving FC and HFB on bronze autocast. *ending note 2
+<p><b>Step 2:</b> Use Druidline Goblin to get 1000 REs(Royal Exchanges) to unlock E50 and E30, leave the game for a while with GobGreed on bronze autocast, use a few excavations to finish if needed.
+<p><b>Step 3:</b> Use Fairyline Fairy to get to 80 RPs, with Craftsmanship prioritized, then unlock C80(16,000 Good buildings) using Gobline Fairy.
+<p><b>Step 4:</b> Use Fairyline Fairy (*sigh*) to get to 105 RPs, the important researches are S105(600 total RPs, will be gotten without trying), A105(2750 Blacksmiths, most likely the same, if not use Gobline Fairy), and C105(25000 clicks in a single abdication).
+<p><b>Note:</b> At this point you should get C105 ASAP, when you go afk just leave the game on Elfline Elves with MB on autocast.
+<p><b>Step 5:</b> Use Fairyline Fairy to get to 120 RPs, then get A120(6500 max mana) and W120(250 base assistants), both should be gotten without trying, then continue until you have all RPs and unlock all researches (except D25, A135 and W50 if you aren't close, if you haven't done A25 and A30 you can delay those too) and Reincarnate.
+<p>The next Rs should be a lot easier, with less researches to unlock and more to start with, if you delayed the researches mentioned as delayable in step 5, you should do these at R18 with all remaining Vanilla challanges.<p>
+<p><b>Ending notes</b><p>
+<p>RPs increase by the number of the R, so if R16 had a max of 136 RPs, R17 has 136+17=153, <a href="http://musicfamily.org/realm/Researchtree/>the research tree page</a> has the max number of RPs for all Rs between R16-R100, along with the total cost of all RPs you can buy at each R. (assuming you already have the max RPs for the previous R)
+<p>The Evil Merc run is intended to get A25(666 HFB casts), A30(750 FC casts), and gain time toward W50(2d evil playtime this R), however at R18 you do all Vanilla challanges, which would get you some evil playtime as well, so if you aren't planning on afking anyway this run isn't necessary, if you are planning to afk it might be better to do so at higher RPs as you'll eventually need to afk once/twice as Elves and once as Angels to unlock all useful researches.
+Also note that if you complete the requirement as Merc you will still need to be Fairy/Demon and access Research at the same R to unlock it or it won't count.
+If you are having trouble with the spell names check <a href-"http://musicfamily.org/realm/Terminology/">this</a>.
+<p>My last tip is to try to have fun while playing the game, if you're not, what's the point of playing?
 <?php include "../scripts/footer.html"; ?>
