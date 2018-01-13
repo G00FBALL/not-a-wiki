3f38dcdf2c255a60d02f5cc7e7a3bfb851d83bdb
G00FY <G00FBALL@users.noreply.github.com>
2018-01-13 18:24:15 -0500

    adding old guide

---
+++ OldR16Guide/index.php
@@ -0,0 +1,58 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN"
+"http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml" lang="en" xml:lang="en">
+<?php include "../scripts/header.html"; ?>
+<h6>R16 Research Guide</h6>
+<p><b>To unlock Research follow the guide below, Do not buy Prestige Faction<p></b>
+<p>The original post can be found <a target="_blank" href="http://www.kongregate.com/forums/8945-realm-grinder/topics/562011"><b>here</b></a> by <a target="_blank" href="http://www.kongregate.com/accounts/why_amihere"><b> why_amihere</b></a>
+<p>This guide was written at v1.4.39. As of v1.5.42 there is an extra building upgrade tier, which makes the gem requirements both more relaxed and easier to reach. Therefore I would still recommend getting at least close to the lower end of the suggested gem ranges before trying to do things, but stop Mercs when you can’t double your gems in a 1h run.
+<p>This guide is to help you make it through the Research phase of R16 reasonably quickly, without wasting time on upgrades that are too hard to get or take a really long time when they would be much faster later, and without getting lost amid the complexity of it all.
+<p>The original post will not include the list below as it was removed. 
+<p>I have left out all the text from the post, so you might want to look at it.
+<p>A complete list of Research Unlock Requirements can be found <a target="_blank" href="http://musicfamily.org/realm/Research/"> <b>here</b></a>
+<p>A complete list of All Unique Buildings Unlock Requirements can be found <a target="_blank" href="http://musicfamily.org/realm/UniqueBuilding/"> <b>here</b></a>
+<p><b>------------------------------<p></b>
+<p>You should have artifact <b>Vanilla Flavor Juice</b> before starting research
+<p><b>Requirement</b>: +R16
+<p><b>Chance</b>: 20% in the first 5m of a game (Excavate)
+<p><b>Cost</b>: 1 coin
+<p><b>Alignment</b>: Vanilla faction
+<p><b>------------------------------<p></b>
+<p><b>RP = Research Points<p></b>
+<p><b>Note</b>: Get to e70-e71 gems before starting to unlock research.
+<p><b>Note</b>: Good Alignment, Elf/Elfline to Merc in under 10-20 minutes.
+<p>You can use <b>Mercenary</b> to get to the required gem level. (Elven Farms build suggested) 
+<p><b>Note</b>: If the upgrade is one of the key points of the run it will be listed outside of brackets/parentheses (). Design your build based on these.
+<p><b>Note</b>: If it is something you should get that run, if you don’t already have it, it will be in brackets: (Mxx)
+<p><b>Note</b>: If it is something you can probably unlock but not vital that run, it will be in square brackets: [Mxx]
+<p>1.1	Unlock the Fairy unique building and the Spellcraft research facility
+<p>1.2  Unlock the Elf unique building and the Craftmenship research facility
+<p>1.3  Unlock the Angel unique building and the Divine research facility
+<p>1.4	Unlock the Goblin unique building and the Economics research facility
+<p>1.5	Unlock the Undead unique building and the Alchemy research facility
+<p>1.6	Unlock the Demon unique building and the Warfare research facility
+<p>2.1	Get to RP55 Using Fairy with Fairy Bloodline
+<p>2.2	S30=4000 Mana (C10=2500 Farms) [A30=750 Fairy Chanting, This R]	Use Fairy with Fairy Bloodline
+<p>2.3	D50 (S10) [C25]	Use Angel with Goblin Bloodline
+<p>2.4	E30 E50 W25 (A10) Use Goblin with Titan Bloodline or Faceless Bloodline 
+<p>2.5	(D10) [A25] [W50] Use Demon with Druid Bloodline or Goblin  Bloodline if hard
+<p>2.6	(E10) Use Undead with Goblin Bloodline 
+<p>2.7	[W10] [E25] [C50] Use Elf with Elf Bloodline
+<p>2.8	[C25] [D55] [A55] Use Angel with Druid Bloodline long run, spam TCs
+<p>3.1	Get to RP80 Using Fairy with Fairy Bloodline get a30 and e50 first
+<p>3.2	C80 Use any Faction? with Goblin Bloodline
+<p>3.3	E80 Use Fairy with Fairy Bloodline or any?/Goblin Bloodline
+<p>4.1	Make A105 visible (RP105 in Alchemy only) Use Fairy with Fairy Bloodline 	
+<p>4.2	Unlock A105 Use Fairy with Goblin Bloodline (if not unlocked on previous run)
+<p>5.1	S50 A120 (A30) [W120] Use Fairy with Faceless Bloodline unlock
+<p>5.2	Get to RP135 Using Elf with Titan Bloodline spam MB, and maybe TC
+<p>6.1	S135 Use Fairy with Goblin Bloodline 	
+<p>6.2	(C105) (C120) (C135) Use Elf with Elf Bloodline
+<p>6.3	[A135] Use Undead with any Bloodline
+<p>6.4	W135 Use Demon with any Bloodline might need to use Mercs with Dwarf 1,2 and Gobline for each alignment to max buildings before this run
+<p>7.1	D25 A50 [D55] [A55] [W50] Use Undead with Druid Bloodline 8 hour minimum
+<p>7.2	D135 (D55) (A55) Use Angel with Druid Bloodline 
+<p>7.3	Earn gems with Elf faction and Elf Bloodline
+<p>7.4	E135 Goblin with Goblin Bloodline needs hours at e71 gems, pretty fast at e75 gems
+<p>7.5	(W120) (W50) (A25) Use Demon with Undead Bloodline 
+<?php include "../scripts/footer.html"; ?>
