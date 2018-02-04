8dad70fcb52af93ed08d84184a897f766d4b87bc
G00FY <G00FBALL@users.noreply.github.com>
2018-02-04 13:09:20 -0500

    working on search page

--- Search/index.php
+++ Search/index.php
@@ -3,5 +3,8 @@
 <html xmlns="http://www.w3.org/1999/xhtml" lang="en" xml:lang="en">
 <head>
 <?php include "../scripts/header.html"; ?>
-
+<iframe style="
+    width: 100%;
+        min-height: 67em; border:0;" id="SearchResult" src="">
+</iframe>
 <?php include "../scripts/footer.html"; ?>
