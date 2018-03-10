957af5fd7208929d496f0e22410be8e2228e4fb2
G00FY <realm@musicfamily.org>
2018-03-10 03:09:45 -0500

    added sets to menu

---
+++ ArtifactSet/index.php
@@ -0,0 +1,117 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN"
+"http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml" lang="en" xml:lang="en">
+<head>
+<?php include "../scripts/header.html"; ?>
+<h6><img src="http://musicfamily.org/realm/Factions/picks/ExcavationTopPage.png"></h6>
+<br/>
+    <p><img src="http://musicfamily.org/realm/Factions/picks/SilkClothArtifact.png" align="middle"><b> Silk Cloth</b></p>
+    <p><b>Description</b>: The purest silk made for Fairies, by Fairies, of Fairies.
+    <p><b>Requirement</b>: A2+, 3000+ Excavations
+    <p><b>Effect</b>: Unlocks Fairy Set.
+    <p><b>Chance</b>: (x - 10000) / 200000)%, where x is Wizard Tower.
+    <p><img src="http://musicfamily.org/realm/Factions/picks/FairySet.png" align="middle"><b> Fairy Set</b></p>
+    <p><b>Requirement</b>: A2+
+    <p><b>Artifacts Required</b>: Silk Cloth, Pink Carrot, Bottled Voice
+    <p><b>Cost</b>: Free
+    <p><b>Effect</b>: Increase Farm, Inn and Blacksmith production based on number of assistants owned.
+    <p><b>Formula</b>: (0.75 * x ^ 0.75), where x is number of assistants owned.
+      <hr>
+    <p><img src="http://musicfamily.org/realm/Factions/picks/RawEmeraldArtifact.png" align="middle"><b> Raw Emerald</b></p>
+    <p><b>Description</b>: Just slightly less precious than a raw Ruby.
+    <p><b>Requirement</b>: A2+, 3000+ Excavations
+    <p><b>Effect</b>: Unlocks Elven Set.
+    <p><b>Chance</b>: ((2 * x ^ 2) / 10000)%, where x is non-ruby excavation resets (this game).
+    <p><img src="http://musicfamily.org/realm/Factions/picks/ElvenSet.png" align="middle"><b> Elven Set</b></p>
+    <p><b>Requirement</b>: A2+
+    <p><b>Artifacts Required</b>: Raw Emerald, Lucky Clover, Mini-treasure
+    <p><b>Cost</b>: Free
+    <p><b>Effect</b>: Increase all building production based on total amount of Elven Lucks triggered (This R)(since unlock).
+    <p><b>Formula</b>: (0.66 * x ^ 0.6), where x is amount of Elven Lucks triggered.
+      <hr>
+    <p><img src="http://musicfamily.org/realm/Factions/picks/FossilizedWingArtifact.png" align="middle"><b> Fossilized Wing</b></p>
+    <p><b>Description</b>: The remains of an Angel fallen to earth.
+    <p><b>Requirement</b>: A2+, 3000+ Excavations
+    <p><b>Effect</b>: Unlocks Angel Set.
+    <p><b>Chance</b>: (x / 25920000 (300 days))%, where x is angel time (all time) in seconds.
+    <p><img src="http://musicfamily.org/realm/Factions/picks/AngelSet.png" align="middle"><b> Angel Set</b></p>
+    <p><b>Requirement</b>: A2+
+    <p><b>Artifacts Required</b>: Fossilized Wing, Pillar Fragment, Divine Sword
+    <p><b>Cost</b>: Free
+    <p><b>Effect</b>: Increase mana regen based on spells cast this game.
+    <p><b>Formula</b>: (0.32 * x ^ 0.32), where x is spells cast.
+      <hr>
+    <p><img src="http://musicfamily.org/realm/Factions/picks/SpikedWhipArtifact.png" align="middle"><b> Spiked Whip</b></p>
+    <p><b>Description</b>: Use with caution. You do not want to exterminate all your slaves.
+    <p><b>Requirement</b>: A2+, 3000+ Excavations
+    <p><b>Effect</b>: Unlocks Goblin Set.
+    <p><b>Chance</b>: (x - 10000) / 300000)%, where x is Slave Pens.
+    <p><img src="http://musicfamily.org/realm/Factions/picks/GoblinSet.png" align="middle"><b> Goblin Set</b></p>
+    <p><b>Requirement</b>: A2+
+    <p><b>Artifacts Required</b>: Spiked Whip, Ancient Coin Piece, Goblin Purse
+    <p><b>Cost</b>: Free
+    <p><b>Effect</b>: Multiplicatively increase gathered faction coins based on time spent this game if they don't match faction or bloodline.
+    <p><b>Formula</b>: (1 + (0.01 * x ^ 0.7)), where x is seconds.
+    <p><b>Note</b>: Ingame tooltip is off by one (actual effect is 1 higher)
+      <hr>
+    <p><img src="http://musicfamily.org/realm/Factions/picks/DustyCoffinArtifact.png" align="middle"><b> Dusty Coffin</b></p>
+    <p><b>Description</b>: Sealed since forever, yet you can hear a strange noise from within.
+    <p><b>Requirement</b>: A2+, 3000+ Excavations
+    <p><b>Effect</b>: Unlocks Undead Set.
+    <p><b>Chance</b>: ((1 / (30 + x ^ 1.75))%, where x is time this game in seconds.
+    <p><b>Chance</b>: 1 in 3000 in 0 seconds 1 in 132349 at 60 seconds. 1 in 438078 at 120 seconds.
+    <p><img src="http://musicfamily.org/realm/Factions/picks/UndeadSet.png" align="middle"><b> Undead Set</b></p>
+    <p><b>Requirement</b>: A2+
+    <p><b>Artifacts Required</b>: DustyCoffin, Rotten Organ, JawBone
+    <p><b>Cost</b>: Free
+    <p><b>Effect</b>: Additively Increase max mana based on offline bonus.
+    <p><b>Formula</b>: (45 * x ^ 0.6), where x is offline Multiplier.
+      <hr>
+    <p><img src="http://musicfamily.org/realm/Factions/picks/CrystallizedLavaArtifact.png" align="middle"><b> Crystallized Lava</b></p>
+    <p><b>Description</b>: Incandescent but still. Can be used efficiently as a desk lamp.
+    <p><b>Requirement</b>: A2+, 3000+ Excavations
+    <p><b>Effect</b>: Unlocks Demon Set.
+    <p><b>Chance</b>: (x - 10000) / 200000)%, where x is Hall of  Legends.
+    <p><img src="http://musicfamily.org/realm/Factions/picks/DemonSet.png" align="middle"><b> Demon Set</b></p>
+    <p><b>Requirement</b>: A2+
+    <p><b>Artifacts Required</b>: Crystallized Lava, Demonic Figurine, Demon Horn
+    <p><b>Cost</b>: Free
+    <p><b>Effect</b>: Increase non unique building production based on the sum of your faction spells activity time.
+    <p><b>Formula</b>: (0.25 * x ^ 0.75), where x is faction spell activity time.
+      <hr>
+    <p><img src="http://musicfamily.org/realm/Factions/picks/TitanHelmetArtifact.png" align="middle"><b> Titan Helmet</b></p>
+    <p><b>Description</b>: Made of enough metal to craft a human-sized full plate.
+    <p><b>Requirement</b>: A2+, 3000+ Excavations
+    <p><b>Effect</b>: Unlocks Titan Set.
+    <p><b>Chance</b>: ((x ^ 2) / 5000000000 (5B))%, where x is Royal Exchanges.
+    <p><img src="http://musicfamily.org/realm/Factions/picks/TitanSet.png" align="middle"><b> Titan Set</b></p>
+    <p><b>Requirement</b>: A2+
+    <p><b>Artifacts Required</b>: Titan Helmet, Huge Titan Statue, Titan Shield
+    <p><b>Cost</b>: Free
+    <p><b>Effect</b>: Increase Unique building production based on time spent this game.
+    <p><b>Formula</b>: (0.07 * x ^ 0.7), where x is seconds.
+      <hr>
+    <p><img src="http://musicfamily.org/realm/Factions/picks/BranchoftheLifeTreeArtifact.png" align="middle"><b> Branch of the Life Tree</b></p>
+    <p><b>Description</b>: Despite being torn from its source tree, it keeps growing buds and leaves.
+    <p><b>Requirement</b>: A2+, 3000+ Excavations
+    <p><b>Effect</b>: Unlocks Druid Set.
+    <p><b>Chance</b>: (x ^ 3) / 2000000 (2M))%, where x is level of current lineage.
+    <p><img src="http://musicfamily.org/realm/Factions/picks/DruidSet.png" align="middle"><b> Druid Set</b></p>
+    <p><b>Requirement</b>: A2+
+    <p><b>Artifacts Required</b>: Branch of the Life Tree, Glyph Table, Stone Of Balance
+    <p><b>Cost</b>: Free
+    <p><b>Effect</b>: Increase production of all buildings based on their quantity.
+    <p><b>Formula</b>: (0.65 * x ^ 0.65), where x is building count.
+      <hr>
+    <p><img src="http://musicfamily.org/realm/Factions/picks/NightmareFigmentArtifact.png" align="middle"><b> Nightmare Figment</b></p>
+    <p><b>Description</b>: An unshaped, ephemeral substance which is politely trying to corrupt your mind.
+    <p><b>Requirement</b>: A2+, 3000+ Excavations
+    <p><b>Effect</b>: Unlocks Faceless Set.
+    <p><b>Chance</b>: (x ^ 1.5 / 20000000 (20M))%, where x is headstart time in seconds.
+    <p><img src="http://musicfamily.org/realm/Factions/picks/FacelessSet.png" align="middle"><b> Faceless Set</b></p>
+    <p><b>Requirement</b>: A2+
+    <p><b>Artifacts Required</b>: Nightmare Figment, Translucent Goo, Octopus-shaped Helmet
+    <p><b>Cost</b>: Free
+    <p><b>Effect</b>: Increase production of all buildings based on highest amount of spells cast in a single game.
+    <p><b>Formula</b>: (0.32 * x ^ 0.32), where x is number of spells cast.
+<?php include "../scripts/footer.html"; ?>
