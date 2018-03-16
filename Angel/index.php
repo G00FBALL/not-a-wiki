cd40fd4f7116876b99a80d881c3ae701f06e2502
G00FY <realm@musicfamily.org>
2018-03-16 05:09:21 -0400

    fix several error, removed un needed breaks

--- Angel/index.php
+++ Angel/index.php
@@ -11,9 +11,9 @@
 <p><b>Challenge</b>: Cast 50 Holy Lights during daytime (6am-6pm local time) within 20 minutes of a new game.</p>
 <p><b>Effect</b>: Reduce all spell costs by 10% (calculated *after* Angel (2,3) Angelic Wisdom).</p>
 <p><b>Upgrade</b>: Good for Angel</p>
-<p><b>Tips</b>
+<p><b>Tips</b></p>
 <p>Recommended +1 B (1E9) gems. You will need a min mana regen of 60 m/s get enough Holy Light autocasts. Avoid purchasing Angelic Dominance (3,2). If you have bloodlines (R7), choose the Druid Bloodline.</p>
-<p><b>------------------------------</b>
+<hr>
 <p><b>Angel Challenge 2</b></p>
 <p><img src="http://musicfamily.org/realm/Factions/picks/ManaCryptsChallenges.png" alt="Mana Crypts" align="middle"> <b>Mana Crypts</b></p>
 <p>The archangel leaders have entrusted you on a sacred mission:quickly harvest enough magical power to become a beacon of magical selflessness. Extorting from the poor is forbidden.</p>
@@ -23,19 +23,19 @@
 <p><b>Upgrade</b>: Good for Undead</p>
 <p><b>Tips</b></p>
 <p>Avoid getting Angelic Wisdom (2,2) and Angelic Dominance (3,2) because they reduce your mana-spent-per-second ability, if you are still finding yourself with a full mana pool, you can also turn off your Angel 1 perk. Tip: Autocast God's Hand with bar at 0. Add in CtA if excess mana builds up. When mana regen gets too high, just put every spell on Gold autocast (Not TC of course).</p>
-<p><b>------------------------------</b>
+<hr>
 <p><b>Angel Challenge 3</b></p>
 <p><img src="http://musicfamily.org/realm/Factions/picks/Nephilim'sBlessingChallenges.png" alt="Nephilim's Blessing" align="middle"> <b>Nephilim's Blessing</b></p>
 <p>The fallen nephilim have noticed your abilities and have entrusted you with a secret mission: Spread false hope upon the legends of mortals</p>
-<p><b>Requirements</b>: 18 Reincarnations, Angel Unique Building, Demon Bloodline
+<p><b>Requirements</b>: 18 Reincarnations, Angel Unique Building, Demon Bloodline</p>
 <p><b>Challenge</b>: Have 2000 Halls of Legends, and no good buildings.</p>
 <p><b>Effect</b>: Halls of Legends benefit from any Certificate, Validation, Affirmation or Proclamation upgrade.</p>
-<p><b>Upgrade</b>: Good for All</h3>
+<p><b>Upgrade</b>: Good for All</p>
 <p><b>Note</b>: Also reduces Ascension penalty for Angel Bloodline</p>
 <p><b>Note</b>: Tier 3 challenges cannot be done as Drow or Dwarf Factions."because you can't buy the U building"</p>
-<p><b>------------------------------</b>
+<hr>
 <p><b>In-Game Challenge Final Reward</b></p>
 <p><img src="http://musicfamily.org/realm/Factions/picks/God'sThroneChallenges.png" alt="God's Throne" align="middle"> <b>God's Throne</b></p>
 <p><b>Effect</b>: Mana regeneration is increased by 50% while God's Hand is active.</p>
-<p><b>Effect</b>: Also increases offline spell cast amount by multiplicatively based on your offline mana regeneration (10 * log10(1 + x)).
+<p><b>Effect</b>: Also increases offline spell cast amount by multiplicatively based on your offline mana regeneration (10 * log10(1 + x)).</p>
 <?php include "../scripts/footer.html"; ?>
--- AngelFaction/index.php
+++ AngelFaction/index.php
@@ -3,8 +3,8 @@
 <html xmlns="http://www.w3.org/1999/xhtml" lang="en" xml:lang="en">
 <head>
 <?php include "../scripts/header.html"; ?>
-<h6><a href="/realm/Factions/"><img src="http://musicfamily.org/realm/Factions/picks/AngelTopPage.png" alt="Spellcraft" align="middle"></h6></a>
-<p><img src="http://musicfamily.org/realm/Factions/picks/Angel.png" alt="Smiley face" align="middle"><b> Champion Trophy</b></p>
+<h6><a href="/realm/Factions/"><img src="http://musicfamily.org/realm/Factions/picks/AngelTopPage.png" align="middle"></h6></a>
+<p><img src="http://musicfamily.org/realm/Factions/picks/Angel.png" align="middle"><b> Champion Trophy</b></p>
 <p><b>In-game description</b>
 <p>Angels' ability with magic is unparalleled as they have the innate power to infuse mana into everything they touch.</p>
 <p>Affiliating yourself with the Angels faction will grant you nearly unlimited spell power and the ability to take advantage of it directly.</p>
@@ -14,82 +14,82 @@
 <p>All Vanilla Factions are enabled by default</p>
 <p><b>Faction spell</b>: God's Hand</p>
 <p><b>Effect</b>: Increase the production of all buildings by 150x your mana regeneration rate for 20 seconds.</p>
-<p><b>Spell Trophy & Upgrade: <img src="http://musicfamily.org/realm/Factions/picks/God'sRestFactionUpgrade.png" alt="God's Rest" align="middle"> God's Rest</b> 
-<p><b>Effect</b>: Gives random Faction Coins based on your mana regeneration for each other spell you cast while God's Hand is active.
+<p><b>Spell Trophy & Upgrade: <img src="http://musicfamily.org/realm/Factions/picks/God'sRestFactionUpgrade.png" align="middle"> God's Rest</b></p>
+<p><b>Effect</b>: Gives random Faction Coins based on your mana regeneration for each other spell you cast while God's Hand is active.</p>
 <p><b>Formula</b>: (floor(x^1.1), where x is FC chance.</p>
 <p><b>Requirement</b>: Cast God's Hand on any Sunday.</p>
-<p><b>----------------------------------</b></p>
+<hr>
 <p><b>Tier 1 Upgrades</b></p>
-<p><b><img src="http://musicfamily.org/realm/Factions/picks/AngelsTradeTreaty.png" alt="Smiley face" align="middle"> Angel Trade Treaty</b></p>
+<p><b><img src="http://musicfamily.org/realm/Factions/picks/AngelsTradeTreaty.png" align="middle"> Angel Trade Treaty</b></p>
 <p><b>Cost</b>: 20 Angel Coins</p>
 <p><b>Effect</b>: Unlocks Angel Upgrades.</p>
-<p><b><img src="http://musicfamily.org/realm/Factions/picks/HolyBellsFactionUpgrade.png" alt="Smiley face" align="middle"> Holy Bells</b></p>
+<p><b><img src="http://musicfamily.org/realm/Factions/picks/HolyBellsFactionUpgrade.png" align="middle"> Holy Bells</b></p>
 <p><b>Cost</b>: 50 M (5E7)</p>
 <p><b>Effect</b>: Cathedrals also increase your mana regeneration rate.</p>
 <p><b>Formula</b>: x^0.28, where x is the number of Cathedrals you own.</p>
-<p><b><img src="http://musicfamily.org/realm/Factions/picks/AngelicDeterminationFactionUpgrade.png" alt="Angelic Determination" align="middle"> Angelic Determination</b></p>
+<p><b><img src="http://musicfamily.org/realm/Factions/picks/AngelicDeterminationFactionUpgrade.png" align="middle"> Angelic Determination</b></p>
 <p><b>Cost</b>: 500 M (5E8)</p>
 <p><b>Effect</b>: Increase the production of all buildings based on spells cast in this game.</p>
 <p><b>Formula</b>: 1.5*x^0.85%, where x is your Spells Cast (This Game) stat.</p>
-<p><b><img src="http://musicfamily.org/realm/Factions/picks/AngelFeathersFactionUpgrade.png" alt="Smiley face" align="middle"> Angel Feathers</b></p>
+<p><b><img src="http://musicfamily.org/realm/Factions/picks/AngelFeathersFactionUpgrade.png" align="middle"> Angel Feathers</b></p>
 <p><b>Cost</b>: 5 B (5E9)</p>
 <p><b>Effect</b>: Increase Mana production by 40%.</p>
-<p><b>----------------------------------</b></p>
+<hr>
 <p><b>Tier 2 Upgrades</b></p>
-<p><b><img src="http://musicfamily.org/realm/Factions/picks/AngelsFriendshipPact.png" alt="Angel Friendship Pact" align="middle"> Angel Friendship Pact</b></p>
+<p><b><img src="http://musicfamily.org/realm/Factions/picks/AngelsFriendshipPact.png" align="middle"> Angel Friendship Pact</b></p>
 <p><b>Cost</b>: 100 Angel Coins</p>
 <p><b>Effect</b>: Unlocks more Angel Upgrades.</p>
-<p><b><img src="http://musicfamily.org/realm/Factions/picks/GuardianAngelsFactionUpgrade.png" alt="Smiley face" align="middle"> Guardian Angels</b></p>
+<p><b><img src="http://musicfamily.org/realm/Factions/picks/GuardianAngelsFactionUpgrade.png" align="middle"> Guardian Angels</b></p>
 <p><b>Cost</b>: 500 B (5E11)</p>
 <p><b>Effect</b>: Royal Castles also increase your mana production rate.</p>
 <p><b>Formula</b>: x^0.305, where x is the number of Royal Castles you own.</p>
-<p><b><img src="http://musicfamily.org/realm/Factions/picks/AngelicWisdomFactionUpgrade.png" alt="Smiley face" align="middle"> Angelic Wisdom</b></p>
+<p><b><img src="http://musicfamily.org/realm/Factions/picks/AngelicWisdomFactionUpgrade.png" align="middle"> Angelic Wisdom</b></p>
 <p><b>Cost</b>: 5 T (5E12)</p>
 <p><b>Effect</b>: Decrease the cost of all spells by 100.</p>
-<p><b>Effect</b>: Also increases offline spell cast amount by multiplicatively based on your offline mana regeneration (10 * log10(1 + x)).
-<p><b><img src="http://musicfamily.org/realm/Factions/picks/ArchangelFeathersFactionUpgrade.png" alt="Smiley face" align="middle"> Archangel Feathers</b></p>
+<p><b>Effect</b>: Also increases offline spell cast amount by multiplicatively based on your offline mana regeneration (10 * log10(1 + x)).</p>
+<p><b><img src="http://musicfamily.org/realm/Factions/picks/ArchangelFeathersFactionUpgrade.png" align="middle"> Archangel Feathers</b></p>
 <p><b>Cost</b>: 50 T (5E13)</p>
 <p><b>Effect</b>: Increase Mana production by 40%.</p>
-<p><b>----------------------------------</b></p>
+<hr>
 <p><b>Tier 3 Upgrades</b></p>
-<p><b><img src="http://musicfamily.org/realm/Factions/picks/AngelsAllianceFactionUpgrade.png" alt="Smiley face" align="middle"> Angel Alliance</b></p>
+<p><b><img src="http://musicfamily.org/realm/Factions/picks/AngelsAllianceFactionUpgrade.png" align="middle"> Angel Alliance</b></p>
 <p><b>Cost</b>: 500 Angel Coins</p>
 <p><b>Effect</b>: Unlocks more Angel Upgrades.</p>
-<p><b><img src="http://musicfamily.org/realm/Factions/picks/MagicalGatesFactionUpgrade.png" alt="Smiley face" align="middle"> Magical Gates</b></p>
+<p><b><img src="http://musicfamily.org/realm/Factions/picks/MagicalGatesFactionUpgrade.png" align="middle"> Magical Gates</b></p>
 <p><b>Cost</b>: 5 Qa (5E15)</p>
 <p><b>Effect</b>: Heaven's Gates also increase your mana production rate.</p>
 <p><b>Formula</b>: x^0.33, where x is the number of Heaven's Gates you own.</p>
-<p><b><img src="http://musicfamily.org/realm/Factions/picks/AngelicDominanceFactionUpgrade.png" alt="Smiley face" align="middle"> Angelic Dominance</b></p>
+<p><b><img src="http://musicfamily.org/realm/Factions/picks/AngelicDominanceFactionUpgrade.png" align="middle"> Angelic Dominance</b></p>
 <p><b>Cost</b>: 50 Qa (5E16)</p>
 <p><b>Effect</b>: Spell durations are increased by 50%.</p>
-<p><b><img src="http://musicfamily.org/realm/Factions/picks/WingsofLibertyFactionUpgrade.png" alt="Smiley face" align="middle"> Wings of Liberty</b></p>
+<p><b><img src="http://musicfamily.org/realm/Factions/picks/WingsofLibertyFactionUpgrade.png" align="middle"> Wings of Liberty</b></p>
 <p><b>Cost</b>: 500 Qa (5E17)</p>
 <p><b>Effect</b>: Increase the production of all buildings based on mana regeneration.</p>
 <p><b>Formula</b>: 3 * x^0.85, where x is your Mana per Second stat.</p>
-<p><b>----------------------------------</b></p>
+<hr>
 <p><img src="http://musicfamily.org/realm/Factions/picks/AngelicHeritage.png" alt="Angelic Heritage" align="middle"><b> Angelic Heritage</b></p>
 <p><b>Cost</b>: 5000 Angel Coins</p>
 <p><b>Requirements</b>: Angel Champion Trophy</p>
 <p><b>Effect</b>: Increase Mana production by 10%.</p>
-<p><b>----------------------------------</b>
+<hr>
 <p><b>Angel Research Requirements</b>
-<p><b><img src="http://musicfamily.org/realm/Factions/picks/Heaven'sDomainQuestFactionUpgrade.png" alt="Smiley face" align="middle"> Heaven's Domain Quest</b></p>
+<p><b><img src="http://musicfamily.org/realm/Factions/picks/Heaven'sDomainQuestFactionUpgrade.png" align="middle"> Heaven's Domain Quest</b></p>
 <p>Bless you, dear friend. The Lands of the sky are expanding as more good souls are joining our ranks. Would you be so kind to provide them a place to stay in the Heavens?
-<p><b>Requirement</b>: R16 + 1500 Heaven's Gates
+<p><b>Requirement</b>: R16 + 1500 Heaven's Gates</p>
 <p><b>Cost</b>: 10 Dtg (1E100)</p>
-<p><b><img src="http://musicfamily.org/realm/Factions/picks/Heaven'sDomainFactionUpgrade.png" alt="Smiley face" align="middle"> Heaven's Domain</b></p>
+<p><b><img src="http://musicfamily.org/realm/Factions/picks/Heaven'sDomainFactionUpgrade.png" align="middle"> Heaven's Domain</b></p>
 <p>Upgrade Heaven's Gate To Heaven's Domains, boosting their production by 35000% per spell affecting them and unlocking more unique perks for the building.</p>
-<p><b>Note</b>: Required once throughout the entire game play and does not count towards the bought upgrade stat.
+<p><b>Note</b>: Required once throughout the entire game play and does not count towards the bought upgrade stat.</p>
 <p><b>Effect</b>: +Access to the Research Facilities.</p>
-<p><b>Requirements</b>: Buy "Heaven's Domain Quest"
-<p><b>Cost</b>: 10 Ttg (1E103)
-<p><b><img src="http://musicfamily.org/realm/Factions/picks/PantheonFactionUpgrade.png" alt="Smiley face" align="middle"> Pantheon</b></p>
-<p><b>Effect</b>: Unlocks the Divine research facility and increases Angel production by 100000%, plus an additional bonus based on Divine research points.
-<p><b>Note</b>: Required once throughout the entire game play and does not count towards the bought upgrade stat.
-<p><b>Requirements</b>: All Angel faction upgrades bought, Heaven's Domain
-<p><b>Cost</b>: 10 Notg (1e121) coins, 1 M (1e6) Angel coins
+<p><b>Requirements</b>: Buy "Heaven's Domain Quest"</p>
+<p><b>Cost</b>: 10 Ttg (1E103)</p>
+<p><b><img src="http://musicfamily.org/realm/Factions/picks/PantheonFactionUpgrade.png" align="middle"> Pantheon</b></p>
+<p><b>Effect</b>: Unlocks the Divine research facility and increases Angel production by 100000%, plus an additional bonus based on Divine research points.</p>
+<p><b>Note</b>: Required once throughout the entire game play and does not count towards the bought upgrade stat.</p>
+<p><b>Requirements</b>: All Angel faction upgrades bought, Heaven's Domain</p>
+<p><b>Cost</b>: 10 Notg (1e121) coins, 1 M (1e6) Angel coins</p>
 <p><b>Unique Building</b>
-<p><img src="http://musicfamily.org/realm/Factions/picks/AngelUniqueBuilding.png" alt="Smiley face" align="middle"></p>
+<p><img src="http://musicfamily.org/realm/Factions/picks/AngelUniqueBuilding.png" align="middle"></p>
 <p>Upgraded from Heaven's Gates</p>
 <p>At R16, the Angels' building Heaven Gates can be upgraded to a Unique Building: Heaven Domain, which is required for some challenges and research quests</p>
 <?php include "../scripts/footer.html"; ?>
