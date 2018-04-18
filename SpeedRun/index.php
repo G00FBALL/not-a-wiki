99ed4bd29822b5ac641b294838e6223edd28c03a
G00FY <realm@musicfamily.org>
2018-04-18 06:16:05 -0400

    spelling error

--- SpeedRun/index.php
+++ SpeedRun/index.php
@@ -3,14 +3,14 @@
 <html xmlns="http://www.w3.org/1999/xhtml" lang="en" xml:lang="en">
 <title>Speed Run Guide</title>
 <?php include "../scripts/header.html"; ?>
-<h3>Speed Run</h3>
-<p><b>----------------------------------</b>
+<h3>Speed Run Instructions</h3>
+<hr>
 <p><img src="http://musicfamily.org/realm/Factions/picks/SpeedrunSecretTrophy.png" alt="Speedrun" align="middle"> <b>Speed Run</b>
 <p><b>Requirement</b>: Reach 1 M (1E6) coins in less than 5 minutes, without using Gem Power.
 <p><b>Effect</b>: Increase your base clicking reward by 1m.
 <p>Really overpowered for the first dozen abdications, so you may as well get this right out of the gate.
 <p>Quite easy by following this guide.
-<p><b>----------------------------------</b>
+<hr>
 <p><b>Should be done at R0</b>
 <p><b>Step by Step Guide</b></p>
 <p><b>Buy all upgrades as they become available</b></p>
@@ -21,11 +21,11 @@
 <p><b>2</b>. Buy 25 Farms</p>
 <p><b>3</b>. Buy 15 Inns</p>
 <p><b>4</b>. Buy 15 Blacksmiths</p>
-<p><b>Bye all upgrades as they become available</b></p>
+<p><b>Buy all upgrades as they become available</b></p>
 <p><b>Do Not Buy Gem Power</b></p>
 <p><b>5</b>. Buy Proof of Evil Deed</p>
 <p><b>6</b>. Buy 6 Slave Pens and 6 Orcish Arenas</p>
-<p><b>Bye all upgrades as they become available</b></p>
+<p><b>Buy all upgrades as they become available</b></p>
 <p><b>Do Not Buy Gem Power</b></p>
 <p><b>7</b>. Cast Blood Frenzy and Call to Arms</p>
 <p><b>8</b>. Buy 6 Witch Conclaves</p>
