d51501beeaaba8dddc6bc370158a766ef9ce1d3c
G00FY <realm@musicfamily.org>
2018-05-23 06:40:11 -0400

    added R111+ page

---
+++ R111-R115/index.php
@@ -0,0 +1,183 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN"
+        "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml" lang="en" xml:lang="en">
+<meta http-equiv="Content-Type" content="text/html; charset=windows-1252">
+<?php include "../scripts/header.html"; ?>
+<br/>
+<h6><img src="http://musicfamily.org/realm/Factions/picks/TopPageResearch.png" alt="Spellcraft" align="middle"></h6>
+<br/>
+<br/>
+    <p><b>Updated May. 23rd 2018</b>
+    <p>All builds comes from the  <a target="_blank" href="http://www.kongregate.com/forums/8945-realm-grinder/topics/922600-3-0-build-megathread-under-construction-please-post-builds-to-add?page=1"><b>megathread</b></a>  forum page</p>
+    <p><b>Note</b>: All Obsolete builds moved to  <b><a target="_blank" href="http://musicfamily.org/realm/Obsolete/">Obsolete</b></a> page
+<br/>
+<div class="shlisting">
+    <p><b>Production Builds</b></p>
+    <div class="category">
+        <div class="shelementwhole">
+            <p onclick="shohid($(this));"><b> <a href="#" onclick="return false;">Druid starter<font color="DarkGoldenRod"> Neutral</font></a></b></p>
+            <div class="autohide">
+                <p><b>Author</b>: ensteffahn
+                <p><b>Range</b>: 0 gems - 1e27 (1 Oc) Gems
+                <p><b>Faction</b>: Druid
+                <p><b>Bloodline</b>: Druid
+                <p><b>Artifact Set</b>: Druid
+                      <p><button onclick="myFunction($(this))">Copy Build</button>
+                      <input type="text" value="S3200,C300,C340,D150,D275,E260,E1425,A250,A251,A375,W350,W1375" id="Build">
+                <p>S3200,
+                <p>C300,C340,
+                <p>D150,D275,
+                <p>E260,E1425,
+                <p>A250,A251,A375,
+                <p>W350,W1375
+         </div>
+    </div>
+        <div class="shelementwhole">
+            <p onclick="shohid($(this));"><b> <a href="#" onclick="return false;">Fairy Rusher<font color="Blue"> Good</font></a></b></p>
+            <div class="autohide">
+                <p><b>Author</b>: Artcrow
+                <p><b>Range</b>: 1e27 (1 Oc) - 1e33 (1 Dc) Gems
+                <p><b>Faction</b>: Fairy
+                <p><b>Bloodline</b>: Undead
+                <p><b>Artifact Set</b>: Fairy
+                      <p><button onclick="myFunction($(this))">Copy Build</button>
+                      <input type="text" value="S1,S180,S3200,C10,C340,D225,D245,E50,E1325,A30,A120,W1375" id="Build">
+                      <p><button onclick="myFunction($(this))">Copy Build</button>
+                      <input type="text" value="S1,S180,S3200,C10,C340,D225,D245,E50,E400,A30,A120,W1375" id="Build"> If R115
+                <p>S1,S180,S3200,
+                <p>C10,C340,
+                <p>D225,D245,
+                <p>E50,E1325,
+                <p>A30,A120,
+                <p>W1375
+                <p><b>Notes</b>: Replace E1325 with E400 if R115.
+         </div>
+    </div>
+        <div class="shelementwhole">
+            <p onclick="shohid($(this));"><b> <a href="#" onclick="return false;">Fairy SS7<font color="Blue"> Good</font></a></b></p>
+            <div class="autohide">
+                <p><b>Author</b>: Artcrow
+                <p><b>Range</b>: 1e37 (10 Ud) - 1e42 (1 Td) Gems
+                <p><b>Faction</b>: Fairy
+                <p><b>Bloodline</b>: Undead
+                <p><b>Artifact Set</b>: Fairy
+                      <p><button onclick="myFunction($(this))">Copy Build</button>
+                      <input type="text" value="S1,S180,S3200,C10,C400,D225,D245,E50,E400,A30,A120,W1375" id="Build">
+                      <p><button onclick="myFunction($(this))">Copy Build</button>
+                      <input type="text" value="S1,S180,S3200,C10,C340,D225,D245,E50,E1325,A30,A120,W1375" id="Build"> Sunforce FC active
+                <p>S1,S180,S3200,
+                <p>C10,C400,
+                <p>D225,D245,
+                <p>E50,E400,
+                <p>A30,A120,
+                <p>W1375
+                <p><b>Notes</b>: Takes estimated time of 3 hours to SS7.
+                <p><b>Notes</b>: Swap C400 with C340 and E400 with E1325 if sunforce FC is active.
+         </div>
+    </div>
+        <div class="shelementwhole">
+            <p onclick="shohid($(this));"><b> <a href="#" onclick="return false;">Dragonline Faceless<font color="DarkGoldenRod"> Neutral</font></a></b></p>
+            <div class="autohide">
+                <p><b>Author</b>: Artcrow
+                <p><b>Range</b>: 1e41 (100 Dd) - ? Gems
+                <p><b>Faction</b>: Faceless
+                <p><b>Bloodline</b>: Dragon
+                <p><b>Artifact Set</b>: Druid, Faceless (if stronger)
+                      <p><button onclick="myFunction($(this))">Copy Build</button>
+                      <input type="text" value="S251,S375,S400,C340,D245,D260,E350,E1425,A250,A300,W275,W1375" id="Build">
+                <p>S251,S375,S400,
+                <p>C340,
+                <p>D245,D260,
+                <p>E350,E1425,
+                <p>A250,A300,
+                <p>W275,W1375
+                <p><b>Notes</b>: Takes estimated time of 2-5 hours to SS7.
+                <p><b>Notes</b>: Do W275 buffing before running this build.
+         </div>
+    </div>
+        <div class="shelementwhole">
+            <p onclick="shohid($(this));"><b> <a href="#" onclick="return false;">Dragonline Undead<font color="DarkRed"> Evil</font></a></b></p>
+            <div class="autohide">
+                <p><b>Author</b>: Artcrow
+                <p><b>Range</b>: 1e45 (1 Qad) - ? Gems
+                <p><b>Faction</b>: Undead
+                <p><b>Bloodline</b>: Dragon
+                <p><b>Artifact Set</b>: Goblin
+                      <p><button onclick="myFunction($(this))">Copy Build</button>
+                      <input type="text" value="S225,S3200,C340,D25,D245,E10,E400,A1,A120,A175,W50,W205" id="Build">
+                <p>S225,S3200,
+                <p>C340,
+                <p>D25,D245,
+                <p>E10,E400,
+                <p>A1,A120,A175,
+                <p>W50,W205
+                <p><b>Notes</b>: akes estimated time of 12 hours to lineage, and another 8 hours to SS7.
+         </div>
+    </div>
+         </div>
+        <br/>
+    <p><b>Special Builds</b></p>
+    <div class="category">
+        <div class="shelementwhole">
+            <p onclick="shohid($(this));"><b> <a href="#" onclick="return false;">Undeadline Faceless (W275 rush buff build)<font color="DarkGoldenRod"> Neutral</font></a></b></p>
+            <div class="autohide">
+                <p><b>Author</b>: Necrodoom
+                <p><b>Range</b>: 1e51 (1 Sxd) Gems
+                <p><b>Faction</b>: Faceless
+                <p><b>Bloodline</b>: Undead
+                <p><b>Artifact Set</b>: Undead
+                      <p><button onclick="myFunction($(this))">Copy Build</button>
+                      <input type="text" value="S251,S375,S400,C400,D200,D260,E350,E1325,A120,A300,W275,W1375" id="Build">
+                <p>S251,S375,S400,
+                <p>C400,
+                <p>D200,D260,
+                <p>E350,E1325,
+                <p>A120,A300,
+                <p>W275,W1375
+                <p><b>Notes</b>: Performance varies based on faction times, but needs very little time to beat most W275 buff variants.
+         </div>
+    </div>
+        <div class="shelementwhole">
+            <p onclick="shohid($(this));"><b> <a href="#" onclick="return false;">Dragonline Goblins (S400 buff build)<font color="DarkRed"> Evil</font></a></b></p>
+            <div class="autohide">
+                <p><b>Author</b>: Artcrow
+                <p><b>Range</b>: 1e43 (10 Td) Gems+
+                <p><b>Faction</b>: Goblin
+                <p><b>Bloodline</b>: Dragon
+                <p><b>Artifact Set</b>: Undead
+                      <p><button onclick="myFunction($(this))">Copy Build</button>
+                      <input type="text" value="S50,S3200,C175,C400,D200,E1,E135,E225,A10,A120,W25,W205" id="Build">
+                      <p><button onclick="myFunction($(this))">Copy Build</button>
+                      <input type="text" value="S50,S400,C175,C400,D200,E1,E135,E225,A10,A120,W25,W205" id="Build"> 3+ days run.
+                <p>S50,S3200,
+                <p>C175,C400,
+                <p>D200,
+                <p>E1,E135,E225,
+                <p>A10,A120,
+                <p>W25,W205
+                <p><b>Notes</b>: Use S400 for runs of over 3(?) days.
+
+         </div>
+    </div>
+        <div class="shelementwhole">
+            <p onclick="shohid($(this));"><b> <a href="#" onclick="return false;">Dwarfline Angels (FC12 buff build)<font color="Blue"> Good</font></a></b></p>
+            <div class="autohide">
+                <p><b>Author</b>: Jgchan29
+                <p><b>Range</b>: 1e45 (1 Qad) Gems+
+                <p><b>Faction</b>: Angel
+                <p><b>Bloodline</b>: Dwarf
+                <p><b>Artifact Set</b>: Angel
+                      <p><button onclick="myFunction($(this))">Copy Build</button>
+                      <input type="text" value="S10,S400,C25,C340,D1,D150,D175,E1325,A50,A250,W225,W1375" id="Build">
+                <p>S10,S400,
+                <p>C25,C340,
+                <p>D1,D150,D175,
+                <p>E1325,
+                <p>A50,A250,
+                <p>W225,W1375
+                <p><b>Notes</b>: Build set up is up to buying Art of the Crow.
+         </div>
+    </div>
+        </div>
+       </div>
+<?php include "../scripts/footer.html"; ?>
