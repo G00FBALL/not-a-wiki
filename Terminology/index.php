5af9edea5bcd28c355c1aba81856c83fefc0a70a
G00FY <realm@musicfamily.org>
2018-05-25 16:50:13 -0400

    updated Terminology page

--- Terminology/index.php
+++ Terminology/index.php
@@ -3,98 +3,123 @@
 <html xmlns="http://www.w3.org/1999/xhtml" lang="en" xml:lang="en">
 <head>
 <?php include "../scripts/header.html"; ?>
+<h6>Hot Keys / Shortcuts</h6>
+<b><p>Spells</p></b>
+<p><b>#1</b>: Tax Collection</p>
+<p><b>#2</b>: Call to Arms</p>
+<p><b>#3</b>: 3rd Spell</p>
+<p><b>#4</b>: 4th Spell</p>
+<p><b>#5</b>: 5th Spell</p>
+<p><b>#6</b>: 6th Spell</p>
+<p><b>#7</b>: 7th Spell</p>
+<p><b>#8</b>: 8th Spell</p>
+<br/>
+<b><p>Buildings</p></b>
+<p>You will need to disable BuyMax Button in options to use hotkeys for buildings</p>
+<p><b>Buy 1</b>: Click spell</p>
+<p><b>Buy 10</b>: CTRL +click spell</p>
+<p><b>Buy 100</b>: SHIFT +click spell</p>
+<p><b>Buy 1000</b>: X +click spell</p>
+<p><b>Buy Max</b>: CTRL, SHIFT +click spell</p>
+<hr>
 <h6>Terminology</h6>
-<p>Below is a list of acronyms and terms used in Realm Grinder.
+<p>Below is a list of acronyms and terms used in Realm Grinder.</p>
 <h6>Soft-Resets & Stats</h6>
-<p><b>A Run</b>: refers to a game between abdications.
-<p><b>A Session</b>: refers to a game between reincarnations.
-<p><b>This game and Current game</b>: started last abdication, and ends next abdication.
-<p><b>Total</b>: refers to the current reincarnation.
-<p><b>All-Time</b>: covers all reincarnations.
+<p><b>A Run</b>: refers to a game between abdications.</p>
+<p><b>A Session</b>: refers to a game between reincarnations.</p>
+<p><b>This game and Current game</b>: started last abdication, and ends next abdication.</p>
+<p><b>Total</b>: refers to the current reincarnation.</p>
+<p><b>All-Time</b>: covers all reincarnations.</p>
+<hr>
 <h6>Commonly Used Abbreviations</h6>
-<p><b>A1</b>: Ascension R40-R99
-<p><b>A2</b>: Ascension R100+
-<p><b>Pre A</b>: Before R40
-<p><b>Post A</b>: After R39 and R99
-<p><b>R#</b>: Number of times reincarnated (eg: R12 = 12th reincarnation)
-<p><b>FC</b>: Faction Coin
-<p><b>RE</b>: Royal Exchange
-<p><b>RP</b>: Research Point
-<p><b>RUS</b>: Research Upgrade Slot
-<p><b>m/s or mps</b>: Mana per Second
-<p><b>BL</b>: Bloodline
-<p><b>U (Build Name)</b>: Builds that target Unique Buildings
-<p><b>NU (Build Name)</b>:Builds that do not target Unique Buildings
-<p><b>RNG</b>: Random Number Generator
-<p><b>Merc or Mercs</b>: Mercenary
+<p><b>A1</b>: Ascension R40-R99</p>
+<p><b>A2</b>: Ascension R100+</p>
+<p><b>Pre A</b>: Before R40</p>
+<p><b>Post A</b>: After R39 and R99</p>
+<p><b>R#</b>: Number of times reincarnated (eg: R12 = 12th reincarnation)</p>
+<p><b>FC</b>: Faction Coin</p>
+<p><b>RE</b>: Royal Exchange</p>
+<p><b>RP</b>: Research Point</p>
+<p><b>RUS</b>: Research Upgrade Slot</p>
+<p><b>m/s or mps</b>: Mana per Second</p>
+<p><b>BL</b>: Bloodline</p>
+<p><b>U (Build Name)</b>: Builds that target Unique Buildings</p>
+<p><b>NU (Build Name)</b>:Builds that do not target Unique Buildings</p>
+<p><b>RNG</b>: Random Number Generator</p>
+<p><b>Merc or Mercs</b>: Mercenary</p>
+<hr>
 <h6>Research Facilities</h6>
-<p><b>Short and long names</b>
-<p><b>S</b>=Spellcraft, <b>C</b>=Craftsmanship, <b>D</b>=Divine, <b>E</b>=Economics, <b>A</b>=Alchemy, and <b>W</b>=Warfare,
+<p><b>Short and long names</b></p>
+<p><b>S</b>=Spellcraft, <b>C</b>=Craftsmanship, <b>D</b>=Divine, <b>E</b>=Economics, <b>A</b>=Alchemy, and <b>W</b>=Warfare,</p>
 <h6>Factions</h6>
-<p><b>Faction Name +ine or +line</b>: Bloodline (eg: Angelline/Angeline for Angel Bloodline)
-<p><b>Fairy (3,2)</b>: Simplifies notation of Faction upgrades. The first number represents the tier, and the second the position.
-<p>For combined Faction names, the prestiges' precede and is shortened to whatever sounds best.
-<p><b>Dwangels</b>: Dwarf+Angel
+<p><b>Faction Name +line</b>: Bloodline (eg: Angelline/Angeline for Angel Bloodline)</p>
+<p><b>Fairy (3,2)</b>: Simplifies notation of Faction upgrades. The first number represents the tier, and the second the position.</p>
+<p>For combined Faction names, the prestiges' precede and is shortened to whatever sounds best.</p>
+<p><b>Dwangels</b>: Dwarf+Angel</p>
 <p><b>Dromon</b>: Drow+Demon
-<p><b>Draguid</b>: Dragon+Druid
+<p><b>Draguid</b>: Dragon+Druid</p>
+<hr>
 <h6>Mercenary Templates</h6>
-<p>Faction abbreviations used in Merc Build templates to identify faction upgrades. These are followed by the numbers 1-9 reflecting their overall position. (eg: Dwarf(3,1) Battlehammers = DN7)
-<p><b>FR</b>: Fairy
-<p><b>EL</b>: Elf
-<p><b>AN</b>: Angel
-<p><b>GB</b>: Goblin
-<p><b>UD</b>: Undead
-<p><b>DM</b>: Demon
-<p><b>TT</b>: Titan
-<p><b>DD</b>: Druid
-<p><b>FC</b>: Faceless
-<p><b>DN</b>: Dwarf
-<p><b>DW</b>: Drow
-<p><b>MC</b>: Mercenary
-<p><b>DG</b>: Dragon
+<p>Faction abbreviations used in Merc Build templates to identify faction upgrades. These are followed by the numbers 1-9 reflecting their overall position. (eg: Dwarf(3,1) Battlehammers = DN7)</p>
+<p><b>FR</b>: Fairy</p>
+<p><b>EL</b>: Elf</p>
+<p><b>AN</b>: Angel</p>
+<p><b>GB</b>: Goblin</p>
+<p><b>UD</b>: Undead</p>
+<p><b>DM</b>: Demon</p>
+<p><b>TT</b>: Titan</p>
+<p><b>DD</b>: Druid</p>
+<p><b>FC</b>: Faceless</p>
+<p><b>DN</b>: Dwarf</p>
+<p><b>DW</b>: Drow</p>
+<p><b>MC</b>: Mercenary</p>
+<p><b>DG</b>: Dragon</p>
+<hr>
 <h6>Spells</h6>
-<p>Spells are shortened to their initials. Relies on context to differentiate identical initials.
-<p><b>Pentacast</b>: Casting 5 spells at once.
-<p><b>Combo-Spell/Spell Combo</b>: Several spells or casting several spells (e.g.: "Do a combo-spell")
-<p><b>TC</b>: Tax Collection
-<p><b>CtA</b>: Call to Arms
-<p><b>SS</b>: Spiritual Surge
-<p><b>HL</b>: Holy Light
-<p><b>BF</b>: Blood Frenzy
-<p><b>GG</b>: Gem Grinder
-<p><b>FC</b>: Fairy Chanting (Not to be confused with Faction Coins)
-<p><b>MB</b>: Moon Blessing
-<p><b>GH</b>: God's Hand
-<p><b>GG</b>: Goblin's Greed
-<p><b>NT</b>: Night Time
-<p><b>HB-HFB</b>: Hellfire Blast
-<p><b>LS</b>: Lightning Strike
-<p><b>GB</b>: Grand Balance
-<p><b>BW</b>: Brainwave
-<p><b>DP</b>: Diamond Pickaxe
-<p><b>CS</b>: Combo Strike
-<p><b>DB</b>: Dragon Breath
-<p>Tiered spells simply have the tier number following the spell's initials (eg: LS1, GG5, CtA5 ...).
+<p>Spells are shortened to their initials. Relies on context to differentiate identical initials.</p>
+<p><b>Pentacast</b>: Casting 5 spells at once.</p>
+<p><b>Combo-Spell/Spell Combo</b>: Several spells or casting several spells (e.g.: "Do a combo-spell")</p>
+<p><b>TC</b>: Tax Collection</p>
+<p><b>CtA</b>: Call to Arms</p>
+<p><b>SS</b>: Spiritual Surge</p>
+<p><b>HL</b>: Holy Light</p>
+<p><b>BF</b>: Blood Frenzy</p>
+<p><b>GG</b>: Gem Grinder</p>
+<p><b>FC</b>: Fairy Chanting (Not to be confused with Faction Coins)</p>
+<p><b>MB</b>: Moon Blessing</p>
+<p><b>GH</b>: God's Hand</p>
+<p><b>GG</b>: Goblin's Greed</p>
+<p><b>NT</b>: Night Time</p>
+<p><b>HB-HFB</b>: Hellfire Blast</p>
+<p><b>LS</b>: Lightning Strike</p>
+<p><b>GB</b>: Grand Balance</p>
+<p><b>BW</b>: Brainwave</p>
+<p><b>DP</b>: Diamond Pickaxe</p>
+<p><b>CS</b>: Combo Strike</p>
+<p><b>DB</b>: Dragon Breath</p>
+<p>Tiered spells simply have the tier number following the spell's initials (eg: LS1, GG5, CtA5 ...).</p>
+<hr>
 <h6>Acronyms</h6>
-<p><b>FIB</b>: Farms, Inns, and Blacksmiths
-<p><b>BS</b>: Blacksmith
-<p><b>DM</b>: Deep Mine
-<p><b>IS</b>: Iron Stronghold
-<p><b>AP</b>: Ancient Pyramid
-<p><b>HoL</b>: Hall of Legends
-<p><b>OH</b>: Olympian Hall (Titan's Unique building)
-<p><b>UB</b>: Unique Building (R16-R99)
-<p><b>UB2</b>: Unique Building 2 (R100+)
+<p><b>FIB</b>: Farms, Inns, and Blacksmiths</p>
+<p><b>BS</b>: Blacksmith</p>
+<p><b>DM</b>: Deep Mine</p>
+<p><b>IS</b>: Iron Stronghold</p>
+<p><b>AP</b>: Ancient Pyramid</p>
+<p><b>HoL</b>: Hall of Legends</p>
+<p><b>OH</b>: Olympian Hall (Titan's Unique building)</p>
+<p><b>UB</b>: Unique Building (R16-R99)</p>
+<p><b>UB2</b>: Unique Building 2 (R100+)</p>
 <h6>Abbreviations</h6>
-<p><b>Mona</b>: Monastery
-<p><b>Laby</b>: Labyrinth
-<p><b>Pillars</b>: Stone Pillars
-<p><b>Barracks</b>: Warrior Barracks
-<p><b>Necros</b>: Necropolis
-<p><b>Pyra</b>: Ancient Pyramids
-<p><b>Den</b>: Wyrm's Den
+<p><b>Mona</b>: Monastery</p>
+<p><b>Laby</b>: Labyrinth</p>
+<p><b>Pillars</b>: Stone Pillars</p>
+<p><b>Barracks</b>: Warrior Barracks</p>
+<p><b>Necros</b>: Necropolis</p>
+<p><b>Pyra</b>: Ancient Pyramids</p>
+<p><b>Den</b>: Wyrm's Den</p>
+<hr>
 <h6>Gift Shop</h6>
-<p><b>GotH</b>: Gift of the Heroes
-<p><b>GotK</b>: Gift of the Kings
-<p><b>GotG</b>: Gift of the Gods
+<p><b>GotH</b>: Gift of the Heroes</p>
+<p><b>GotK</b>: Gift of the Kings</p>
+<p><b>GotG</b>: Gift of the Gods</p>
 <?php include "../scripts/footer.html"; ?>
