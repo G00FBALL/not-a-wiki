565a2c49aa253293f711d93d4c66e304cbfdaed5
Kardan1123 <39067428+Kardan1123@users.noreply.github.com>
2018-05-27 01:05:25 +0300

    Create index.php

---
+++ tips/index.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN"
+"http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml" lang="en" xml:lang="en">
+<head>
+<?php include "../scripts/header.html"; ?>
+<p><b>Game's window relationships</b></p>
+<p>While your game is not in active tab or window in which the game runs is minimized, all processes slow down so it's generally 
+adviced to open a new window for a game, and run it in background but <b>not</b> minimized.</p>
+<p><b>Reseting spell timers</b></p>
+<p>You can cast spells without waiting for them to run out by exporting the game, and then importing it again.This resets all spels,
+which is especially important with spells like Brainwave, and with trophies like Prismatic mana. If you want to automate the process
+you can also set your auto-clicker to Cast a spell-&gt;Open settings-&gt;Export-&gt;Import-&gt;Close settings.</p>
+<?php include "../scripts/footer.html"; ?>
