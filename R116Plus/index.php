617cc0459ff36b15793257e7a6fb842157c07226
G00FY <realm@musicfamily.org>
2018-05-27 12:08:14 -0400

    added R116 page

---
+++ R116Plus/index.php
@@ -0,0 +1,142 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN"
+        "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml" lang="en" xml:lang="en">
+<meta http-equiv="Content-Type" content="text/html; charset=windows-1252">
+<?php include "../scripts/header.html"; ?>
+<br/>
+<h6><img src="http://musicfamily.org/realm/Factions/picks/TopPageResearch.png" alt="Spellcraft" align="middle"></h6>
+<br/>
+<br/>
+    <p><b>Updated May. 27th 2018</b>
+    <p>All builds comes from the  <a target="_blank" href="https://www.kongregate.com/forums/8945/topics/1628696?page=1"><b>megathread 3.2</b></a>  forum page</p>
+    <p><b>Note</b>: All Obsolete builds moved to  <b><a target="_blank" href="http://musicfamily.org/realm/Obsolete/">Obsolete</b></a> page
+<br/>
+<div class="shlisting">
+    <p><b>Production Builds</b></p>
+    <div class="category">
+        <div class="shelementwhole">
+            <p onclick="shohid($(this));"><b> <a href="#" onclick="return false;">Faceless Rush<font color="DarkGoldenRod"> Neutral</font></a></b></p>
+            <div class="autohide">
+                <p><b>Author</b>: Lapare
+                <p><b>Range</b>: 0 gems - 1e33 (1 Dc) Gems
+                <p><b>Faction</b>: Faceless
+                <p><b>Bloodline</b>: Dwarf
+                <p><b>Artifact Set</b>: Undead
+                      <p><button onclick="myFunction($(this))">Copy Build</button>
+                      <input type="text" value="S251,S375,S1450,S1500,S3200,C340,C1300,D245,D260,D1125,E350,E1225,E1425,A250,A300,A1200,W275,W400,W1400" id="Build">
+                  <p>S251,S375,S1450,S1500,S3200,
+                  <p>C340,C1300,
+                  <p>D245,D260,D1125,
+                  <p>E350,E1225,E1425,
+                  <p>A250,A300,A1200,
+                  <p>W275,W400,W1400
+                  <p><b>Notes</b>: Excavate for Dwarf lineage, then excavate for T7 spells.
+         </div>
+    </div>
+        <div class="shelementwhole">
+            <p onclick="shohid($(this));"><b> <a href="#" onclick="return false;">Undeadline Faceless<font color="DarkGoldenRod"> Neutral</font></a></b></p>
+            <div class="autohide">
+                <p><b>Author</b>: Artcrow
+                <p><b>Range</b>: 1e33 (1 Dc) - 1e51 (1 Sxd) Gems
+                <p><b>Faction</b>: Faceless
+                <p><b>Bloodline</b>: Undead
+                <p><b>Artifact Set</b>: Dragon, Faceless (If high (%1000+) R108 bonus)
+                      <p><button onclick="myFunction($(this))">Copy Build</button>
+                      <input type="text" value="S251,S375,S500,S1450,S1500,C340,C1300,D245,D260,D1125,E350,E1225,E1425,A120,A300,A1200,W180,W275,W1400" id="Build">
+                      <p><button onclick="myFunction($(this))">Copy Build</button>
+                      <input type="text" value="S251,S375,S500,S1450,S1500,C340,C1300,D245,D260,D1125,E350,E1225,E1425,A250,A300,A1200,W180,W275,W1400" id="Build"> If able to buy DG11
+                      <p><button onclick="myFunction($(this))">Copy Build</button>
+                      <input type="text" value="S251,S375,S400,S1450,S1500,C340,C1300,D245,D260,D1125,E350,E1225,E1425,A250,A300,A1200,W180,W275,W1400" id="Build"> S400 is stronger
+                <p>S251,S375,S500,S1450,S1500,
+                <p>C340,C1300,
+                <p>D245,D260,D1125,
+                <p>E350,E1225,E1425,
+                <p>A120,A300,A1200,
+                <p>W180,W275,W1400
+                <p><b>Notes</b>: Replace A120 with A250 if able to buy DG11.
+                <p><b>Notes</b>: Replace S500 with S400 if stronger.
+         </div>
+    </div>
+         </div>
+        <br/>
+    <p><b>Special Builds</b></p>
+    <div class="category">
+        <div class="shelementwhole">
+            <p onclick="shohid($(this));"><b> <a href="#" onclick="return false;">Beard Hair (Dwarf Artifact) Excavator build<font color="DarkGoldenRod"> Neutral</font></a></b></p>
+            <div class="autohide">
+                <p><b>Author</b>: Necrodoom
+                <p><b>Range</b>: 1e42 (1 Qad) Gems+
+                <p><b>Faction</b>: Fairy/Dwarf
+                <p><b>Bloodline</b>: Undead
+                <p><b>Artifact Set</b>: Fairy
+                      <p><button onclick="myFunction($(this))">Copy Build</button>
+                      <input type="text" value="S1,S180,S590,S3200,C10,C400,C405,C520,D225,D245,D435,E50,E135,E410,A30,A120,A495,W275,W560,W1375" id="Build">
+                <p>S1,S180,S590,S3200,
+                <p>C10,C400,C405,C520,
+                <p>D225,D245,D435,
+                <p>E50,E135,E410,
+                <p>A30,A120,A495,
+                <p>W560,W1375
+                <p><b>Notes</b>: Capable of approximately 20M assistants at 1e42 (1 Qad) gems for a 1 in 500 chance for artifact.
+         </div>
+    </div>
+        <div class="shelementwhole">
+            <p onclick="shohid($(this));"><b> <a href="#" onclick="return false;">Undeadline Faceless (W275 buff build)<font color="DarkRed"> Evil</font></a></b></p>
+            <div class="autohide">
+                <p><b>Author</b>: Necrodoom
+                <p><b>Range</b>: ?
+                <p><b>Faction</b>: Faceless/Dragon
+                <p><b>Bloodline</b>: Undead
+                <p><b>Artifact Set</b>: Dwarf
+                      <p><button onclick="myFunction($(this))">Copy Build</button>
+                      <input type="text" value="S251,S375,S400,S1450,S1500,C400,C1300,D200,D260,D1125,E350,E460,E1225,A120,A300,A1200,W275,W1375,W1400" id="Build">
+                <p>S251,S375,S400,S1450,S1500,
+                <p>C400,C1300,
+                <p>D200,D260,D1125,
+                <p>E350,E460,E1225,
+                <p>A120,A300,A1200,
+                <p>W275,W1375,W1400
+         </div>
+    </div>
+        <div class="shelementwhole">
+            <p onclick="shohid($(this));"><b> <a href="#" onclick="return false;">Faceline Titans (FC12 buff build)<font color="Blue"> Good</font></a></b></p>
+            <div class="autohide">
+                <p><b>Author</b>: Necrodoom
+                <p><b>Range</b>: 1e45 (1 Qad) Gems+
+                <p><b>Faction</b>: Titan/Dragon
+                <p><b>Bloodline</b>: Faceless
+                <p><b>Artifact Set</b>: Druid, Faceless (If stronger)
+                      <p><button onclick="myFunction($(this))">Copy Build</button>
+                      <input type="text" value="S300,S400,S1450,C251,C340,C375,C1300,C1500,D245,D350,D1125,E275,E1325,A250,A1200,W260,W1375,W1400" id="Build">
+                <p>S300,S400,S1450,
+                <p>C251,C340,C375,C1300,C1500,
+                <p>D245,D350,D1125,
+                <p>E275,E1325,
+                <p>A250,A1200,
+                <p>W260,W1375,W1400
+                <p><b>Notes</b>: Requires excavations to both set up the build and boost royal exchange count.
+                </div>
+           </div>
+               <div class="shelementwhole">
+                   <p onclick="shohid($(this));"><b> <a href="#" onclick="return false;">Angelline Titans (FC12 buff build)<font color="DarkGoldenRod"> Neutral</font></a></b></p>
+                   <div class="autohide">
+                       <p><b>Author</b>: Necrodoom
+                       <p><b>Range</b>: ? Gems+
+                       <p><b>Requirements</b>: Angel Lineage Level 30
+                       <p><b>Faction</b>: Titan/Dragon
+                       <p><b>Bloodline</b>: Angel
+                       <p><b>Artifact Set</b>: Druid, Faceless (if stronger)
+                             <p><button onclick="myFunction($(this))">Copy Build</button>
+                             <input type="text" value="S300,S400,S1450,C251,C340,C375,C1300,C1500,D245,D350,D1125,E275,E1325,A250,A1200,W260,W1375,W1400" id="Build">
+                       <p>S300,S400,S1450,
+                       <p>C251,C340,C375,C1300,C1500,
+                       <p>D245,D350,D1125,
+                       <p>E275,E1325,
+                       <p>A250,A1200,
+                       <p>W260,W1375,W1400
+                       <p><b>Notes</b>: Requires excavations to both set up the build and boost royal exchange count.
+         </div>
+    </div>
+        </div>
+       </div>
+<?php include "../scripts/footer.html"; ?>
