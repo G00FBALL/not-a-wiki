815304c118a0a3cb3c0a477aac0fea6f87b1978a
G00FY <realm@musicfamily.org>
2018-08-18 06:06:14 -0400

    updated R111-R115

--- R111-R115/index.php
+++ R111-R115/index.php
@@ -43,7 +43,7 @@
                       <p><button onclick="myFunction($(this))">Copy Build</button>
                       <input type="text" value="S1,S180,S3200,C10,C340,D225,D245,E50,E1325,A30,A120,W1375" id="Build">
                       <p><button onclick="myFunction($(this))">Copy Build</button>
-                      <input type="text" value="S1,S180,S3200,C10,C340,D225,D245,E50,E400,A30,A120,W1375" id="Build"> If R115
+                      <input type="text" value="S1,S180,S3200,C10,C340,D225,D245,E50,E400,A30,A120,W1375" id="Build"> R115
                 <p>S1,S180,S3200,
                 <p>C10,C340,
                 <p>D225,D245,
@@ -51,6 +51,7 @@
                 <p>A30,A120,
                 <p>W1375
                 <p><b>Notes</b>: Replace E1325 with E400 if R115.
+                <p><b>Notes</b>: Maintaining high FR10 is recommended.
                 </div>
            </div>
                <div class="shelementwhole">
@@ -69,31 +70,8 @@
                        <p>E350,E1425,
                        <p>A250,A300,
                        <p>W275,W1375
-                       <p><b>Notes</b>: Takes estimated time of 2-5 hours to SS7.
+                       <p><b>Notes</b>: Takes estimated time of 45 minutes - 2 hours to SS7.
                        <p><b>Notes</b>: Do W275 buffing before running this build.
-                       </div>
-                  </div>
-                      <div class="shelementwhole">
-                          <p onclick="shohid($(this));"><b> <a href="#" onclick="return false;">Fairy SS7<font color="Blue"> Good</font></a></b></p>
-                          <div class="autohide">
-                              <p><b>Author</b>: Artcrow
-                              <p><b>Range</b>: 1e37 (10 Ud) - 1e42 (1 Td) Gems
-                              <p><b>Faction</b>: Fairy
-                              <p><b>Bloodline</b>: Undead
-                              <p><b>Artifact Set</b>: Fairy
-                                    <p><button onclick="myFunction($(this))">Copy Build</button>
-                                    <input type="text" value="S1,S180,S3200,C10,C400,D225,D245,E50,E400,A30,A120,W1375" id="Build">
-                                    <p><button onclick="myFunction($(this))">Copy Build</button>
-                                    <input type="text" value="S1,S180,S3200,C10,C340,D225,D245,E50,E400,A30,A120,W1375" id="Build"> Sunforce FC active
-                              <p>S1,S180,S3200,
-                              <p>C10,C400,
-                              <p>D225,D245,
-                              <p>E50,E400,
-                              <p>A30,A120,
-                              <p>W1375
-                              <p><b>Notes</b>: Takes estimated time of 3 hours to SS7.
-                              <p><b>Notes</b>: Swap C400 with C340 if sunforce FC is active.
-                              <p><b>Notes</b>: Build depends on Art of the Crow and high excavs.
          </div>
     </div>
         <div class="shelementwhole">
@@ -112,7 +90,7 @@
                 <p>E350,E1425,
                 <p>A250,A300,
                 <p>W275,W1375
-                <p><b>Notes</b>: Takes estimated time of 2-5 hours to SS7.
+                <p><b>Notes</b>: Takes estimated time of 45 minutes - 1.5 hours to SS7.
                 <p><b>Notes</b>: Do W275 buffing before running this build.
                 </div>
     </div>
@@ -137,7 +115,7 @@
                 <p>W225,W1375
                 <p><b>Notes</b>: Takes estimated time of 1.5-2 days to SS7.
                 <p><b>Notes</b>: Buffing Elf set, S400 and max excavation with E290 before running this build is highly recommended.
-                <p><b>Notes</b>: Swap C400 with C340 if high C340 and longer runs (3-5 days to SS7)
+                <p><b>Notes</b>: Swap C400 with C340 if high C340 and longer runs (2-4(?) days to SS7)
               </div>
     </div>
     </div>
@@ -153,16 +131,40 @@
                 <p><b>Bloodline</b>: Undead
                 <p><b>Artifact Set</b>: Undead
                       <p><button onclick="myFunction($(this))">Copy Build</button>
-                      <input type="text" value="S251,S375,S400,C400,D200,D260,E350,E1325,A120,A300,W275,W1375" id="Build">
-                <p>S251,S375,S400,
+                      <input type="text" value="S251,S375,S500,C400,D200,D260,E350,E1325,A120,A300,W275,W1375" id="Build">
+                      <p><button onclick="myFunction($(this))">Copy Build</button>
+                      <input type="text" value="S251,S375,S400,C400,D200,D260,E350,E1325,A120,A300,W275,W1375" id="Build"> S400 Buffed
+                <p>S251,S375,S500,
                 <p>C400,
                 <p>D200,D260,
                 <p>E350,E1325,
                 <p>A120,A300,
                 <p>W275,W1375
                 <p><b>Notes</b>: Performance varies based on faction times, but needs very little time to beat most W275 buff variants.
+                <p><b>Notes</b>: Swap artifact to Druid (or Faceless, if stronger) if able to afford Art of the Crow (1e37 (10 Ud) gems+) with SS7 (45 minutes - 1.5 hours wait).
          </div>
     </div>
+    <div class="shelementwhole">
+        <p onclick="shohid($(this));"><b> <a href="#" onclick="return false;">Undeadline Faceless (W275 buff) (1e37 (10 Ud) Gems+)<font color="DarkGoldenRod"> Neutral</font></a></b></p>
+        <div class="autohide">
+            <p><b>Author</b>: Necrodoom
+            <p><b>Range</b>: ? Gems
+            <p><b>Faction</b>: Faceless
+            <p><b>Bloodline</b>: Undead
+            <p><b>Artifact Set</b>: Druid, Faceless (if stronger)
+                  <p><button onclick="myFunction($(this))">Copy Build</button>
+                  <input type="text" value="S251,S375,S500,C400,D200,D260,E350,E1325,A120,A300,W275,W1375" id="Build">
+                  <p><button onclick="myFunction($(this))">Copy Build</button>
+                  <input type="text" value="S251,S375,S400,C400,D200,D260,E350,E1325,A120,A300,W275,W1375" id="Build"> S400 Buffed
+            <p>S251,S375,S500,
+            <p>C400,
+            <p>D200,D260,
+            <p>E350,E1325,
+            <p>A120,A300,
+            <p>W275,W1375
+            <p><b>Notes</b>: Performance varies baRequires SS7 to afford Art of the Crow trophy (30 min - 1.5 hours wait).
+     </div>
+</div>
         <div class="shelementwhole">
             <p onclick="shohid($(this));"><b> <a href="#" onclick="return false;">Dragonline Goblins (S400 buff)<font color="DarkRed"> Evil</font></a></b></p>
             <div class="autohide">
@@ -203,25 +205,6 @@
                 <p>A400,
                 <p>W10,W1375
                 <p><b>Notes</b>: Swap E1325 with E135 if R115.
-         </div>
-    </div>
-        <div class="shelementwhole">
-            <p onclick="shohid($(this));"><b> <a href="#" onclick="return false;">Dwarfline Angels (FC12 buff)<font color="Blue"> Good</font></a></b></p>
-            <div class="autohide">
-                <p><b>Author</b>: Jgchan29
-                <p><b>Range</b>: 1e45 (1 Qad) Gems+
-                <p><b>Faction</b>: Angel
-                <p><b>Bloodline</b>: Dwarf
-                <p><b>Artifact Set</b>: Angel, Elf (if buffed)
-                      <p><button onclick="myFunction($(this))">Copy Build</button>
-                      <input type="text" value="S10,S400,C25,C340,D1,D150,D175,E1325,A50,A250,W225,W1375" id="Build">
-                <p>S10,S400,
-                <p>C25,C340,
-                <p>D1,D150,D175,
-                <p>E1325,
-                <p>A50,A250,
-                <p>W225,W1375
-                <p><b>Notes</b>: Build set up is up to buying Art of the Crow.
                 </div>
            </div>
         <div class="shelementwhole">
@@ -237,7 +220,7 @@
                 <p>S251,S375,S400,
                 <p>C400,
                 <p>D200,D260,
-                <p>E320,E350,
+                <p>E145,E350,
                 <p>A300,A3400,
                 <p>W275,W1375
                 <p><b>Notes</b>: Significally buffed by sunforce faction coins.
