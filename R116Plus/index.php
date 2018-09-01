2add6fde3b5a65ee00e25ca16bdcdea727b6aa61
Necrodoom <doomed.war@gmail.com>
2018-09-02 00:03:43 +0300

    Multiple corrections

--- R116Plus/index.php
+++ R116Plus/index.php
@@ -40,7 +40,7 @@
                 <p><b>Range</b>: 1e33 (1 Dc) - 1e56 (100 SpD) Gems
                 <p><b>Faction</b>: Faceless
                 <p><b>Bloodline</b>: Undead
-                <p><b>Artifact Set</b>: Dragon, Faceless (If high (%1000+) R108 bonus)
+                <p><b>Artifact Set</b>: Dragon, Faceless (If FC12, Faceless Set and R108 power add up to x1000(?) and above)
                       <p><button onclick="myFunction($(this))">Copy Build</button>
                       <input type="text" value="S251,S375,S500,S1450,S1500,C340,C1300,D245,D260,D1125,E350,E1225,E1425,A250,A300,A1200,W180,W275,W1400" id="Build">
                       <p><button onclick="myFunction($(this))">Copy Build</button>
@@ -90,8 +90,6 @@
                               <p><b>Artifact Set</b>: Undead
                                     <p><button onclick="myFunction($(this))">Copy Build</button>
                                     <input type="text" value="S225,S435,S3200,C340,D25,D245,D560,E10,E400,E495,A1,A175,A400,A410,A590,W50,W205,W405,W520" id="Build">
-                                    <p><button onclick="myFunction($(this))">Copy Build</button>
-                                    <input type="text" value="S225,S400,S435,C340,D25,D245,D560,E10,E400,E495,A1,A175,A400,A410,A590,W50,W205,W405,W520" id="Build"> S400 Buffed
                               <p>S225,S435,S3200,
                               <p>C340,
                               <p>D25,D245,D560,
@@ -99,7 +97,6 @@
                               <p>A1,A175,A400,A410,A590,
                               <p>W50,W205,W405,W520
                               <p><b>Notes</b>: Several hours setup time, grower build.
-                              <p><b>Notes</b>: Swap S3200 with S400 if buffed.
                        </div>
                   </div>
                       <div class="shelementwhole">
@@ -119,12 +116,12 @@
                               <p>E25,E400,E410,
                               <p>A120,A495,
                               <p>W10,W560,W1375
-                              <p><b>Notes</b>: S400 is required before running this build.
-                              <p><b>Notes</b>: Swap C400 with C340 for longer runs (1 day+).
+                              <p><b>Notes</b>: Buffing S400 is required before running this build.
+                              <p><b>Notes</b>: Swap C400 with C340 for longer runs (6 hours+).
                        </div>
                   </div>
                       <div class="shelementwhole">
-                          <p onclick="shohid($(this));"><b> <a href="#" onclick="return false;">Fairyline Druid (R120+)<font color="DarkGoldenRod"> Neutral</font></a></b> Obsolete?</p>
+                          <p onclick="shohid($(this));"><b> <a href="#" onclick="return false;">Fairyline Druid (R120+) (obsolete?)<font color="DarkGoldenRod"> Neutral</font></a></b> Obsolete?</p>
                           <div class="autohide">
                               <p><b>Author</b>: Rellikrellik
                               <p><b>Range</b>: 1e61 (10 Nod) Gems
@@ -181,17 +178,14 @@
                       <input type="text" value="S1,S180,S590,S3200,C10,C400,C405,C520,D225,D245,D435,E50,E135,E410,A30,A120,A495,W560,W1375" id="Build">
                       <p><button onclick="myFunction($(this))">Copy Build</button>
                       <input type="text" value="S1,S180,S400,S590,C10,C400,C405,C520,D225,D245,D435,E50,E135,E410,A30,A120,A495,W560,W1375" id="Build"> S400 Buffed
-                      <p><button onclick="myFunction($(this))">Copy Build</button>
-                      <input type="text" value="S1,S180,S400,S590,C10,C400,C405,C520,D225,D245,D435,E50,E410,E460,A30,A120,A495,W560,W1375" id="Build"> Fairy AH
                 <p>S1,S180,S590,S3200,
                 <p>C10,C400,C405,C520,
                 <p>D225,D245,D435,
                 <p>E50,E135,E410,
                 <p>A30,A120,A495,
                 <p>W560,W1375
-                <p><b>Notes</b>: Can afford DN12 at 1e63 (1 Vg)(?) gems+.
+                <p><b>Notes</b>: Can afford DN12 at 1e66 (1 UVg)(?) gems+.
                 <p><b>Notes</b>: Replace S3200 with S400 if buffed.
-                <p><b>Notes</b>: Replace E135 with E460 if Fairy AH and/or Faceless AH are available and A120 is less than 6x stronger than them.
          </div>
     </div>
     <div class="shelementwhole">
@@ -213,6 +207,7 @@
             <p>A10,A120,A410,
             <p>W25,W205,W405,W520
             <p><b>Notes</b>: At higher gems (1e53 (100 SxD) gems+), can swap C340 with C250 for lineage building.
+            <p><b>Notes</b>: Swap Dwarf artifact with Undead at 1e64 (10 UVg) gems+.
      </div>
 </div>
 <div class="shelementwhole">
@@ -271,7 +266,7 @@
 <p>A50,A120,A495,
 <p>W225,W560,W1375
 <p><b>Notes</b>: Buffing S400, DN12, excavation count will greatly benefit this build.
-<p><b>Notes</b>: Time to Mana Spring is 2-7 days (highly dependant on buffs).
+<p><b>Notes</b>: Time to Mana Spring is ? days (highly dependant on buffs).
              </div>
     </div>
          </div>
@@ -301,9 +296,9 @@
                 <p>E145,E350,E1225,
                 <p>A300,A1200,A3400,
                 <p>W275,W1375,W1400
+                <p><b>Notes</b>: Use the more specialized builds below if possible.
                 <p><b>Notes</b>: Swap E145 with E320 when leveling Goblin Lineage.
                 <p><b>Notes</b>: Swap D200 with D330 if Druid AH is available and is at least 95%, or if leveling anything but Titan and Undead and able to be always online.
-                <p><b>Notes</b>: Use the more specialized builds below if possible.
                 <p><b>Notes</b>: Buffing W275 and S400 is highly recommended.
                 <p><b>Notes</b>: Leveling Undead to L30 first is fastest.
          </div>
@@ -322,14 +317,15 @@
           <p><button onclick="myFunction($(this))">Copy Build</button>
           <input type="text" value="S50,S400,S435,C175,C250,D245,D560,E1,E135,E225,E495,E590,A10,A120,A410,W25,W205,W405,W520" id="Build">
           <p><button onclick="myFunction($(this))">Copy Build</button>
-          <input type="text" value="S50,S400,S435,C175,D200,C250,D560,E1,E135,E225,E495,E590,A10,A120,A410,W25,W205,W405,W520" id="Build"> 1e63 (1 Vg) (Angel, Dragon)
+          <input type="text" value="S50,S400,S435,C175,C250,D200,D560,E1,E135,E225,E495,E590,A10,A120,A410,W25,W205,W405,W520" id="Build"> D200 swap
     <p>S50,S400,S435,
     <p>C175,C250,
     <p>D245,D560,
     <p>E1,E135,E225,E495,E590,
     <p>A10,A120,A410,
     <p>W25,W205,W405,W520
-    <p><b>Notes</b>: Swap D245 with D200 at atleast 1e63 (1 Vg) gems+ (Angel, Dragon) / 1e64 (10 Vg) gems+ (Angel) / 1e66 (1 UVg) gems+ (Druid) / 1e68 (100 UVg)(?) gems+ (Faceless)
+    <p><b>Notes</b>: Swap W205 with W1375 for Faceless Lineage leveling.
+    <p><b>Notes</b>: Swap D245 with D200 at atleast 1e63 (1 Vg) gems+ (Angel, Dragon) / 1e64 (10 Vg) gems+ (Angel) / 1e66 (1 UVg) gems+ (Druid) / 1e69 (1 DVg)(?) gems+ (Faceless)
          </div>
     </div>
     <div class="shelementwhole">
