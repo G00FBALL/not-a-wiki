b6a5fc1df8e4021ee8849194def7a2dfd8440f6f
G00FY <realm@musicfamily.org>
2018-12-21 07:00:53 -0500

    added new link

---
+++ BrowserIssues/index.php
@@ -0,0 +1,9 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN"
+"http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml" lang="en" xml:lang="en">
+<head>
+<?php include "../scripts/header.html"; ?>
+    <h6><a target="_blank" research="View all games on Kongregate" href="https://www.silvergames.com/en/"><img src="http://musicfamily.org/realm/Factions/picks/kongregate_anthill.png" alt="TrophiesTopPage" align="middle"></h6>
+<br/>
+<p><b><a target="_blank" href="https://www.kongregate.com/forums/1-kongregate/topics/1783434-flash-games-do-not-save-on-firefox-for-windows-solution-inside">Flash games do not save on Firefox for Windows - Solution inside</a></b></p>
+<?php include "../scripts/footer.html"; ?>
