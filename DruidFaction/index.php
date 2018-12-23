32cb3ca05b7673af6bcd2437b6ac2021be77fe5f
Necrodoom <doomed.war@gmail.com>
2018-12-23 19:56:44 +0200

    fix GB formula

--- DruidFaction/index.php
+++ DruidFaction/index.php
@@ -12,7 +12,7 @@
 <p><b>Alignment</b>: Neutral</p>
 <p><b>Faction spell</b>: Grand Balance</p>
 <p><b>Effect</b>: Increase the production of your least productive building based on the amount of the three most built ones for 20 seconds.</p>
-<p><b>Formula</b>: round(0.25*(x*y*z/729)^0.9) where x,y, and z are the number of your three most-built buildings.</p>
+<p><b>Formula</b>: 25 * (x * y * z / (A + 3) ^ 6) ^ 0.9 where x, y, and z are the number of your three most-built buildings.</p>
 <p><b>Spell Trophy & Upgrade</b>: <img src="http://musicfamily.org/realm/Factions/picks/HallsofBalanceFactionUpgrade.png"> <b>Halls of Balance</b></p>
 <p><b>Effect</b>: Increases Grand Balance duration to 24 seconds and lower its cost by 200 mana.</p>
 <p><b>Requirements</b>: Have Grand Balance affect Hall of Legends.</p>
