104b42e2792a6dc94b02a0721d81ab2c25a7a5a3
G00FY <realm@musicfamily.org>
2018-12-30 06:19:53 -0500

    add R125

---
+++ R125Plus/index.php
@@ -0,0 +1,68 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN"
+        "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml" lang="en" xml:lang="en">
+<meta http-equiv="Content-Type" content="text/html; charset=windows-1252">
+<?php include "../scripts/header.html"; ?>
+<br/>
+<h6><img src="http://musicfamily.org/realm/Factions/picks/TopPageResearch.png" alt="Spellcraft" align="middle"></h6>
+<br/>
+<br/>
+    <p><b>Updated Dec 30th 2018</b>
+    <p>All builds comes from the  <a target="_blank" href="https://www.kongregate.com/forums/8945-realm-grinder/topics/1787989-3-4-3-build-megathread-under-construction?page=1"><b>megathread</b></a>  forum page</p>
+<br/>
+<div class="shlisting">
+    <p><b>Production Builds</b></p>
+    <div class="category">
+        <div class="shelementwhole">
+            <p onclick="shohid($(this));"><b> <a href="#" onclick="return false;">Elfline Faceless<font color="DarkGoldenRod"> Neutral</font></a></b></p>
+            <div class="autohide">
+                <p><b>Author</b>: Ensteffahn
+                <p><b>Requirements</b>: Elf Lineage Level 35(?)+
+                <p><b>Range</b>: 0 - 1e63 (1 Vg) Gems
+                <p><b>Faction</b>: Faceless/Dragon/Djinn
+                <p><b>Bloodline</b>: Elf
+                <p><b>Artifact Set</b>: Druid
+                  <p><button onclick="myFunction($(this))">Copy Build</button>
+                  <input type="text" value="S251,S375,S1450,S1500,S3200,C400,C1300,D245,D260,D1125,E350,E1225,E1425,A250,A300,A1200,W275,W1375,W1400" id="Build">
+                  <p><button onclick="myFunction($(this))">Copy Build</button>
+                  <input type="text" value="S251,S375,S1450,S1500,S3200,C1300,C3100,D245,D260,D1125,E350,E1225,E1425,A250,A300,A1200,W275,W1375,W1400" id="Build"> W275 Buffed
+                  <p><button onclick="myFunction($(this))">Copy Build</button>
+                  <input type="text" value="S251,S375,S1450,S1500,S3200,C340,C1300,D245,D260,D1125,E350,E1225,E1425,A250,A300,A1200,W275,W1375,W1400" id="Build"> C340</p>
+                <br/>
+                <p>S251,S375,S1450,S1500,S3200,
+                <p>C400,C1300,
+                <p>D245,D260,D1125,
+                <p>E350,E1225,E1425,
+                <p>A250,A300,A1200,
+                <p>W275,W1375,W1400
+                <p><b>Notes</b>: Replace C400 with C3100 after buffing W275.
+                <p><b>Notes</b>: Replace C3100 with C340 if C340 is strong enough (?)
+                <p><b>Notes</b>: Buffing W275 and FC12 is highly recommended.
+                <p><b>Notes</b>: Target Maelstrom at Wyrm's den for maximum production.
+         </div>
+    </div>
+        <div class="shelementwhole">
+            <p onclick="shohid($(this));"><b> <a href="#" onclick="return false;">Fairyline Faceless<font color="DarkGoldenRod"> Neutral</font></a></b></p>
+            <div class="autohide">
+                <p><b>Author</b>: Ensteffahn
+                <p><b>Range</b>: 1e63 (1 Vg) Gems
+                <p><b>Requirements</b>: Fairy Lineage Level 35
+                <p><b>Faction</b>: Faceless/Dragon/Djinn
+                <p><b>Bloodline</b>: Fairy
+                <p><b>Artifact Set</b>: Fairy
+                  <p><button onclick="myFunction($(this))">Copy Build</button>
+                  <input type="text" value="S251,S375,S400,S1450,S1500,C340,C1300,D245,D260,D1125,E350,E1225,E1425,A250,A300,A1200,W275,W1375,W1400" id="Build">
+                <p>S251,S375,S400,S1450,S1500,
+                <p>C340,C1300,
+                <p>D245,D260,D1125,
+                <p>E350,E1225,E1425,
+                <p>A250,A300,A1200,
+                <p>W275,W1375,W1400
+                <p><b>Notes</b>: Buffing W275 and FC12 is highly recommended.
+                <p><b>Notes</b>: Target Maelstrom at Dragon Pasture for maximum production
+
+         </div>
+    </div>
+         </div>
+    </div>
+<?php include "../scripts/footer.html"; ?>
