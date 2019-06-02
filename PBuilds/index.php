2068415697e13d0605a8638eeaf1bfbbbcd09832
xpt14 <43166103+xpt14@users.noreply.github.com>
2019-06-02 12:39:15 +0700

    Update builds

--- PBuilds/index.php
+++ PBuilds/index.php
@@ -5,25 +5,24 @@
     <?php include "../scripts/header.html"; ?>
     <br/>
     <h6><img src="http://musicfamily.org/realm/Factions/picks/TopPageResearch.png" alt="Spellcraft" align="middle"></h6><br/> <br/>
-    <p><b>Updated June 1st, 2019</b></p><br/>
+    <p><b>Updated June 2nd, 2019</b></p><br/>
     <p><b>Note</b>: Factions order is Angels→Dwangels(no researches)→Dwairies(R30-33) or Dwangels(R34-39)</p>
     <div class="shlisting">
-        <p><b>R29-33</b></p>
         <div class="category">
             <div class="shelementwhole">
-                <p onclick="shohid($(this));"><b><a href="#" onclick="return false;">U-Dwairies Transition R30-33</a></b></p>
+                <p onclick="shohid($(this));"><b><a href="#" onclick="return false;">An Angel's Apprentice R30-39</a></b></p>
                 <div class="autohide">
                     <p><b>Author</b>: Cyden</p>
-                    <p><b>Faction</b>: Fairy/Dwarf</p>
-                    <p><b>Bloodline</b>: Fairy</p>
-                    <p><b>Range</b>: e66 (1 Uvg) - e90 (1 Novg) Gems</p>
+                    <p><b>Faction</b>: Angel/Dwarf</p>
+                    <p><b>Bloodline</b>: Druid</p>
+                    <p><b>Range</b>: e66 (1 Uvg) - e93 (1 Tg) Gems</p>
                     <p><button onclick="myFunction($(this))">Copy Build</button>
-                    <input type="text" value="S135,S180,S200,S215,S330,C80,C120,C150,C250,C330,D55,D225,D245,D290,D320,E50,E135,E150,E200,E330,A30,A105,A120,A150,A250,W150,W180,W290,W320,W330" id="Build">
-                    <p>S135,S180,S200,S215,S330,</p>
-                    <p>C80,C120,C150,C250,C330,</p>
-                    <p>D55,D225,D245,D290,D320,</p>
-                    <p>E50,E135,E150,E200,E330,</p>
-                    <p>A30,A105,A120,A150,A250,</p>
+                    <input type="text" value="S135,S150,S200,S215,S330,C105,C120,C150,C250,C330,D1,D55,D245,D290,D320,E80,E135,E150,E200,E330,A50,A105,A120,A150,A330,W150,W180,W290,W320,W330" id="Build">
+                    <p>S135,S150,S200,S215,S330,</p>
+                    <p>C105,C120,C150,C250,C330,</p>
+                    <p>D1,D55,D245,D290,D320,</p>
+                    <p>E80,E135,E150,E200,E330,</p>
+                    <p>A50,A105,A120,A150,A330,</p>
                     <p>W150,W180,W290,W320,W330</p>
                 </div>
             </div>
@@ -33,7 +32,7 @@
                     <p><b>Author</b>: Lapare</p>
                     <p><b>Faction</b>: Fairy/Dwarf</p>
                     <p><b>Bloodline</b>: Fairy</p>
-                    <p><b>Range</b>: 1e113 (100 SxTg) gems - ?</p>
+                    <p><b>Range</b>: 1e93 (1 Tg) gems - ?</p>
                     <p><button onclick="myFunction($(this))">Copy Build</button>
                     <input type="text" value="S135,S180,S200,S215,S330,C80,C120,C150,C330,C400,D55,D225,D245,D290,D320,E50,E135,E150,E330,E410,A105,A120,A150,A330,A400,W150,W180,W290,W320,W330" id="Build">
                     <p><button onclick="myFunction($(this))">Copy Build</button>
@@ -56,7 +55,7 @@
                     <p><b>Author</b>: Lapare</p>
                     <p><b>Faction</b>: Fairy/Dwarf</p>
                     <p><b>Bloodline</b>: Fairy</p>
-                    <p><b>Range</b>: 1e90 (1 Novg) gems - ?</p>
+                    <p><b>Range</b>: 1e93 (1 Tg) gems - ?</p>
                     <p><button onclick="myFunction($(this))">Copy Build</button>
                     <input type="text" value="S135,S180,S200,S215,S330,C80,C120,C150,C330,C400,D55,D225,D245,D290,D320,E50,E135,E150,E330,E410,A105,A120,A330,A400,A495,W150,W180,W290,W320,W330" id="Build">
                     <p><button onclick="myFunction($(this))">Copy Build</button>
@@ -80,7 +79,7 @@
                     <p><b>Author</b>: Cyden</p>
                     <p><b>Faction</b>: Fairy/Dwarf</p>
                     <p><b>Bloodline</b>: Fairy</p>
-                    <p><b>Range</b>: e90 (1 Novg) gems - e126 (1 Uqag) gems</p>
+                    <p><b>Range</b>: e93 (1 Tg) gems - e126 (1 Uqag) gems</p>
                     <p><button onclick="myFunction($(this))">Copy Build</button>
                     <input type="text" value="S135,S200,S215,S330,S500,C80,C120,C150,C400,C500,D55,D225,D245,D290,D320,E50,E135,E150,E330,E410,A105,A120,A150,A400,A495,W150,W180,W290,W320,W330" id="Build">
                     <p><button onclick="myFunction($(this))">Copy Build</button>
@@ -99,33 +98,13 @@
                     <p><b>Note</b>: If you don't have A400(requires 2 days as Druids) A400→A150</p>
                 </div>
             </div>
-        </div>
-        <p><b>R34-39</b></p>
-        <div class="category">
             <div class="shelementwhole">
-                <p onclick="shohid($(this));"><b><a href="#" onclick="return false;">An Angel's Apprentice</a></b></p>
+                <p onclick="shohid($(this));"><b><a href="#" onclick="return false;">An Angelic Ascension R34-39</a></b></p>
                 <div class="autohide">
                     <p><b>Author</b>: Cyden</p>
                     <p><b>Faction</b>: Angel/Dwarf</p>
                     <p><b>Bloodline</b>: Druid</p>
-                    <p><b>Range</b>: e66 (1 Uvg) - e90 (1 Novg) Gems</p>
-                    <p><button onclick="myFunction($(this))">Copy Build</button>
-                    <input type="text" value="S135,S150,S200,S215,S330,C105,C120,C150,C250,C330,D1,D55,D245,D290,D320,E80,E135,E150,E200,E330,A50,A105,A120,A150,A330,W150,W180,W290,W320,W330" id="Build">
-                    <p>S135,S150,S200,S215,S330,</p>
-                    <p>C105,C120,C150,C250,C330,</p>
-                    <p>D1,D55,D245,D290,D320,</p>
-                    <p>E80,E135,E150,E200,E330,</p>
-                    <p>A50,A105,A120,A150,A330,</p>
-                    <p>W150,W180,W290,W320,W330</p>
-                </div>
-            </div>
-            <div class="shelementwhole">
-                <p onclick="shohid($(this));"><b><a href="#" onclick="return false;">An Angelic Ascension</a></b></p>
-                <div class="autohide">
-                    <p><b>Author</b>: Cyden</p>
-                    <p><b>Faction</b>: Angel/Dwarf</p>
-                    <p><b>Bloodline</b>: Druid</p>
-                    <p><b>Range</b>: 1e90 (1 Novg) gems - 1e144 (1 Spqag) gems</p>
+                    <p><b>Range</b>: 1e93 (1 Tg) gems - 1e144 (1 Spqag) gems</p>
                     <p><button onclick="myFunction($(this))">Copy Build</button>
                     <input type="text" value="S135,S150,S215,S330,S500,C120,C150,C400,C500,C520,D55,D290,D320,D435,D590,E80,E150,E200,E330,E410,A105,A120,A150,A495,A545,W150,W290,W320,W330,W400,C250,C330,D480" id="Build">
                     <p><button onclick="myFunction($(this))">Copy Build</button>
