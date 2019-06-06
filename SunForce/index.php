b53eff3eeb76c0fce085271a3754f3dcdf2b577b
G00FY <realm@musicfamily.org>
2019-06-06 16:53:54 -0400

    fix sun force

--- SunForce/index.php
+++ SunForce/index.php
@@ -28,8 +28,8 @@
     <p><b>Formula</b>: floor(ln(1+x) ^ 1.35).</p>
       <br/>
     <p><img src="http://musicfamily.org/realm/Factions/picks/SunForce6am12pm.png" alt="SunForce12pm6pm" align="middle"><b><font color="red"> 6 AM - 12 PM</font></b></p>
-    <p><b>Effect</b>: Multipicatively increase Faction Coin find chance based on the amount of assistants you own.</p>
-    <p><b>Formula</b>: floor(0.25 * X ^ (0.4 + 0.05 * A), where x is your assistants stat and A is Ascension count.</p>
+    <p><b>Effect</b>: Increase Mana Regeneration based on the amount of assistants you own. (Additive)</p>
+    <p><b>Formula</b>: (1.75 * ln(1 + x) ^ (1.75 + 0.5 * A)), where x is amount of assistants you own.</p>
     <br/>
     <p><img src="http://musicfamily.org/realm/Factions/picks/SunForce12pm6pm.png" alt="SunForce12pm6pm" align="middle"><b><font color="red"> 12 PM - 6 PM</font></b></p>
     <p><b>Effect</b>: Increase the production of all buildings based on the amount of Faction Coins you collected in this game.</p>
