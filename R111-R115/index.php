783c58269c0128f32553c106091c29891f913a22
Necrodoom <doomed.war@gmail.com>
2019-06-15 17:28:11 +0300

    Update index.php

--- R111-R115/index.php
+++ R111-R115/index.php
@@ -16,43 +16,43 @@
         <div class="shelementwhole">
             <p onclick="shohid($(this));"><b> <a href="#" onclick="return false;">Druidline Elf + Elf Artifact Excavator build<font color="Blue"> Good</font></</a></b></p>
             <div class="autohide">
-                <p><b>Author</b>: ?</p>
-                <p><b>Range</b>: 0 Gems - 1e44 (100 TD) Gems</p>
-                <p><b>Faction</b>: Elf</p>
-                <p><b>Bloodline</b>: Druid</p>
-                <p><b>Artifact Set</b>: Elf > Angel (From left to right, if avaliable)</p>
+                <p><b>Author</b>: ?
+                <p><b>Range</b>: 0 Gems - 1e44 (100 TD) Gems
+                <p><b>Faction</b>: Elf
+                <p><b>Bloodline</b>: Druid
+                <p><b>Artifact Set</b>: Elf > Angel (From left to right, if avaliable)
                     <p><button onclick="myFunction($(this))">Copy Build</button>
-                    <input type="text" value="S175,S3200,C1,C225,C400,D50,D290,E25,E3250,A120,W10,W1375" id="Build"> 0 Gems - 1e36 (1 UD) Gems</p>
+                    <input type="text" value="S175,S3200,C1,C225,C400,D50,D290,E25,E3250,A120,W10,W1375" id="Build"> 0 Gems - 1e36 (1 UD) Gems
                     <p><button onclick="myFunction($(this))">Copy Build</button>
-                    <input type="text" value="S175,S3200,C1,C225,C400,D50,D1375,E25,E3250,A120,W10,W1375" id="Build"> 1e36 (1 UD) Gems - 1e44 (100 TD) Gems</p>
+                    <input type="text" value="S175,S3200,C1,C225,C400,D50,D1375,E25,E3250,A120,W10,W1375" id="Build"> 1e36 (1 UD) Gems - 1e44 (100 TD) Gems
                     <p><button onclick="myFunction($(this))">Copy Build</button>
-                    <input type="text" value="S175,S3200,C1,C225,C400,D50,D1375,E25,E290,A120,W10,W1375" id="Build"> Elf Artifact Excavator</p>
-                <p>S175,S3200,</p>
-                <p>C1,C225,C400,</p>
-                <p>D50,D290,</p>
-                <p>E25,E3250,</p>
-                <p>A120,</p>
-                <p>W10,W1375</p>
-                <p><b>Notes</b>: Swap D290 for D1375 at 1e36 (1 UD) Gems+.</p>
-                <p><b>Notes</b>: Swap E3250 with E290 for Elf Artifact excavating.</p>
+                    <input type="text" value="S175,S3200,C1,C225,C400,D50,D1375,E25,E290,A120,W10,W1375" id="Build"> Elf Artifact Excavator
+                <p>S175,S3200,
+                <p>C1,C225,C400,
+                <p>D50,D290,
+                <p>E25,E3250,
+                <p>A120,
+                <p>W10,W1375
+                <p><b>Notes</b>: Swap D290 for D1375 at 1e36 (1 UD) Gems+.
+                <p><b>Notes</b>: Swap E3250 with E290 for Elf Artifact excavating.
             </div>
         </div>
         <div class="shelementwhole">
             <p onclick="shohid($(this));"><b> <a href="#" onclick="return false;">Undeadline Elf<font color="Blue"> Good</font></</a></b></p>
             <div class="autohide">
-                <p><b>Author</b>: ?</p>
-                <p><b>Range</b>: 1e44 (100 TD) Gems+</p>
-                <p><b>Faction</b>: Elf</p>
-                <p><b>Bloodline</b>: Undead</p>
-                <p><b>Artifact Set</b>: Elf > Angel (From left to right, if avaliable)</p>
+                <p><b>Author</b>: ?
+                <p><b>Range</b>: 1e44 (100 TD) Gems+
+                <p><b>Faction</b>: Elf
+                <p><b>Bloodline</b>: Undead
+                <p><b>Artifact Set</b>: Elf > Angel (From left to right, if avaliable)
                     <p><button onclick="myFunction($(this))">Copy Build</button>
-                    <input type="text" value="S175,S3200,C1,C225,C400,D50,D1375,E25,E145,A120,W10,W1375" id="Build"></p>
-                <p>S175,S3200,</p>
-                <p>C1,C225,C400,</p>
-                <p>D50,D1375,</p>
-                <p>E25,E145,</p>
-                <p>A120,</p>
-                <p>W10,W1375</p>
+                    <input type="text" value="S175,S3200,C1,C225,C400,D50,D1375,E25,E145,A120,W10,W1375" id="Build">
+                <p>S175,S3200,
+                <p>C1,C225,C400,
+                <p>D50,D1375,
+                <p>E25,E145,
+                <p>A120,
+                <p>W10,W1375
             </div>
         </div>
     </div>
