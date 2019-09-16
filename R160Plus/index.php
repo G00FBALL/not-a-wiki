3c07d2e6a2dea116b07f92d9d09b3b52f3a3fed7
G00FY <realm@musicfamily.org>
2019-09-16 06:32:51 -0400

    new ppage

---
+++ R160Plus/index.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN"
+        "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml" lang="en" xml:lang="en">
+<meta http-equiv="Content-Type" content="text/html; charset=windows-1252">
+<?php include "../scripts/header.html"; ?>
+<br/>
+<h6><img src="http://musicfamily.org/realm/Factions/picks/MercenaryTopPage.png" alt="Spellcraft" align="middle"></h6>
+<br/>
+<br/>
+    <center><p><b>And you thought!!!</b></center>
+<?php include "../scripts/footer.html"; ?>
