f4d05fef25ada06ad6a2509859350939a0a935cc
G00FY <realm@musicfamily.org>
2019-09-21 05:56:25 -0400

    add build R160+

--- R160Plus/index.php
+++ R160Plus/index.php
@@ -4,8 +4,40 @@
 <meta http-equiv="Content-Type" content="text/html; charset=windows-1252">
 <?php include "../scripts/header.html"; ?>
 <br/>
-<h6><img src="http://musicfamily.org/realm/Factions/picks/MercenaryTopPage.png" alt="Spellcraft" align="middle"></h6>
+<h6><img src="http://musicfamily.org/realm/Factions/picks/MercenaryTopPage.png" align="middle"></h6>
 <br/>
-<br/>
-    <center><p><b>And you thought!!!</b></center>
+<br>
+    <br/>
+    <p><b>Production Builds</b></p>
+    <div class="category">
+        <div class="shelementwhole">
+            <p onclick="shohid($(this));"><b> <a href="#" onclick="return false;">Faceline Titans (R160-R170)<font color="darkgoldenrod"> Neutral</font></a></b></p>
+            <div class="autohide">
+                <p><b>Author</b>: Rellikrellik</p>
+                <p><b>Requirement</b>: Faceless Lineage 45</p>
+                <p><b>Faction</b>: Titan</p>
+                <p><b>Bloodline</b>: Faceless</p>
+                <p><b>Artifact Set</b>: Dragon</p>
+                <p><b>Range</b>: 0-R</p>
+                    <p><button onclick="myFunction($(this))">Copy Build</button>
+                    <input type="text" value="S300,S1450,S3200,S200,S400,S30,S305,C251,C375,C1300,C1500,C400,C340,C250,C150,C200,D350,D1125,D200,D330,D1375,D290,D3350,E275,E1225,E135,E400,E1425,E1325,E145,A1200,A400,A270,A120,A3400,A545,W260,W1400,W1375,W1275,W180,W400,W150" id="Build"></p>
+                <p>S300,S1450,S3200,S200,S400,S30,S305</p>
+                <p>C251,C375,C1300,C1500,C400,C340,C250,C150,C200,</p>
+                <p>D350,D1125,D200,D330,D1375,D290,D3350,</p>
+                <p>E275,E1225,E135,E400,E1425,E1325,E145,</p>
+                <p>A1200,A400,A270,A120,A3400,A545,</p>
+                <p>W260,W1400,W1375,W1275,W180,W400,W150</p>
+                <p><b>After Archon treaty</b>: Reload Build and after each new research slot opens.</p>
+                <p><b>Note</b>: Build is listed in order of purchase.</p>
+            </div>
+        </div>
+    </div>
+
+
+
+
+
+
+
+
 <?php include "../scripts/footer.html"; ?>
