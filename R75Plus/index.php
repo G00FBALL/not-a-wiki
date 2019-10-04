4cd9a80c5f8609de14f33f08048bab87dbe09b08
Necrodoom <doomed.war@gmail.com>
2019-10-04 14:20:36 +0300

    shorten build title

--- R75Plus/index.php
+++ R75Plus/index.php
@@ -133,7 +133,7 @@
             </div>
         </div>
         <div class="shelementwhole">
-            <p onclick="shohid($(this));"><b> <a href="#" onclick="return false;">Mercenomicon (Know Your Enemy, Part 2) (R75+)<font color="DarkGoldenRod"> Neutral</font></a></b></p>
+            <p onclick="shohid($(this));"><b> <a href="#" onclick="return false;">Mercenomicon (Know Your Enemy Part 2) (R75)<font color="DarkGoldenRod"> Neutral</font></a></b></p>
             <div class="autohide">
                 <p><b>Author</b>: Cyden</p>
                 <p><b>Range</b>: 1e87 (1 Ocvg) Gems+</p>
