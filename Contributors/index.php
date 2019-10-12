60cbec931100e06faeb32afb9ffef69c39768ce2
G00FY <realm@musicfamily.org>
2019-10-12 06:38:43 -0400

    add credits

--- Contributors/index.php
+++ Contributors/index.php
@@ -4,6 +4,8 @@
 <head>
 <?php include "../scripts/header.html"; ?>
 <h6>Thank you for all your help</h6>
+<p>mangafreak140</p>
+<p>acer4321</p>
 <p>Sas Assassin</p>
 <p>Mysticman89</p>
 <p>LeoC.II</p>
