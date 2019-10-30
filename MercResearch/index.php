03eac2f062ceb4894088091eafa5164f7eb2ceb3
Necrodoom <doomed.war@gmail.com>
2019-10-30 23:25:20 +0200

    page corrections

--- MercResearch/index.php
+++ MercResearch/index.php
@@ -87,7 +87,7 @@
 <p><b>Requirement</b>: Freemason's Hall</p>
 <p><b>Cost</b>: 1 Qaqag (1e135)</p>
 <p><b>Effect</b>: Generates additional Faction Coins per cast</p>
-<p><b>Formula</b>: (2.5*x^2.5), where x is original Faction Coin chance.</p>
+<p><b>Formula</b>: (2.5 * x ^ 2.5), where x is original Faction Coin chance.</p>
 <br/>
 <p><img src="http://musicfamily.org/realm/Factions/picks/ObsidianShardArtifact.png" alt="Secrets of the Warriors" align="middle"> <b>Obsidian Shard</b></p>
 <p><b>Requirement</b>: 8000th Excavation as any Faction, R75+</p>
@@ -118,7 +118,7 @@
 <p><b>Formula</b>: (1.5 * x ^ 1.5), where X is artifacts you own.</p>
 <br/>
 <p><b>C3100</b>: For All Factions - Spell Name: Engineering</p>
-<p><b>Requirement</b>: 15 days Mercenary time spent (Total).</p>
+<p><b>Requirement</b>: 15 days Mercenary time spent (All time).</p>
 <p><b>Cost</b>: 329.6 NoQig (3.296e182)</p>
 <p><b>Effect</b>: Increase maximum mana based on the total amount of clicks made (This R).</p>
 <p><b>Formula</b>: (65 * x ^ 0.35), where x is clicks made.</p>
@@ -130,7 +130,7 @@
 <p><b>Formula</b>: 0.1 * x ^ 0.7</p>
 <br/>
 <p><b>D3350</b>: For All Factions - Spell Name: Vampirism</p>
-<p><b>Requirement</b>: 1QA% (1e15) offline bonus, (Intervention D2775) and (Upheaval W3150).</p>
+<p><b>Requirement</b>: 1e26 (100 Sp)% offline bonus, (Intervention D2775) and (Upheaval W3150).</p>
 <p><b>Cost</b>: 492.5 QaSxg (4.925e197)</p>
 <p><b>Effect</b>: Increase assistants additively and multiplicatively based on your Offline Bonus.</p>
 <p><b>Formula</b>
@@ -138,13 +138,13 @@
 <p><b>Multiplicative</b>: (0.05 * log10(1 + x) ^ 2), where x is your Offline Bonus.
 <hr>
 <p><b>E3250</b> - <b>For</b> All - <b>Research Name</b>: Hirelings</p>
-<p><b>Requirement</b>: (Intimidation E1325) and (Scholarship S2875).</p>
+<p><b>Requirement</b>: Intimidation (E1325) and Scholarship (S2875).</p>
 <p><b>Cost</b>: 661.6 USxg (6.616e188)</p>
 <p><b>Effect</b>: Gives assistants based on the amount of coins you own.</p>
 <p><b>Formula</b>: (1.5 * ln(1 + x) ^ 1.5), where x is amount of coins you own.</p>
 <br/>
 <p><b>E3300</b> - <b>For</b> Mercenary - <b>Research Name</b>: Estates</p>
-<p><b>Requirement</b>: 45000 Unique buildings, (Hoarding E1225) and (Combination A2950).</p>
+<p><b>Requirement</b>: 45000 Unique buildings, Hoarding (E1225) and Combination (A2950).</p>
 <p><b>Cost</b>: 629.2 DSxg (6.292e191)</p>
 <p><b>Effect</b>: Gives you all the Unique Buildings of your alignment.</p>
 <hr>
@@ -159,7 +159,7 @@
 <p><b>Cost</b>: 533.7 QiSxg (5.337e200)</p>
 <p><b>Effect</b>: Increases Faction Coin find chance based on the total amount of Lineage levels you have.</p>
 <p><b>Effect</b>: Also increases Faction Coin find chance by a multiplicative 350%.</p>
-<p><b>Formula</b>: 3 * x ^ 2.25, where x is total Lineage levels.</p>
+<p><b>Formula</b>: (3 * x ^ 2.25), where x is total Lineage levels.</p>
 <hr>
 <p><b>W3050</b>: For Mercenary - Spell Name: Flanking</p>
 <p><b>Requirement</b>: 50000 Base Assistants, (Authority W1275) and (Intimidation E1325).</p>
@@ -171,5 +171,5 @@
 <p><b>Requirement</b>: 60000 Enchanted Fields, Inns and Dwarven Forges.</p>
 <p><b>Cost</b>: 357.2 Sxg (3.572e185)</p>
 <p><b>Effect</b>: Increases the production of all buildings based on their tier, giving the highest bonus to the lowest.</p>
-<p><b>Formula</b>: 50*(12-T)^2.15, where T is building tier.</p>
+<p><b>Formula</b>: (50 * (12 - T) ^ 2.15), where T is building tier.</p>
 <?php include "../scripts/footer.html"; ?>
