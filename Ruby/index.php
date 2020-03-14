936fc7432a8e68f6e512dafe7166125ff612d19e
Necrodoom <doomed.war@gmail.com>
2020-03-14 19:30:40 +0200

    Remove obsolete trophy page listings

--- Ruby/index.php
+++
@@ -1,21 +0,0 @@
-<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN"
-"http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
-<html xmlns="http://www.w3.org/1999/xhtml" lang="en" xml:lang="en">
-<head>
-<?php include "../scripts/header.html"; ?>
-<h6><img src="http://musicfamily.org/realm/Factions/picks/TrophiesTopPage.png"></h6>
-<p><b>Ruby Trophies</b>
-<p><b>Note</b>: All Ruby Trophies are (All time)</p>
-<p><b><img src="http://musicfamily.org/realm/Factions/picks/1Ruby.png" alt="1 Ruby" align="middle"> 1 Ruby</b></p>
-<p><b>Requirement</b>: Gain 1 Ruby</p>
-<br/>
-<p><b><img src="http://musicfamily.org/realm/Factions/picks/10Ruby.png" alt="10 Ruby" align="middle"> 10 Rubies</b></p>
-<p><b>Requirement</b>: Gain 10 Rubies</p>
-<br/>
-<p><b><img src="http://musicfamily.org/realm/Factions/picks/25Ruby.png" alt="25 Ruby" align="middle"> 25 Rubies</b></p>
-<p><b>Requirement</b>: Gain 25 Rubies</p>
-<br/>
-<p><b><img src="http://musicfamily.org/realm/Factions/picks/50Ruby.png" alt="50 Ruby" align="middle"> 50 Rubies</b></p>
-<p><b>Requirement</b>: Gain 50 Rubies</p>
-<br/>
-<?php include "../scripts/footer.html"; ?>
