3539d13e2d06ce2e8e55efea194a5feb0d5a03b0
Necrodoom <doomed.war@gmail.com>
2020-03-20 22:14:07 +0200

    Fix various builds

--- R160Plus/index.php
+++ R160Plus/index.php
@@ -35,7 +35,7 @@
             <p onclick="shohid($(this));"><b> <a href="#" onclick="return false;">R171-R175 Production Rush</a></b></a><b><font color="darkgoldenrod"> Neutral</font></b>/<b><font color="MediumTurquoise">Order</font></b></p>
             <div class="autohide">
                 <p><b>Author</b>: dzed</p>
-                <p><b>Range</b>: 0 - 1e70 (10 Dvg) Gems</p>
+                <p><b>Range</b>: 0 - 1e60 (1 Nod) Gems</p>
                 <p><b>Requirement</b>: Mercenary Duel</p>
                 <p><b>Alignment</b>: Neutral/Order Mercenary</p>
                 <p><b>Bloodline</b>: Makers</p>
@@ -83,7 +83,7 @@
             <p onclick="shohid($(this));"><b> <a href="#" onclick="return false;">R181+ Production Rush</a></b></a><b><font color="darkgoldenrod"> Neutral</font></b>/<b><font color="MediumTurquoise">Order</font></b></p>
             <div class="autohide">
                 <p><b>Author</b>: rellikrellik</p>
-                <p><b>Range</b>: 0 - 1e95 (100 Tg) Gems(?)</p>
+                <p><b>Range</b>: 0 - 1e85 (10 Spvg) Gems</p>
                 <p><b>Alignment</b>: Neutral/Order Mercenary</p>
                 <p><b>Bloodline</b>: Dwarf</p>
                 <p><b>A2950</b>: Dragon</p>
@@ -111,10 +111,11 @@
             </div>
         </div>
 		<div class="shelementwhole">
-            <p onclick="shohid($(this));"><b> <a href="#" onclick="return false;">R181+ Production/Excavation Buff</a></b></a><b><font color="darkred"> Evil</font></b>/<b><font color="MediumPurple">Chaos</font></b></p>
+            <p onclick="shohid($(this));"><b> <a href="#" onclick="return false;">R182+ Production/Excavation Buff</a></b></a><b><font color="darkred"> Evil</font></b>/<b><font color="MediumPurple">Chaos</font></b></p>
             <div class="autohide">
                 <p><b>Author</b>: ensteffahn</p>
-                <p><b>Range</b>: 1e95 (100 Tg) Gems(?) - ?</p>
+                <p><b>Range</b>: 1e80 (100 Qivg) Gems(?) - ?</p>
+				<p><b>Requirement</b>: F6000 buff before each run</p>
                 <p><b>Alignment</b>: Evil/Chaos Mercenary</p>
                 <p><b>Bloodline</b>: Fairy, Demon (2d16-8d16h)</p>
                 <p><b>A2950</b>: Dragon, Faceless (8d16h+)</p>
@@ -122,38 +123,38 @@
                 <p><b>Artifact Set</b>: Faceless</p>
                 <p>
 					<button onclick="myFunction($(this))">Copy Build</button>
-                    <input type="text" value="EL3,EL11,AN5,AN8,AN12,GB4,GB7,UD10,DM9,TT3,DN2,DW12,DG4,DJ3,DJ5,DJ7,SP:Grand Balance,SP:Infinite Spiral,UB:Flesh Workshop,UNN:DN,S200,S400,S5875,C175,C340,C400,S30,S105,S135,S150,S180,S215,S270,S330,C5125,C25,C80,C105,C120,C135,C150,C250,C305,C590,D5875,D290,D330,D200,D25,D55,D135,D150,D245,D275,D320,E5125,E135,E230,E290,E320,E25,E30,E80,E145,E150,E200,E275,E350,E480,A270,A305,A2950,A25,A30,A55,A105,A120,A135,A150,A175,A200,A250,A300,A330,A375,A480,A495,A545,A590,W5125,W1275,W205,W275,W400,W25,W525,F5500">
+                    <input type="text" value="EL3,EL11,AN5,AN8,AN12,GB4,GB7,UD10,DM9,TT3,DD6,DN2,DW12,DJ3,DJ5,DJ7,SP:Grand Balance,SP:Infinite Spiral,UB:Flesh Workshop,UNN:DN,D5875,S200,S400,S5875,S30,S105,S135,S150,S180,S215,S270,S330,C5125,C175,C340,C400,C25,C80,C105,C120,C135,C150,C250,C305,C590,D290,D330,D200,D25,D55,D135,D150,D245,D275,D320,E5125,E135,E230,E290,E320,E25,E30,E80,E145,E150,E200,E275,E350,E480,A270,A305,A2950,A25,A30,A55,A105,A120,A135,A150,A175,A200,A250,A300,A330,A375,A480,W5125,W1275,W275,W400,W25,W525,W205,F5500">
 				</p>
 				<p>
 					<button onclick="myFunction($(this))">Copy Build</button>
-                    <input type="text" value="EL3,EL11,AN5,AN8,GB4,GB7,UD10,DM9,TT3,DN2,DW12,DG4,AR12,DJ3,DJ5,DJ7,SP:Grand Balance,SP:Infinite Spiral,UB:Flesh Workshop,UNN:DN,S200,S400,S5875,C175,C340,C400,S30,S105,S135,S150,S180,S215,S270,S330,C5125,C25,C80,C105,C120,C135,C150,C250,C305,C590,D5875,D290,D330,D200,D25,D55,D135,D150,D245,D275,D320,E5125,E135,E230,E290,E320,E25,E30,E80,E145,E150,E200,E275,E350,E480,A270,A305,A2950,A25,A30,A55,A105,A120,A135,A150,A175,A200,A250,A300,A330,A375,A480,A495,A545,A590,W5125,W1275,W205,W275,W400,W25,W525,F5500">
+                    <input type="text" value="EL3,EL11,AN5,AN8,GB4,GB7,UD10,DM9,TT3,DD6,DN2,DW12,AR12,DJ3,DJ5,DJ7,SP:Grand Balance,SP:Infinite Spiral,UB:Flesh Workshop,UNN:DN,D5875,S200,S400,S5875,S30,S105,S135,S150,S180,S215,S270,S330,C5125,C175,C340,C400,C25,C80,C105,C120,C135,C150,C250,C305,C590,D290,D330,D200,D25,D55,D135,D150,D245,D275,D320,E5125,E135,E230,E290,E320,E25,E30,E80,E145,E150,E200,E275,E350,E480,A270,A305,A2950,A25,A30,A55,A105,A120,A135,A150,A175,A200,A250,A300,A330,A375,A480,W5125,W1275,W275,W400,W25,W525,W205,F5500">
 					<b>8d16h+ time this R</b>
 				</p>
 				<p>
 					<button onclick="myFunction($(this))">Copy Build</button>
-                    <input type="text" value="EL3,EL11,AN5,AN8,AN12,GB4,GB7,UD10,DM9,TT3,DN2,DW12,DG4,DJ3,DJ5,DJ7,SP:Grand Balance,SP:Infinite Spiral,UB:Flesh Workshop,UNN:DN,S200,S400,S5875,C175,C340,C400,S30,S105,S135,S150,S180,S215,S270,S330,C5125,C25,C80,C105,C120,C135,C150,C250,C305,C590,D5875,D290,D330,D200,D25,D55,D135,D150,D245,D275,D320,E5125,E135,E230,E290,E320,E25,E30,E80,E145,E150,E200,E275,E350,E480,A30,A270,A2950,A5125,W5125,W1275,W205,W275,W400,W25,W525,F5500">
+                    <input type="text" value="EL3,EL11,AN5,AN8,AN12,GB4,GB7,UD10,DM9,TT3,DD6,DN2,DW12,DJ3,DJ5,DJ7,SP:Grand Balance,SP:Infinite Spiral,UB:Flesh Workshop,UNN:DN,D5875,S200,S400,S5875,C175,C340,C400,S30,S105,S135,S150,S180,S215,S270,S330,C5125,C175,C340,C400,C25,C80,C105,C120,C135,C150,C250,C305,C590,D290,D330,D200,D25,D55,D135,D150,D245,D275,D320,E5125,E135,E230,E290,E320,E25,E30,E80,E145,E150,E200,E275,E350,E480,A30,A270,A2950,A5125,W5125,W1275,W275,W400,W25,W525,W205,F5500">
 					<b>1d10h+ time this game</b>
 				</p>
 				<p>
 					<button onclick="myFunction($(this))">Copy Build</button>
-                    <input type="text" value="EL3,EL11,AN5,AN8,GB4,GB7,UD10,DM9,TT3,DN2,DW12,DG4,AR12,DJ3,DJ5,DJ7,SP:Grand Balance,SP:Infinite Spiral,UB:Flesh Workshop,UNN:DN,S200,S400,S5875,C175,C340,C400,S30,S105,S135,S150,S180,S215,S270,S330,C5125,C25,C80,C105,C120,C135,C150,C250,C305,C590,D5875,D290,D330,D200,D25,D55,D135,D150,D245,D275,D320,E5125,E135,E230,E290,E320,E25,E30,E80,E145,E150,E200,E275,E350,E480,A30,A270,A2950,A5125,W5125,W1275,W205,W275,W400,W25,W525,F5500">
+                    <input type="text" value="EL3,EL11,AN5,AN8,GB4,GB7,UD10,DM9,TT3,DD6,DN2,DW12,AR12,DJ3,DJ5,DJ7,SP:Grand Balance,SP:Infinite Spiral,UB:Flesh Workshop,UNN:DN,D5875,S200,S400,S5875,S30,S105,S135,S150,S180,S215,S270,S330,C5125,C175,C340,C400,C25,C80,C105,C120,C135,C150,C250,C305,C590,D290,D330,D200,D25,D55,D135,D150,D245,D275,D320,E5125,E135,E230,E290,E320,E25,E30,E80,E145,E150,E200,E275,E350,E480,A30,A270,A2950,A5125,W5125,W1275,W275,W400,W25,W525,W205,F5500">
 					<b>1d10h+ time this game, 8d16h+ time this R</b>
 				</p>
-                <p>EL3,EL11,AN5,AN8,AN12,GB4,GB7,UD10,DM9,TT3,DN2,DW12,DG4,DJ3,DJ5,DJ7,</p>
+                <p>EL3,EL11,AN5,AN8,AN12,GB4,GB7,UD10,DM9,TT3,DD6,DN2,DW12,DJ3,DJ5,DJ7,</p>
 				<p>SP:Grand Balance,SP:Infinite Spiral,UB:Flesh Workshop,UNN:DN,</p>
                 <p>S200,S400,S5875,S30,S105,S135,S150,S180,S215,S270,S330,</p>
                 <p>C5125,C175,C340,C400,C25,C80,C105,C120,C135,C150,C250,C305,C590,</p>
                 <p>D5875,D290,D330,D200,D25,D55,D135,D150,D245,D275,D320,</p>
                 <p>E5125,E135,E230,E290,E320,E25,E30,E80,E145,E150,E200,E275,E350,E480,</p>
-                <p>A270,A305,A2950,A25,A30,A55,A105,A120,A135,A150,A175,A200,A250,A300,A330,A375,A480,A495,A545,A590,</p>
-                <p>W5125,W1275,W205,W275,W400,W25,W525,</p>
+                <p>A270,A305,A2950,A25,A30,A55,A105,A120,A135,A150,A175,A200,A250,A300,A330,A375,A480,</p>
+                <p>W5125,W1275,W275,W400,W25,W525,W205,</p>
                 <p>F5500</p>
-				<p><b>Note</b>: Buffing W275, S400, F6000 is very important.</p>
+				<p><b>Note</b>: Buffing W275, S400 is very important.</p>
 				<p><b>Note</b>: Maelstrom should buff HoL with assistants.</p>
                 <p><b>Note</b>: 2d16h-8d16h time this R: use Demonline + Dragon A2950.</p>
                 <p><b>Note</b>: 8d16h+ time this R: use Fairyline + Faceless A2950, swap AN12 -> AR12.</p>
-                <p><b>Note</b>: If buffing excavations is the primary goal, always use Fairyline + Dragon A2950.</p>
                 <p><b>Note</b>: 1d10h+ time this game: swap all A researches except A30, A270, A2950 for A5125.</p>
+				<p><b>Note</b>: If buffing excavations is the primary goal, always use Fairyline + Dragon A2950.</p>
             </div>
         </div>
     </div>
@@ -232,7 +233,67 @@
     <p><b>Buff Builds</b></p>
     <div class="category">
         <div class="shelementwhole">
-            <p onclick="shohid($(this));"><b> <a href="#" onclick="return false;">F6000</a></b><b><font color="darkgoldenrod"> Neutral</font></b>/<b><font color="MediumTurquoise">Order</font></b></a></p>
+            <p onclick="shohid($(this));"><b> <a href="#" onclick="return false;">R182+ Spell casts and Mana Produced</b></a><b><font color="darkred"> Evil</font></b>/<b><font color="MediumTurquoise">Order</font></b></p>
+            <div class="autohide">
+                <p><b>Author</b>: ensteffahn</p>
+                <p><b>Range</b>: 1e60 (1 Nod) Gems+</p>
+                <p><b>Alignment</b>: Evil/Order Mercenary</p>
+                <p><b>Bloodline</b>: Dwarf</p>
+                <p><b>A2950</b>: Dragon</p>
+                <p><b>D5875</b>: Archon</p>
+                <p><b>Artifact Set</b>: Mercenary</p>
+                <p>
+					<button onclick="myFunction($(this))">Copy Build</button>
+                    <input type="text" value="EL3,EL7,AN5,AN8,AN12,GB4,GB7,TT3,DD11,DN2,DW6,DW12,AR2,DJ5,DJ7,MK10,SP:Fairy Chanting,SP:Precognition,UB:Burning Abyss,UNN:AR,S5875,S200,S400,S500,C5625,C175,C340,C500,C250,C400,C590,D5875,D200,D275,D290,D330,E135,E230,E290,E320,E400,E495,E1325,E3300,A30,A120,A250,A270,A305,A545,A1500,A2950,W205,W275,W400,W5625,W1275,F5250">
+				</p>
+				<p>
+					<button onclick="myFunction($(this))">Copy Build</button>
+                    <input type="text" value="EL3,EL7,AN5,AN8,AN12,GB4,GB7,TT3,DD11,DN2,DN12,DW6,AR2,DJ5,DJ7,MK10,SP:Fairy Chanting,SP:Precognition,UB:Burning Abyss,UNN:AR,S5875,S200,S400,S500,C5625,C175,C340,C500,C250,C400,C590,D5875,D200,D275,D290,D330,E135,E230,E290,E320,E400,E495,E1325,E3300,A30,A120,A250,A270,A305,A545,A1500,A2950,W205,W275,W400,W5625,W1275,F5250">
+					<b>DN12 is 100%+</b>
+				</p>
+				<p>EL3,EL7,AN5,AN8,AN12,GB4,GB7,TT3,DD11,DN2,DW6,DW12,AR2,DJ5,DJ7,MK10,</p>
+				<p>SP:Brainwave,SP:Precognition,UB:Burning Abyss,UNN:AR,</p>
+				<p>S5875,S200,S400,S500,</p>
+				<p>C5625,C175,C340,C500,C250,C400,C590,</p>
+				<p>D5875,D200,D275,D290,D330,</p>
+				<p>E135,E230,E290,E320,E400,E495,E1325,E3300,</p>
+				<p>A30,A120,A250,A270,A305,A545,A1500,A2950,</p>
+				<p>W205,W275,W400,W5625,W1275,</p>
+				<p>F5250</p>
+				<p><b>Notes</b>: Use excavation resets to buff C5625.</p>
+				<p><b>Notes</b>: Buffing F6000 helps.</p>
+				<p><b>Notes</b>: Swap DW12 -> DN12, if DN12 is 100%+.</p>
+				<p><b>Notes</b>: Use remaining points to buff production.</p>
+            </div>
+        </div>
+        <div class="shelementwhole">
+            <p onclick="shohid($(this));"><b> <a href="#" onclick="return false;">R182+ Max Assistants</b></a><b><font color="darkred"> Evil</font></b>/<b><font color="MediumPurple">Chaos</font></b></p>
+            <div class="autohide">
+                <p><b>Author</b>: ensteffahn</p>
+                <p><b>Range</b>: 1e66 (1 Uvg) Gems+</p>
+                <p><b>Alignment</b>: Evil/Chaos Mercenary</p>
+                <p><b>Bloodline</b>: Dwarf</p>
+                <p><b>A2950</b>: Dragon</p>
+                <p><b>D5875</b>: Archon</p>
+                <p><b>Artifact Set</b>: Mercenary</p>
+                <p>
+					<button onclick="myFunction($(this))">Copy Build</button>
+                    <input type="text" value="EL3,EL4,EL7,AN5,AN8,GB4,GB7,UD10,TT3,DD11,DN2,DW12,AR2,DJ5,DJ7,MK10,SP:Fairy Chanting,SP:Precognition,UB:Flesh Workshop,UNN:DN,S5875mS200,S400,S500,S330,S215,C5625,C175,C250,C340,C400,C520,C590,D5875,D200,D290,D330,D275,E135,E230,E290,E320,E460,E495,E3300,E1325,E400,A30,A120,A270,A305,A545,A1500,A2950,W5625,W205,W275,W400,W1275,W180,F5500">
+				</p>
+                <p>EL3,EL4,EL7,AN5,AN8,GB4,GB7,UD10,TT3,DD11,DN2,DW12,AR2,DJ5,DJ7,MK10,</p>
+                <p>SP:Fairy Chanting,SP:Precognition,UB:Flesh Workshop,UNN:DN,</p>
+                <p>S5875,S200,S400,S500,S330,S215,</p>
+                <p>C5625,C175,C250,C340,C400,C520,C590,</p>
+                <p>D5875,D200,D290,D330,D275,</p>
+                <p>E135,E230,E290,E320,E460,E495,E3300,E1325,E400,</p>
+                <p>A30,A120,A270,A305,A545,A1500,A2950,</p>
+                <p>W5625,W205,W275,W400,W1275,W180,</p>
+                <p>F5500</p>
+                <p><b>Notes</b>: Buff Excavation resets, do F6000, build scales on time this R.</p>
+            </div>
+        </div>
+		<div class="shelementwhole">
+            <p onclick="shohid($(this));"><b> <a href="#" onclick="return false;">R182+ F6000</a></b><b><font color="darkgoldenrod"> Neutral</font></b>/<b><font color="MediumTurquoise">Order</font></b></a></p>
             <div class="autohide">
                 <p><b>Author</b>: kiluh</p>
                 <p><b>Range</b>: ?</p>
@@ -254,67 +315,6 @@
                 <p>A120,A270,A2950,A5375,A1500,A305,</p>
                 <p>W275,W400,W1275,W1375,W5125,W120,W150,W180,W1400,</p>
                 <p>F6000</p>
-                <p><b>Note</b>: Getting 11h+ on F6000 with 4e16 (40 Qa)% W275, 4e6 (4 M) S400, Faceless Lineage L75, 1e70 (10 Dvg) gems.</p>
-            </div>
-        </div>
-        <div class="shelementwhole">
-            <p onclick="shohid($(this));"><b> <a href="#" onclick="return false;">R182+ S400/Mana Produced</b></a><b><font color="darkred"> Evil</font></b>/<b><font color="MediumTurquoise">Order</font></b></p>
-            <div class="autohide">
-                <p><b>Author</b>: ensteffahn</p>
-                <p><b>Range</b>: 1e60 (1 Nod) Gems+</p>
-                <p><b>Alignment</b>: Evil/Order Mercenary</p>
-                <p><b>Bloodline</b>: Dwarf</p>
-                <p><b>A2950</b>: Dragon</p>
-                <p><b>D5875</b>: Archon</p>
-                <p><b>Artifact Set</b>: Mercenary</p>
-                <p>
-					<button onclick="myFunction($(this))">Copy Build</button>
-                    <input type="text" value="EL3,EL7,AN5,AN8,AN12,GB4,GB7,TT3,DD11,DN2,DW6,DW12,AR2,DJ5,DJ7,MK10,SP:Fairy Chanting,SP:Precognition,UB:Burning Abyss,UNN:AR,S200,S400,S500,S5875,C175,C340,C500,C5625,C250,C400,C590,D5875,D200,D275,D290,D330,E135,E230,E290,E320,E400,E495,E1325,E3300,A30,A120,A250,A270,A305,A545,A1500,A2950,W205,W275,W400,W5625,W1275,F5250">
-				</p>
-				<p>
-					<button onclick="myFunction($(this))">Copy Build</button>
-                    <input type="text" value="EL3,EL7,AN5,AN8,AN12,GB4,GB7,TT3,DD11,DN2,DN12,DW6,AR2,DJ5,DJ7,MK10,SP:Fairy Chanting,SP:Precognition,UB:Burning Abyss,UNN:AR,S200,S400,S500,S5875,C175,C340,C500,C5625,C250,C400,C590,D5875,D200,D275,D290,D330,E135,E230,E290,E320,E400,E495,E1325,E3300,A30,A120,A250,A270,A305,A545,A1500,A2950,W205,W275,W400,W5625,W1275,F5250">
-					<b>DN12 is 100%+</b>
-				</p>
-				<p>EL3,EL7,AN5,AN8,AN12,GB4,GB7,TT3,DD11,DN2,DW6,DW12,AR2,DJ5,DJ7,MK10,</p>
-				<p>SP:Fairy Chanting,SP:Precognition,UB:Burning Abyss,UNN:AR,</p>
-				<p>S200,S400,S500,S5875,</p>
-				<p>C175,C340,C500,C5625,C250,C400,C590,</p>
-				<p>D5875,D200,D275,D290,D330,</p>
-				<p>E135,E230,E290,E320,E400,E495,E1325,E3300,</p>
-				<p>A30,A120,A250,A270,A305,A545,A1500,A2950,</p>
-				<p>W205,W275,W400,W5625,W1275,</p>
-				<p>F5250</p>
-				<p><b>Notes</b>: Use excavation resets to buff C5625.</p>
-				<p><b>Notes</b>: Buffing F6000 helps.</p>
-				<p><b>Notes</b>: Swap DW12 -> DN12, if DN12 is 100%+.</p>
-				<p><b>Notes</b>: Use remaining points to buff production.</p>
-            </div>
-        </div>
-        <div class="shelementwhole">
-            <p onclick="shohid($(this));"><b> <a href="#" onclick="return false;">R182+ W275/Max Assistants</b></a><b><font color="darkred"> Evil</font></b>/<b><font color="MediumPurple">Chaos</font></b></p>
-            <div class="autohide">
-                <p><b>Author</b>: ensteffahn</p>
-                <p><b>Range</b>: 1e66 (1 Uvg) Gems+</p>
-                <p><b>Alignment</b>: Evil/Chaos Mercenary</p>
-                <p><b>Bloodline</b>: Dwarf</p>
-                <p><b>A2950</b>: Dragon</p>
-                <p><b>D5875</b>: Archon</p>
-                <p><b>Artifact Set</b>: Mercenary</p>
-                <p>
-					<button onclick="myFunction($(this))">Copy Build</button>
-                    <input type="text" value="EL3,EL4,EL7,AN5,AN8,GB4,GB7,UD10,TT3,DD11,DN2,DW12,AR2,DJ5,DJ7,MK10,SP:Fairy Chanting,SP:Precognition,UB:Flesh Workshop,UNN:DN,S200,S5875,S400,S500,S330,S215,C5625,C175,C250,C340,C400,C520,C590,D5875,D200,D290,D330,D275,E135,E230,E290,E320,E460,E495,E3300,E1325,E400,A30,A120,A270,A305,A545,A1500,A2950,W5625,W205,W275,W400,W1275,W180,F5500">
-				</p>
-                <p>EL3,EL4,EL7,AN5,AN8,GB4,GB7,UD10,TT3,DD11,DN2,DW12,AR2,DJ5,DJ7,MK10,</p>
-                <p>SP:Fairy Chanting,SP:Precognition,UB:Flesh Workshop,UNN:DN,</p>
-                <p>S200,S5875,S400,S500,S330,S215,</p>
-                <p>C5625,C175,C250,C340,C400,C520,C590,</p>
-                <p>D5875,D200,D290,D330,D275,</p>
-                <p>E135,E230,E290,E320,E460,E495,E3300,E1325,E400,</p>
-                <p>A30,A120,A270,A305,A545,A1500,A2950,</p>
-                <p>W5625,W205,W275,W400,W1275,W180,</p>
-                <p>F5500</p>
-                <p><b>Notes</b>: Buff Excavation resets, do F6000, build scales on time this R.</p>
             </div>
         </div>
     </div>
