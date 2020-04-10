444f61b4743181176c6dfe6e4d138261e05c3405
G00FY <realm@musicfamily.org>
2020-04-10 08:35:56 -0400

    fix build

--- R125Plus/index.php
+++ R125Plus/index.php
@@ -488,7 +488,7 @@
                 <p><b>Artifact Set</b>: Faceless</p>
                 <p>
 					<button onclick="myFunction($(this))">Copy Build</button>
-                    <input type="text" value="S300,S1450,S3200,C251,C375,C400,C1300,C1500,D290,D350,D1125,E275,E320,E1225,A400,A1200,W260,W1375,W1400">
+                    <input type="text" value="S300,S1450,S3200,C251,C375,C400,C1300,C1500,D290,D350,D1125,E275,E320,E1225,A400,A1200,W260,W1375,W1400,S400,C1325,D150,E1325,A305,W400">
 				</p>
                 <p>S300,S1450,S3200,</p>
                 <p>C251,C375,C400,C1300,C1500,</p>
