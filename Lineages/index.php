82ac9d92b83f672adaa901d56ac068ae605f9a63
Necrodoom <doomed.war@gmail.com>
2020-04-29 19:04:34 +0300

    fix error

--- Lineages/index.php
+++ Lineages/index.php
@@ -162,7 +162,7 @@
     <p><img src="http://musicfamily.org/realm/Factions/picks/FairyPerk4.png" align="middle"><b> Fairy Perk 4</b></p>
     <p><b>Requirement</b>: A2+, Reach Lineage Level 30.</p>
     <p><b>Effect</b>: While Fairy Chanting is active, further increase your temporary assistants based on its duration.</p>
-    <p><b>Formula</b>: (2.5 * (x * 30) ^ 0.5), where x is Fairy Chanting duration in seconds.</p>
+    <p><b>Formula</b>: (2.5 * x ^ 0.5), where x is Fairy Chanting duration in seconds.</p>
     <br/>
     <p><b>Level 45</b></p>
     <p><b><img src="http://musicfamily.org/realm/Factions/picks/FairyPerk5.png" align="middle"> Fairy Perk 5</b></p>
