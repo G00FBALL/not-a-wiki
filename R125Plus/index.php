8a8f4a62b9d813646ae2d5a932bec28312cab75d
Necrodoom <doomed.war@gmail.com>
2020-05-07 06:36:38 +0300

    also useless

--- R125Plus/index.php
+++ R125Plus/index.php
@@ -475,7 +475,6 @@
                 <p>E400,E410,E135,</p>
                 <p>A50,A400,A495,A120,</p>
                 <p>W225,W560,W1375,W400</p>
-				<p><b>Notes</b>: Estimated Time to Primal Balance +10 is ?</p>
             </div>
         </div>
 		<div class="shelementwhole">
