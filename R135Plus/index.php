02049f4aa925c303d9ff9039594716efae82f239
Necrodoom <doomed.war@gmail.com>
2020-10-12 19:04:56 +0300

    Fix sets

--- R135Plus/index.php
+++ R135Plus/index.php
@@ -21,6 +21,7 @@
 				<p><b>Faction</b>: Elf</p>
 				<p><b>Bloodline</b>: Druid</p>
 				<p><b>Artifact Set</b>: Elf</p>
+				<p><b>Stoneheart Set (R148+, MKC4)</b>: Druid</p>
 				<p>
 					<button onclick="myFunction($(this))">Copy Build</button>
 					<input type="text" value="S175,S3200,C1,C225,C340,C405,C520,C590,D50,D435,D1375,E25,E410,E3250,A495,A3400,W10,W560,W1375">
@@ -164,7 +165,8 @@
 				<p><b>Range</b>: 0 - 1e58 (10 Ocd) Gems</p>
 				<p><b>Faction</b>: Elf</p>
 				<p><b>Bloodline</b>: Fairy</p>
-				<p><b>Artifact Set</b>: Elf</p>
+				<p><b>Artifact Set</b>: Fairy</p>
+				<p><b>Stoneheart Set (R148+, MKC4)</b>: Druid</p>
 				<p>
 					<button onclick="myFunction($(this))">Copy Build</button>
 					<input type="text" value="S175,S3200,C1,C225,C340,C405,C520,C590,D50,D435,D1375,E25,E410,E3250,A495,A3400,W10,W560,W1375">
