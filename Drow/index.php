5e9db7c0d7f945ccb8d59258bdc2f7e13a53165b
xpt14 <43166103+xpt14@users.noreply.github.com>
2020-12-17 00:05:45 +0700

    v4.0 update

--- Drow/index.php
+++ Drow/index.php
@@ -10,7 +10,8 @@
 <p>"The mysterious drow wish for you to prove your murderous honor to them."</p>
 <p><b>Requirements</b>: Demon as Base Faction, Drow as Prestige Faction, Reincarnation 6+, Perfect Combo upgrade purchased, Goblin, Demon and Undead challenge 1 completed.</p>
 <p><b>Challenge</b>: Have Honor Among Killers with at least 500 trophies and 1000 Royal Exchanges bought.</p>
-<p><b>Effect</b>: Increase Royal Exchange bonus by 0.025% per trophy you own.</p>
+<p><b>Effect</b>: Increase Royal Exchange bonus based on the amount of unlocked trophies.</p>
+<p><b>Formula</b>: +(0.2 * x ^ 0.8)%, where x is the amount of unlocked trophies.</p>
 <p><b>Upgrade</b>: Works with Demon+Drow</p>
 <p><b>Tip</b>: Use Elven Bloodline.</p>
 <hr>
@@ -18,8 +19,8 @@
 <p><img src="http://musicfamily.org/realm/Factions/picks/DarkEleganceChallenges.png" alt="Dark Elegance" align="middle"> <b>Dark Elegance</b></p>
 <p>"The Spider Queen is not pleased with some of her worshippers. You must weed out the weak and sacrifice them at the temple altar."</p>
 <p><b>Requirements</b>: Goblin as Base Faction, Drow as Prestige Faction, Reincarnation 11+, Drow Bloodline, Goblin, Demon and Undead challenge 2 and Drow challenge 1 completed.</p>
-<p><b>Challenge</b>: Build 2000 Spider Sanctuaries (Dark Temples).</p>
-<p><b>Effect</b>: Increase the production of all buildings based on faction coins earned this game.</p>
+<p><b>Challenge</b>: Build 1750 Spider Sanctuaries (Dark Temples).</p>
+<p><b>Effect</b>: Increase the production of all buildings based on Faction Coins found this game. Does not suffer from Ascension penalties.</p>
 <p><b>Formula</b>: log^2(1 + x), where x is your Faction Coins Gained (This Game) stat.</p>
 <p><b>Upgrade</b>: Works with Goblin+Drow</p>
 <hr>
@@ -28,7 +29,8 @@
 <p>"A legion of interlopers have invaded our sanctuaries. Work yourselves into a rage and tear them apart."</p>
 <p><b>Requirements</b>: Undead as Base Faction, Drow as Prestige Faction, Reincarnation 21+, 1750 excavations, Goblin, Demon and Undead challenge 3 and Drow challenge 2 completed.</p>
 <p><b>Challenge</b>: Have at least 2 hours of activity time for both Blood Frenzy and Combo Strike.(This Game)</p>
-<p><b>Effect</b>: Online Time (This game) is added to Offline Time (This game).</p>
+<p><b>Effect</b>: Increases Offline Production based on time spent in this game. Does not suffer from Ascension penalties.</p>
+<p><b>Formula</b>: (x ^ 0.7)%, where x is time spent this game.</p>
 <p><b>Upgrade</b>: Works with Undead+Drow</p>
 <p><b>Tip</b>: Run both spells at the same time, depending on mana regeneration it takes about 1 hour 15 minutes to complete the challenge.</p>
 <hr>
@@ -36,7 +38,7 @@
 <p><img src="http://musicfamily.org/realm/Factions/picks/TrainedAssasinsChallenges.png" alt="Trained Assassins" align="middle"> <b>Trained Assassins</b></p>
 <p>There is a civil war between 2 Spider Gods. Bolster our forces and wipe the weaklings off the face of the realm.</p>
 <p><b>Requirements</b>: Any Evil as Base Faction, Drow as Prestige Faction, Reincarnation 27+, 2000 excavations, Drow Challenge 3 completed.</p>
-<p><b>Challenge</b>: Cast Call to Arms with at least 35,000 buildings.</p>
+<p><b>Challenge</b>: Cast Call to Arms with at least 30,000 buildings.</p>
 <p><b>Effect</b>: Increase your Offline Time (This Game) based on spells cast in this R.</p>
 <p><b>Formula</b>: (8 * log10(x) ^ 4) where x is spells cast in this R.</p>
 <p><b>Upgrade</b>: Works with any Evil+Drow</p>
