cb6e92164aa8cb6e1171e35e6dedae50a4dbc672
Alright <shootingwithportals@gmail.com>
2020-12-18 13:44:37 -0500

    4.0 Faction pages update
    - Undead, Demon, Titan, Druid
    - Fixing icon url errors

--- TitanFaction/index.php
+++ TitanFaction/index.php
@@ -15,31 +15,31 @@
         <p><img src="http://musicfamily.org/realm/Factions/picks/AncientStoneSlab1FactionUpgrade.png" alt="Ancient Stone Slab 1" align="middle"><b> Ancient Stone Slab 1</b></p>
         <p><b>Description</b>: We discovered an ancient stoneslab written in old scriptures. It appears to say something about Halls of Legends.</p>
         <p><b>Type</b>: Quest Artifact</p>
-        <p><b>Requirement</b>: Have at least 1B (1E9) gems in the abdication treasury, 5 Excavations</p>
+        <p><b>Requirement</b>: Have at least 1 B (1e9) gems in the abdication treasury, 5 Excavations</p>
         <p><b>Description</b>: 1st clue required to unlock the Titan Alliance.</p>
         <br/>
         <p><img src="http://musicfamily.org/realm/Factions/picks/AncientStoneSlab2FactionUpgrade.png" alt="Ancient Stone Slab 2" align="middle"><b> Ancient Stone Slab 2</b></p>
         <p><b>Description</b>: We discovered an ancient stoneslab written in old scriptures. We can recognize the number 300.
         <p><b>Type</b>: Quest Artifact</p>
-        <p><b>Requirement</b>: Have at least 1B (1E9) gems in the abdication treasury, 20 Excavations</p>
+        <p><b>Requirement</b>: Have at least 1 B (1e9) gems in the abdication treasury, 20 Excavations</p>
         <p><b>Description</b>: 2nd clue required to unlock the Titan Alliance.</p>
         <br/>
         <p><img src="http://musicfamily.org/realm/Factions/picks/FragmentofaTitanStatue.png" alt="Fragment of a Titan Statue" align="middle"><b> Fragment of a Titan Statue</b></p>
         <p><b>Type</b>: Upgrade</p>
         <p><b>Requirement</b>: 2 Ancient Stone Slabs, 300 Halls of Legends</p>
-        <p><b>Cost</b>: 1 No (1E30), ,25,000 Angel Coins, 25,000 Goblin Coins</p>
+        <p><b>Cost</b>: 1 No (1E30), 25,000 Angel Coins, 25,000 Goblin Coins</p>
         <br/>
         <p><b>Faction Spell</b></p>
-        <p><img src="http://musicfamily.org/realm/Factions/picks/LightningStrike.png" alt="Smiley face" align="middle"> <b>Lightning Strike</b></p>
-        <p><b>Effect</b>: The production of a random building is multiplied by 300% for each Iron Stronghold.</p>
-        <p><b>Effect</b>: Also grants Faction Coins depending on your current amount of gems.</p>
-        <p><b>Formula</b>: round(1.5 * ln(1 + x) ^ 3), where x is your current gems.</p>
+        <p><img src="http://musicfamily.org/realm/Factions/picks/spellLightningStrike.png" alt="Smiley face" align="middle"> <b>Lightning Strike</b></p>
+        <p><b>Effect</b>: The production of a random building is multiplied by 500% for each Iron Stronghold for 10 seconds.</p>
+        <p><b>Effect</b>: Also multiplicatively increase Faction Coins find chance based on amount of Gems owned.</p>
+        <p><b>Formula</b>: (10 + log10(1 + x) ^ 2)%, where x is Gems owned.</p>
         <br/>
         <p><b>Spell Trophy & Upgrade</b></p>
         <p><img src="http://musicfamily.org/realm/Factions/picks/LightningStormFactionUpgrade.png" alt="Smiley face" align="middle"> <b>Lightning Storm</b></p>
         <p><b>Requirement</b>: Cast Lightning Strike 100 times in a single game.</p>
         <p><b>Cost</b>: 10 Dd (1e40), A1+ Free</p>
-        <p><b>Effect</b>: Increase Lightning Strike bonus to 400% for each Iron Strongholds.</p>
+        <p><b>Effect</b>: Increase Lightning Strike bonus to 750% per Iron Stronghold.</p>
         <hr>
         <p><img src="http://musicfamily.org/realm/Factions/picks/ProofofNeutralityFactionUpgrade.png" alt="Proof of Neutrality" align="middle"><b> Proof Of Neutrality</b></p>
         <p><b>Type</b>: Alignment Upgrade</p>
@@ -53,21 +53,22 @@
         <p><b>Tier 1 Upgrades</b></p>
         <p><b><img src="http://musicfamily.org/realm/Factions/picks/TitansTradeTreatyFactionUpgrade.png" alt="Titans Trade Treaty" align="middle"> Titans Trade Treaty</b></p>
         <p><b>Cost</b>: 250 Angel Coins + 250 Goblin Coins</p>
-        <p><b>Effect</b>: Unlocks Tier 1 Titan Upgrades.</p>
+        <p><b>Effect</b>: Unlocks Tier 1 Titan upgrades.</p>
         <br/>
         <p><b><img src="http://musicfamily.org/realm/Factions/picks/ColossalForgeFactionUpgrade.png" alt="Colossal Forge" align="middle"> Colossal Forge</b></p>
-        <p><b>Cost</b>: 500 Qi (5E20)</p>
-        <p><b>Effect</b>: Increase the production of all buildings by 1% per Blacksmith you own.</p>
+        <p><b>Cost</b>: 500 Qi (5e20)</p>
+        <p><b>Effect</b>: Increase the production of all buildings based on the amount of buildings you own.</p>
+        <p><b>Formula</b>: (6 * x ^ 0.6)%, where x is the amount of buildings built.</p>
         <br/>
         <p><b><img src="http://musicfamily.org/realm/Factions/picks/ChargedClicksFactionUpgrade.png" alt="Charged Clicks" align="middle"> Charged Clicks</b></p>
-        <p><b>Cost</b>: 5 Sx (5E21)</p>
-        <p><b>Effect</b>: Whenever you cast a spell, your click reward is increased by 250% for 20 seconds. If another spell is cast while this effect is active, it is restored to full duration.</p>
-        <p><b>Note</b>: Despite its description, Charged Clicks applies to all buildings, and is identical in effect to Charged Structures.</p>
+        <p><b>Cost</b>: 5 Sx (5e21)</p>
+        <p><b>Effect</b>: Multiplicatively increase Mana Regeneration based on Clicks made in this game.</p>
+        <p><b>Formula</b>: (25 + 2.5 * log10(1 + x) ^ 2.5 + x ^ 0.25)%, where x is Clicks made this game.</p>
         <br/>
-        <p><b><img src="http://musicfamily.org/realm/Factions/picks/OversizedLegendsFactionUpgrade.png" alt="Oversized Legends" align="middle"> Oversized Legends</b></p>
-        <p><b>Cost</b>: 50 Sx (5E22)</p>
-        <p><b>Effect</b>: Reduce Hall of Legends building cost multiplier and increase assistants production by 1% per Hall of Legends you own.</p>
-        <p><b>Effect</b>: Reduces cost multiplier by 0.02; with no other reductions applying, the multiplier will be 1.13 instead of 1.15.</p>
+        <p><b><img src="http://musicfamily.org/realm/Factions/picks/TitanObeliskFactionUpgrade.png" alt="Titan Obelisk" align="middle"> Titan Obelisk</b></p>
+        <p><b>Cost</b>: 50 Sx (5e22)</p>
+        <p><b>Effect</b>: Increase Faction Coin find chance additively based on time spent as Neutral in this Reincarnation.</p>
+        <p><b>Formula</b>: +(3 * x ^ 0.7)%, where x is time spent as Neutral this Reincarnation.</p>
         <hr>
         <p><b>Tier 2 Upgrades</b></p>
         <p><b><img src="http://musicfamily.org/realm/Factions/picks/TitansFriendshipPactFactionUpgrade.png" alt="Titan Friendship Pact" align="middle"> Titan Friendship Pact</b></p>
@@ -75,33 +76,37 @@
         <p><b>Effect</b>: Unlocks Tier 2 Titan Upgrades.</p>
         <br/>
         <p><b><img src="http://musicfamily.org/realm/Factions/picks/TitanDrillFactionUpgrade.png" alt="Titan Drill" align="middle"> Titan Drill</b></p>
-        <p><b>Cost</b>: 5 Sp (5E24)</p>
-        <p><b>Effect</b>: Increases the production of Deep Mines by 250,000%</p>
+        <p><b>Cost</b>: 5 Sp (5e24)</p>
+        <p><b>Effect</b>: Increase the base production of Warrior Barracks / Slave Pens / Deep Mines by +9635, and Cathedrals / Dark Temples / Monasteries by +98000 (based on your Alignment).</p>
         <br/>
         <p><b><img src="http://musicfamily.org/realm/Factions/picks/ChargedStructuresFactionUpgrade.png" alt="Charged Structures" align="middle"> Charged Structures</b></p>
-        <p><b>Cost</b>: 50 Sp (5E25)</p>
-        <p><b>Effect</b>: Whenever you cast a spell, your production is increased by 250% for 20 seconds. If another spell is cast while this effect is active, it is restored to full duration.</p>
+        <p><b>Cost</b>: 50 Sp (5e25)</p>
+        <p><b>Effect</b>: Increase the production of all buildings based on the amount of active spells.</p>
+        <p><b>Formula</b>: (15 * x)%, where x is active spells.</p>
         <br/>
-        <p><b><img src="http://musicfamily.org/realm/Factions/picks/CyclopeanStrengthFactionUpgrade.png" alt="Cyclopean Strength" align="middle"> Cyclopean Strength</b></p>
-        <p><b>Cost</b>: 500 Sp (5E26)</p>
-        <p><b>Effect</b>: Each assistant increases the production of all buildings by 0.25% per Hall of Legends you own.</p>
+        <p><b><img src="http://musicfamily.org/realm/Factions/picks/TitanSizedWallsFactionUpgrade.png" alt="Titan Sized Walls" align="middle"> Titan Sized Walls</b></p>
+        <p><b>Cost</b>: 500 Sp (5e26)</p>
+        <p><b>Effect</b>: Increase Hall of Legends production based on the amount of assistants you own.</p>
+        <p><b>Formula</b>: (15 * x)%, where x is active spells.</p>
         <hr>
         <p><b>Tier 3 Upgrades</b></p>
         <p><b><img src="http://musicfamily.org/realm/Factions/picks/TitansAllianceFactionUpgrade.png" alt="Titan Alliance" align="middle"> Titan Alliance</b></p>
         <p><b>Cost</b>: 10,000 Angel Coins + 10,000 Goblin Coins</p>
         <p><b>Effect</b>: Unlocks Tier 3 Titan Upgrades.</p>
         <br/>
-        <p><b><img src="http://musicfamily.org/realm/Factions/picks/TitanSizedWallsFactionUpgrade.png" alt="Titan Sized Walls" align="middle"> Titan Sized Walls</b></p>
-        <p><b>Cost</b>: 50 Oc (5E28)</p>
-        <p><b>Effect</b>: Increases the production of Monasteries by 25,000%</p>
+        <p><b><img src="http://musicfamily.org/realm/Factions/picks/CyclopeanStrengthFactionUpgrade.png" alt="Cyclopean Strength" align="middle"> Cyclopean Strength</b></p>
+        <p><b>Cost</b>: 50 Oc (5e28)</p>
+        <p><b>Effect</b>: Assistants count 200% more, and increase their production by 200%.</p>
         <br/>
         <p><b><img src="http://musicfamily.org/realm/Factions/picks/HeavyCoinsFactionUpgrade.png" alt="Heavy Coins" align="middle"> Heavy Coins</b></p>
-        <p><b>Cost</b>: 500 Oc (5E29)</p>
-        <p><b>Effect</b>: Increases Royal Exchanges production bonus by an additional 40% each.</p>
+        <p><b>Cost</b>: 500 Oc (5e29)</p>
+        <p><b>Effect</b>: Increases Royal Exchanges production bonus by an additional 40% each, and reduces Royal Exchange cost multiplier by 0.01; with no other reductions applying, the multiplier will be 1.09 isntead of 1.1.</p>
         <br/>
-        <p><b><img src="http://musicfamily.org/realm/Factions/picks/TitanObeliskFactionUpgrade.png" alt="Titan Obelisk" align="middle"> Titan Obelisk</b></p>
-        <p><b>Cost</b>: 5 No (5E30)</p>
-        <p><b>Effect</b>: Increase the production of all other buildings by 2.25% per Ancient Pyramid you own.</p>
+        <p><b><img src="http://musicfamily.org/realm/Factions/picks/OversizedLegendsFactionUpgrade.png" alt="Oversized Legends" align="middle"> Oversized Legends</b></p>
+        <p><b>Cost</b>: 5 No (5e30)</p>
+        <p><b>Effect</b>: Reduce Hall of Legends building cost multiplier by 0.02; with no other reductions applying, the multiplier will be 1.13 instead of 1.15.</p>
+        <p><b>Effect</b>: Also increase the production of assistants based on the amount of Halls of Legends built.</p>
+        <p><b>Formula</b>: (6 * x ^ 0.6)%, where x is the amount of Hall of Legends built.</p>
         <hr>
         <p><img src="http://musicfamily.org/realm/Factions/picks/TitanHeritage.png" alt="Titan Heritage" align="middle"><b> Titan Heritage</b></p>
         <p><b>Requirements</b>: Titan Champion Trophy</p>
@@ -111,17 +116,18 @@
         <p><b>Titan Research Requirements</b></p>
         <p><b><img src="http://musicfamily.org/realm/Factions/picks/OlympianHallsQuestFactionUpgrade.png" alt="Smiley face" align="middle"> Olympian Hall Quest</b></p>
         <p><b>Description</b>: Khaire, ruler. More of our people will join your cause if you build enough Hall of Legends. Our architects will take care of expanding them to accommodate the almighty Titans.</p>
-        <p><b>Requirement</b>: R22 + 2000 Halls of Legends.</p>
-        <p><b>Cost</b>: 10 Dtg (1E100)</p>
+        <p><b>Requirement</b>: R22 + 2,000 Halls of Legends.</p>
+        <p><b>Cost</b>: 10 Dtg (1e100)</p>
         <br/>
         <p><b><img src="http://musicfamily.org/realm/Factions/picks/OlympianHallsFactionUpgrade.png" alt="Smiley face" align="middle"> Olympian Halls</b></p>
-        <p>Upgrade Halls of Legend to Olympian Halls, boosting their production by 15% per Royal Exchange you've purchased and unlocking more unique perks for the building.</p>
-        <p><b>Cost</b>: 10 Ttg (1E103)</p>
-        <p><b>Effect</b>: Also allows access to the Research Facilities.(after completing Secrets of the Ancients).</p>
+        <p>Upgrade Halls of Legend to Olympian Halls, boosting their production based on Royal Exchanges you purchased and unlocking more unique perks for the building.</p>
+        <p><b>Formula</b>: (0.9 * x ^ 0.9)%, where x is Royal Exchanges purchased.</p>
+        <p><b>Cost</b>: 10 Ttg (1e103)</p>
+        <p><b>Effect</b>: Also allows access to the Research Facilities (after completing Secrets of the Ancients).</p>
         <br/>
         <p><b><img src="http://musicfamily.org/realm/Factions/picks/KeytotheLostCityFactionUpgrade.png" alt="Smiley face" align="middle"> Key to the Lost City</b></p>
         <p><b>Description</b>: Despite being thousands of years old, it's still shiny.</p>
-        <p><b>Requirements</b>: Found on the 1500th excavation, R23+</p>
+        <p><b>Requirements</b>: Found on the 1,500th excavation, R23+</p>
         <p><b>Effect</b>: Unlocks the Secrets of the Ancients.</p>
         <br/>
         <p><b><img src="http://musicfamily.org/realm/Factions/picks/SecretsoftheAncientsFactionUpgrade.png" alt="Smiley face" align="middle"> Secrets of the Ancients</b></p>
@@ -133,15 +139,14 @@
         <p><b><img src="http://musicfamily.org/realm/Factions/picks/AncientDeviceFactionUpgrade.png" alt="Smiley face" align="middle"> Ancient Device</b></p>
         <p><b>Description</b>: This strange device seems to react to the Ancient Races magical capabilities... We may channel its power to increase their research potential!</p>
         <p><b>Type</b>: Quest Artifact and Upgrade</p>
-        <p><b>Requirements</b>: Play a Neutral Faction, + 2,000 excavations, Unique Building.</p>
-        <p><b>Cost</b>: 100 QiSxg (1E200)</p>
+        <p><b>Requirements</b>: Play a Neutral Faction, + 2,000 Excavations, Unique Building.</p>
+        <p><b>Cost</b>: 100 QiSxg (1e200)</p>
         <p><b>Effect</b>: Adds 2 extra slots to the Neutral faction you are playing.</p>
         <p><b>Note</b>: Titans are related to the Craftsmanship and Warfare Facilities.</p>
         <p><b>Chance</b>: 0.2% per excavation</p>
         <br/>
         <p><b>Unique Building</b></p>
         <p><img src="http://musicfamily.org/realm/Factions/picks/TitanUniqueBuilding.png" align="middle"></p>
-        <p>Upgraded from Hall of Legends</p>
         <p>At R22, the Titan's Hall Of Legends building can be upgraded to a Unique Building: Olympian Halls, which is required for some challenges and research quests.</p>
         <hr>
         <p><b>Ascension 2 - R100+</b></p>
@@ -154,8 +159,8 @@
         <p><b>Formula</b>: (1 + 0.3 * level)</p>
         <br/>
         <p><img src="http://musicfamily.org/realm/Factions/picks/MountainPalaceQuest.png" alt="Mountain Palace Quest" align="middle"> <b>Mountain Palace Quest</b></p>
-		<p><b>Description</b>: Khaire, ruler. Our kind want to reshape the mountain into something more fitting, but we need funds. Make enough Royal Exchanges, and we'll let you see our masterpiece.</p>
-        <p><b>Requirement</b>: 3750 Royal Exchanges</p>
+	    	<p><b>Description</b>: Khaire, ruler. Our kind want to reshape the mountain into something more fitting, but we need funds. Make enough Royal Exchanges, and we'll let you see our masterpiece.</p>
+        <p><b>Requirement</b>: 4,000 Royal Exchanges</p>
         <p><b>Cost</b>: 1 Ud (1e36) Emerald Coins</p>
         <p><b>Effect</b>: Grants access to Unique Building.</p>
         <br/>
@@ -168,7 +173,7 @@
         <p><img src="http://musicfamily.org/realm/Factions/picks/MountainPalaceUniqueBuilding.png" alt="Mountain Palace" align="middle"></p>
         <p>Upgrade Deep Mines to Mountain Palaces, boosting their production based on buildings owned and unlocking more unique perks for the building.</p>
         <p><b>Effect</b>: Increase production based on buildings owned.</p>
-        <p><b>Formula</b>: (0.75 * x ^ 0.75), where x is buildings owned.</p>
+        <p><b>Formula</b>: (0.75 * x ^ 0.75)%, where x is buildings owned.</p>
         <p><b>Effect</b>: Grants access to Faction Union.</p>
         <br/>
         <p><img src="http://musicfamily.org/realm/Factions/picks/TitansUnion.png" alt="Titans Union" align="middle"> <b>Titans Union</b></p>
@@ -176,26 +181,26 @@
         <p><b>Cost</b>: 1 Qi (1e18) Angel and Goblin Coins</p>
         <p><b>Effect Requirement</b>: R111+</p>
         <p><b>Effect</b>: Multiplicatively increase Faction Coin find chance based on time spent in this game.</p>
-        <p><b>Formula</b>: 50 * (x / 3600) ^ 0.85), where x is time spent in this game.(per hours).</p>
-        <p><b>Effect</b>: Also increase Maximum Mana based on time spent in this game.</p>
-        <p><b>Formula</b>: (20 * x ^ 0.75), where x is time spent in this game.(in seconds)</p>
+        <p><b>Formula</b>: 50 * (x / 3600) ^ 0.85)%, where x is time spent in this game.(per hours).</p>
+        <p><b>Effect</b>: Also increase maximum Mana additively based on time spent in this game.</p>
+        <p><b>Formula</b>: +(20 * x ^ 0.75), where x is time spent in this game.(in seconds)</p>
         <p><b>Effect</b>: Grants access to Union Upgrades.</p>
         <br/>
         <p><img src="http://musicfamily.org/realm/Factions/picks/GiantMarket.png" alt="Giant Market" align="middle"> <b>Giant Market</b></p>
         <p><b>Requirement</b>: Titan Union</p>
         <p><b>Cost</b>: 1 Nod (1e60) Emerald Coins</p>
-        <p><b>Effect</b>: Increase Royal Exchanges count based on the amount of Mountain Palaces you own.</p>
-        <p><b>Formula</b>: (1.25 * x ^ 0.725), where x is amount of Mountain Palaces owned.</p>
+        <p><b>Effect</b>: Makes Royal Exchanges count more based on the highest amount of Mountain Palaces you built in this Reincarnation.</p>
+        <p><b>Formula</b>: (x ^ 0.7)%, where x is the highest amount of Mountain Palaces you built this Reincarnation.</p>
         <br/>
         <p><img src="http://musicfamily.org/realm/Factions/picks/TitanicAuthority.png" alt="Titanic Authority" align="middle"> <b>Titanic Authority</b></p>
         <p><b>Requirement</b>: Titan Union</p>
         <p><b>Cost</b>: 100 Qivg (1e80) Emerald Coins</p>
         <p><b>Effect</b>: Increase the production of Unique Buildings based on the amount of Royal Exchanges you made.</p>
-        <p><b>Formula</b>: (0.7 * x ^ 0.7), where x is amount of Royal Exchanges you made.</p>
+        <p><b>Formula</b>: (x ^ 0.6)%, where x is amount of Royal Exchanges bought.</p>
         <br/>
         <p><img src="http://musicfamily.org/realm/Factions/picks/ColossusKingdom.png" alt="Colossus Kingdom" align="middle"> <b>Colossus Kingdom</b></p>
         <p><b>Requirement</b>: Titan Union</p>
         <p><b>Cost</b>: 100 Tg (1e95) Emerald Coins</p>
-        <p><b>Effect</b>: Additively gain assistants based on total time spent as Order.</p>
-        <p><b>Formula</b>: (25 * x ^ 0.7), where x is seconds this R.</p>
+        <p><b>Effect</b>: Gain assistants additively based on total time spent as Order in this Reincarnation.</p>
+        <p><b>Formula</b>: +(25 * x ^ 0.7), where x is seconds this Reincarnation.</p>
         <?php include "../scripts/footer.html"; ?>
