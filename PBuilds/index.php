502b6eb88a6d23d1fc0d384e209a0879550c8694
xpt14 <43166103+xpt14@users.noreply.github.com>
2021-01-03 00:12:41 +0700

    4.0 update (#218)

--- PBuilds/index.php
+++ PBuilds/index.php
@@ -9,116 +9,136 @@
     <br/>
     <p><b>Updated June 2nd, 2019</b></p>
     <br/>
-    <p><b>Note</b>: Factions order is Angels > Dwangels(no researches) > Dwairies(R30-33)/Dwangels(R34-39)</p>
+    <p><b>Note</b>: Factions order is Angels > Dwangels(no researches) > Dwairies Research > Dwangels Research(R36-39)</p>
     <div class="shlisting">
         <div class="category">
             <div class="shelementwhole">
-                <p onclick="shohid($(this));"><b><a href="#" onclick="return false;">An Angel's Apprentice R30-39</a></b></p>
+                <p onclick="shohid($(this));"><b><a href="#" onclick="return false;">Enchanted Field-Dwairies R30-39</a></b></p>
                 <div class="autohide">
                     <p><b>Author</b>: Cyden</p>
-                    <p><b>Faction</b>: Angel/Dwarf</p>
-                    <p><b>Bloodline</b>: Druid</p>
-                    <p><b>Range</b>: 1e66 (1 Uvg) Gems - 1e93 (1 Tg) Gems</p>
+                    <p><b>Faction</b>: Fairy/Dwarf</p>
+                    <p><b>Bloodline</b>: Fairy</p>
+                    <p><b>Range</b>: 1e66 (1 Uvg) Gems - 1e90 (1 Novg) Gems</p>
                     <p>
                         <button onclick="myFunction($(this))">Copy Build</button>
-                        <input type="text" value="S135,S150,S200,S215,S330,C105,C120,C150,C250,C330,D1,D55,D245,D290,D320,E80,E135,E150,E200,E330,A50,A105,A120,A150,A330,W150,W180,W290,W320,W330">
+                        <input type="text" value="S1,S135,S215,S250,S330,C80,C120,C150,C330,C400,D55,D225,D245,D290,D320,E50,E135,E150,E200,E330,A105,A120,A150,A330,A400,W150,W180,W290,W320,W330">
                     </p>
-                    <p>S135,S150,S200,S215,S330,</p>
-                    <p>C105,C120,C150,C250,C330,</p>
-                    <p>D1,D55,D245,D290,D320,</p>
-                    <p>E80,E135,E150,E200,E330,</p>
-                    <p>A50,A105,A120,A150,A330,</p>
+                    <p>S1,S135,S215,S250,S330,</p>
+                    <p>C80,C120,C150,C330,C400,</p>
+                    <p>D55,D225,D245,D290,D320,</p>
+                    <p>E50,E135,E150,E200,E330,</p>
+                    <p>A105,A120,A150,A330,A400,</p>
                     <p>W150,W180,W290,W320,W330</p>
                 </div>
             </div>
             <div class="shelementwhole">
-                <p onclick="shohid($(this));"><b><a href="#" onclick="return false;">U-Dwairies R29-31</a></b></p>
+                <p onclick="shohid($(this));"><b><a href="#" onclick="return false;">Dwarven Forge-Dwairies R30-39</a></b></p>
                 <div class="autohide">
-                    <p><b>Author</b>: Lapare</p>
+                    <p><b>Author</b>: Cyden</p>
                     <p><b>Faction</b>: Fairy/Dwarf</p>
                     <p><b>Bloodline</b>: Fairy</p>
                     <p><b>Range</b>: 1e93 (1 Tg) Gems - ?</p>
                     <p>
                         <button onclick="myFunction($(this))">Copy Build</button>
-                        <input type="text" value="S135,S180,S200,S215,S330,C80,C120,C150,C330,C400,D55,D225,D245,D290,D320,E50,E135,E150,E330,E410,A105,A120,A150,A330,A400,W150,W180,W290,W320,W330">
-                    </p>
+                        <input type="text" value="S135,S180,S215,S250,S330,C10,C80,C150,C400,C405,D55,D245,D250,D290,D320,E50,E135,E150,E200,E330,A105,A120,A150,A330,A400,W150,W180,W290,W320,W330,S270,C135,C330">
+                    			</p>
                     <p>
                         <button onclick="myFunction($(this))">Copy Build</button>
-                        <input type="text" value="S135,S180,S200,S215,S330,C80,C120,C150,C330,C400,D55,D225,D245,D290,D320,E135,E150,E290,E330,E410,A105,A120,A150,A330,A400,W150,W180,W290,W320,W330">
-						<b>For Max Excavations</b>
-					</p>
-                    <p>
-                        <button onclick="myFunction($(this))">Copy Build</button>
-                        <input type="text" value="S135,S180,S200,S215,S330,C80,C120,C150,C330,C400,D55,D225,D245,D290,D320,E50,E135,E150,E330,E410,A30,A105,A120,A150,A330,W150,W180,W290,W320,W330">
+                        <input type="text" value="S135,S180,S215,S250,S330,C10,C80,C150,C400,C405,D55,D245,D250,D290,D320,E50,E135,E150,E200,E330,A105,A120,A150,A330,A495,W150,W180,W290,W320,W330,S270,C135,C330">
 						<b>A400 not unlocked</b>
 					</p>
-                    <p>S135,S180,S200,S215,S330,</p>
-                    <p>C80,C120,C150,C330,C400,</p>
-                    <p>D55,D225,D245,D290,D320,</p>
-                    <p>E50,E135,E150,E330,E410,</p>
+                    <p>
+                        <button onclick="myFunction($(this))">Copy Build</button>
+                        <input type="text" value="S135,S180,S215,S250,S330,C10,C80,C150,C400,C405,D55,D245,D250,D290,D320,E135,E150,E200,E290,E330,A105,A120,A150,A330,A400,W150,W180,W290,W320,W330,S270,C135,C330">
+						<b>For Max Excavations</b>
+					</p>
+                    <p>S135,S180,S215,S250,S330,</p>
+                    <p>C10,C80,C150,C400,C405,</p>
+                    <p>D55,D245,D250,D290,D320,</p>
+                    <p>E50,E135,E150,E200,E330,</p>
                     <p>A105,A120,A150,A330,A400,</p>
                     <p>W150,W180,W290,W320,W330</p>
+                    <p><b>Note</b>: After Dwarven Horn is bought, use also S270,C135,C330 (Import the build again).</p>
+                    <p><b>Note</b>: Swap A400 with A495 if you don't have A400 (requires 2 days as Druids).</p>
                     <p><b>Note</b>: Swap E50 with E290 for maximizing excavations.</p>
-                    <p><b>Note</b>: Swap A400 with A30 if you don't have A400.</p>
                 </div>
             </div>
             <div class="shelementwhole">
-                <p onclick="shohid($(this));"><b><a href="#" onclick="return false;">U-Dwairy R32-33</a></b></p>
+                <p onclick="shohid($(this));"><b><a href="#" onclick="return false;">Inn-Dwairies R34-39</a></b></p>
                 <div class="autohide">
                     <p><b>Author</b>: Cyden</p>
                     <p><b>Faction</b>: Fairy/Dwarf</p>
                     <p><b>Bloodline</b>: Fairy</p>
-                    <p><b>Range</b>: 1e93 (1 Tg) Gems - 1e126 (1 Uqag) Gems</p>
+                    <p><b>Range</b>: 1e111 (1 Sxtg) Gems - 1e132 (1 Tqag) Gems</p>
                     <p>
                         <button onclick="myFunction($(this))">Copy Build</button>
-                        <input type="text" value="S135,S200,S215,S330,S500,C80,C120,C150,C400,C500,D55,D225,D245,D290,D320,E50,E135,E150,E330,E410,A105,A120,A150,A330,A400,W150,W180,W290,W320,W330,S1,C330,C520">
-                    </p>
+                        <input type="text" value="S215,S330,S500,S545,S590,C10,C80,C120,C150,C400,D55,D205,D250,D290,D320,E135,E150,E200,E330,E410,A105,A120,A150,A330,A400,W120,W150,W180,W320,W330,S560,C135,C500">
+                    			</p>
                     <p>
                         <button onclick="myFunction($(this))">Copy Build</button>
-                        <input type="text" value="S135,S200,S215,S330,S500,C80,C120,C150,C400,C500,D55,D225,D245,D290,D320,E135,E150,E290,E330,E410,A105,A120,A150,A330,A400,W150,W180,W290,W320,W330,S1,C330,C520">
-						<b>For Max Excavations</b>
+                        <input type="text" value="S180,S215,S330,S545,S590,C10,C80,C120,C150,C400,D55,D205,D250,D290,D320,E135,E150,E200,E330,E410,A105,A120,A150,A330,A400,W120,W150,W180,W320,W330,S560,C135,C500">
+						<b>S500 not unlocked</b>
 					</p>
                     <p>
                         <button onclick="myFunction($(this))">Copy Build</button>
-                        <input type="text" value="S135,S200,S215,S330,S500,C80,C120,C150,C400,C500,D55,D225,D245,D290,D320,E50,E135,E150,E330,E410,A105,A120,A150,A330,A400,W150,W180,W290,W320,W330,S1,C330,C520">
+                        <input type="text" value="S215,S330,S500,S545,S590,C10,C80,C120,C150,C400,D55,D205,D250,D290,D320,E135,E150,E200,E330,E410,A105,A120,A150,A330,A495,W120,W150,W180,W320,W330,S560,C135,C500">
 						<b>A400 not unlocked</b>
 					</p>
-                    <p>S135,S200,S215,S330,S500,</p>
-                    <p>C80,C120,C150,C400,C500,</p>
-                    <p>D55,D225,D245,D290,D320,</p>
-                    <p>E50,E135,E150,E330,E410,</p>
-                    <p>A105,A120,A150,A330,A400</p>
-                    <p>W150,W180,W290,W320,W330</p>
-                    <p><b>Note</b>: Use the template of the previous R if you don't have S500 (requires 40 or more artifacts).</p>
-                    <p><b>Note</b>: After buying Dwarven Horn use also S1,C330,C520.</p>
-                    <p><b>Note</b>: Swap E50 with E290 for maximizing excavations.</p>
-                    <p><b>Note</b>: Swap A400 with A330 if you don't have A400 (requires 2 days as Druids).</p>
+                    <p>
+                        <button onclick="myFunction($(this))">Copy Build</button>
+                        <input type="text" value="S180,S215,S330,S545,S590,C10,C80,C120,C150,C400,D55,D205,D250,D290,D320,E135,E150,E200,E330,E410,A105,A120,A150,A330,A495,W120,W150,W180,W320,W330,S560,C135,C500">
+						<b>S500 and A400 not unlocked</b>
+					</p>
+                    <p>
+                        <button onclick="myFunction($(this))">Copy Build</button>
+                        <input type="text" value="S215,S330,S500,S545,S590,C10,C80,C120,C150,C400,D55,D205,D250,D290,D320,E150,E200,E290,E330,E410,A105,A120,A150,A330,A400,W120,W150,W180,W320,W330,S560,C135,C500">
+						<b>For Max Excavations</b>
+					</p>
+                    <p>S215,S330,S500,S545,S590,</p>
+                    <p>C10,C80,C120,C150,C400,</p>
+                    <p>D55,D205,D250,D290,D320,</p>
+                    <p>E135,E150,E200,E330,E410,</p>
+                    <p>A105,A120,A150,A330,A400,</p>
+                    <p>W120,W150,W180,W320,W330</p>
+                    <p><b>Note</b>: After Dwarven Horn is bought, use also S560,C135,C500 (Import the build again).</p>
+                    <p><b>Note</b>: Swap S500 with S180 if you don't have S500 (requires 40 total artifacts).</p>
+                    <p><b>Note</b>: Swap A400 with A495 if you don't have A400 (requires 2 days as Druids).</p>
+                    <p><b>Note</b>: Swap E135 with E290 for maximizing excavations.</p>
                 </div>
             </div>
             <div class="shelementwhole">
-                <p onclick="shohid($(this));"><b><a href="#" onclick="return false;">An Angelic Ascension R34-39</a></b></p>
+                <p onclick="shohid($(this));"><b><a href="#" onclick="return false;">Heaven's Domain-Dwangel R36-39</a></b></p>
                 <div class="autohide">
                     <p><b>Author</b>: Cyden</p>
                     <p><b>Faction</b>: Angel/Dwarf</p>
                     <p><b>Bloodline</b>: Druid</p>
-                    <p><b>Range</b>: 1e93 (1 Tg) Gems - 1e144 (1 Spqag) Gems</p>
+                    <p><b>Requirement</b>: Dwarven Horn (Horn of the Kings artifact)</p>
+                    <p><b>Duration</b>: 15+ minutes</p>			
+                    <p><b>Range</b>: 1e132 (1 Tqag) Gems - 1e144 (1 Spqag) Gems</p>
                     <p>
                         <button onclick="myFunction($(this))">Copy Build</button>
-                        <input type="text" value="S135,S150,S215,S330,S500,C120,C150,C400,C500,C520,D55,D290,D320,D435,D590,E80,E150,E200,E330,E410,A105,A120,A150,A400,A545,W150,W290,W320,W330,W400,C250,C330,D1">
+                        <input type="text" value="S135,S150,S215,S250,S330,C120,C135,C150,C200,C400,C500,C520,D1,D55,D175,D320,D435,D590,E135,E150,E200,E330,E410,A105,A150,A330,A400,A495,W150,W225,W290,W320,W330">
 					</p>
                     <p>
                         <button onclick="myFunction($(this))">Copy Build</button>
-                        <input type="text" value="S135,S150,S215,S330,S500,C120,C150,C400,C500,C520,D55,D290,D320,D435,D590,E80,E150,E200,E330,E410,A105,A120,A150,A400,A545,W150,W290,W320,W330,W400,C250,C330,D480">
-						<b>D480</b>
+                        <input type="text" value="S135,S150,S215,S250,S330,C120,C135,C150,C200,C400,C500,C520,D1,D55,D175,D320,D435,D590,E135,E150,E200,E330,E410,A105,A150,A330,A495,A545,W150,W225,W290,W320,W330">
+						<b>A400 not unlocked</b>
 					</p>
-                    <p>S135,S150,S215,S330,S500,</p>
-                    <p>C120,C150,C400,C500,C520,</p>
-                    <p>D55,D290,D320,D435,D590,</p>
-                    <p>E80,E150,E200,E330,E410,</p>
-                    <p>A105,A120,A150,A400,A545,</p>
-                    <p>W150,W290,W320,W330,W400</p>
-                    <p><b>Note</b>: After buying Dwarven Horns use also C250,C330,D1.</p>
-                    <p><b>Note</b>: Swap D1 with D480 if buffed.</p>
+                    <p>
+                        <button onclick="myFunction($(this))">Copy Build</button>
+                        <input type="text" value="S135,S150,S215,S250,S330,C120,C135,C150,C200,C400,C500,C520,D1,D55,D175,D320,D435,D590,E150,E200,E290,E330,E410,A105,A150,A330,A400,A495,W150,W225,W290,W320,W330">
+						<b>For Max Excavations</b>
+					</p>
+                    <p>S135,S150,S215,S250,S330,</p>
+                    <p>C120,C135,C150,C200,C400,C500,C520,</p>
+                    <p>D1,D55,D175,D320,D435,D590,</p>
+                    <p>E135,E150,E200,E330,E410,</p>
+                    <p>A105,A150,A330,A400,A495,</p>
+                    <p>W150,W225,W290,W320,W330</p>
+                    <p><b>Note</b>: The build is a grower build, meaning that the build will benefit more from longer (15 minutes or more) runs instead of shorter (2-5 minutes) runs.</p>			
+                    <p><b>Note</b>: It's heavily recommended that you have Dwarven Horn before running this build (and ascending).</p>
+                    <p><b>Note</b>: Swap A400 with A545 if you don't have A400 (requires 2 days as Druids).</p>
+                    <p><b>Note</b>: Swap E135 with E290 for maximizing excavations.</p>			
                 </div>
             </div>
         </div>
@@ -127,20 +147,20 @@
             <div class="shelementwhole">
                 <p onclick="shohid($(this));"><b> <a href="#" onclick="return false;">Van!shment R29+<font color="DarkRed"> Evil</font></a></b></p>
                 <div class="autohide">
-                    <p><b>Author</b>: Lapare modified by Cyden</p>
+                    <p><b>Author</b>: Cyden</p>
                     <p><b>Faction</b>: Undead/Drow</p>
                     <p><b>Bloodline</b>: Druid</p>
                     <p><b>Range</b>: ? Gems</p>
                     <p>
                         <button onclick="myFunction($(this))">Copy Build</button>
-                        <input type="text" value="S135,S215,S225,S330,S500,C120,C250,C150,C330,C400,D25,D55,D290,D320,D400,E10,E80,E135,E150,E330,A55,A105,A120,A480,A545,W135,W180,W205,W400,W405">
+                        <input type="text" value="S135,S215,S225,S330,S500,C120,C135,C150,C330,C400,D25,D55,D290,D320,D400,E10,E80,E135,E150,E330,A55,A105,A120,A175,A480,W135,W150,W180,W290,W320,W330">
                     </p>
                     <p>S135,S215,S225,S330,S500,</p>
                     <p>C120,C135,C150,C330,C400,</p>
                     <p>D25,D55,D290,D320,D400,</p>
                     <p>E10,E80,E135,E150,E330,</p>
-                    <p>A55,A105,A120,A480,A545,</p>
-                    <p>W135,W205,W250,W400,W405</p>
+                    <p>A55,A105,A120,A175,A480,</p>
+                    <p>W150,W180,W290,W320,W330</p>
                 </div>
             </div>
             <div class="shelementwhole">
@@ -152,10 +172,10 @@
                     <p><b>Range</b>: 1e130 (10 Dqag) Gems</p>
                     <p>
                         <button onclick="myFunction($(this))">Copy Build</button>
-                        <input type="text" value="S50,S135,S215,S330,S500,C120,C135,C150,C330,C400,D55,D245,D250,D290,D320,E80,E150,E225,E290,E330,A10,A105,A120,A150,A330,W150,W180,W290,W320,W330">
+                        <input type="text" value="S135,S215,S250,S330,S500,C120,C135,C150,C400,C500,D55,D245,D250,D290,D320,E80,E150,E225,E290,E330,A10,A105,A120,A150,A330,W150,W180,W290,W320,W330">
                     </p>
-                    <p>S50,S135,S215,S330,S500,</p>
-                    <p>C120,C135,C150,C330,C400,</p>
+                    <p>S135,S215,S250,S330,S500,</p>
+                    <p>C120,C135,C150,C400,C500,</p>
                     <p>D55,D245,D250,D290,D320,</p>
                     <p>E80,E150,E225,E290,E330,</p>
                     <p>A10,A105,A120,A150,A330,</p>
