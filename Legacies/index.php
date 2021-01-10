e3b861e01a2c72aa027cf6e4d1e53e8645130c86
Curunur <40272706+Curunur@users.noreply.github.com>
2021-01-10 12:02:10 +0100

    Map Test 2

--- Legacies/index.php
+++ Legacies/index.php
@@ -6,23 +6,36 @@
     <?php include "../scripts/header.html"; ?>
         <h6><img src="http://musicfamily.org/realm/Factions/picks/LegacyBanner.png"></h6>
         <p>
-            <h6>Legacies</h6>
+        <h6>Legacies</h6>
            
-            <center><b>Legacy Menu</b></center>
-            <center><img src="http://musicfamily.org/realm/Factions/picks/LegacyMenu.png" usemap="#Legacies-map"></center>
-            <map name="Legacies-map">
-                <area href="#FairyBronzeLegacy" research="
-				<p><img src=http://musicfamily.org/realm/Factions/picks/KindHeartsFactionUpgrade.png align=middle><b>Fairy Bronze Legacy</b></p>
-        <p>Pixie Dust Fertilizer</p>
-				<p><b>Effect</b>: Increase the base production of Farms by +98.</p>
-        <p><b>Effect</b>: Reduces cost multiplier by 0.02; with no other reductions applying, the multiplier will be 1.13 instead of 1.15.</p>
-        <p></p>
-        <p>Fairy Workers</p>
-        <p><b>Effect</b>: Increase the production of Farms, Inns and Blacksmiths by 15,000%.</p>
-        <p></p>
-        <p>Kind Hearts</p>
-        <p><b>Effect</b>: Increases maximum Mana additively based on Good buildings you own.</p>
-        <p><b>Formula</b>: +(1.3 * x ^ 0.7), where x is the amount of Good buildings you own.</p>
-				" coords="10,10,64,64" shape="rect">      
-            </map>
-            <?php include "../scripts/footer.html"; ?>
+        <center><b>Legacy Menu</b></center>
+        <center><img src="http://musicfamily.org/realm/Factions/picks/LegacyMenu.png" usemap="#Legacies-map"></center>
+        <map name="Legacies-map">
+        <area href="#FairyBronzeLegacy" research="
+		<p><img src=http://musicfamily.org/realm/Factions/picks/KindHeartsFactionUpgrade.png align=middle><b> Fairy Bronze Legacy</b></p>
+		<p><b>Pixie Dust Fertilizer</b></p>
+		<p><b>Effect</b>: Increase the base production of Farms by +98.</p>
+        	<p><b>Effect</b>: Reduces cost multiplier by 0.02; with no other reductions applying, the multiplier will be 1.13 instead of 1.15.</p>
+        	<p></p>
+		<p><b>Fairy Workers</b></p>
+        	<p><b>Effect</b>: Increase the production of Farms, Inns and Blacksmiths by 15,000%.</p>
+        	<p></p>
+		<p><b>Kind Hearts</b></p>
+        	<p><b>Effect</b>: Increases maximum Mana additively based on Good buildings you own.</p>
+        	<p><b>Formula</b>: +(1.3 * x ^ 0.7), where x is the amount of Good buildings you own.</p>
+	" coords="10,10,64,64" shape="rect">
+	<area href="#FairySilverLegacy" research="
+		<p><img src=http://musicfamily.org/realm/Factions/picks/SpellsmithFactionUpgrade.png align=middle><b> Fairy Silver Legacy</b></p>
+		<p><b>Fairy Cuisine</b></p>
+		<p><b>Effect</b>: Increase the base production of Inns by +234 and reduce the building cost multiplier.</p>
+        	<p><b>Effect</b>: Reduces cost multiplier by 0.02; with no other reductions applying, the multiplier will be 1.13 instead of 1.15.</p>
+        	<p></p>
+		<p><b>Golden Pots</b></p>
+        	<p><b>Effect</b>: Additively increase base clicking reward by 20% of the building production of Farms, Inns and Blacksmiths combined.</p>
+        	<p></p>
+		<p><b>Spellsmith</b></p>
+        	<p><b>Effect</b>: Blacksmiths also increase your Mana Regeneration additively.</p>
+        	<p><b>Formula</b>: (x ^ 0.25), where x is the number of Blacksmiths you own.</p>
+	" coords="70,10,70,124" shape="rect"> 
+        </map>
+	<?php include "../scripts/footer.html"; ?>
