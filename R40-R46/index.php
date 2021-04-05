7c6effd75c27996fbab97e44754d5047bf5a1952
xpt14 <43166103+xpt14@users.noreply.github.com>
2021-04-05 23:13:31 +0700

    build & dragon unlock update

--- R40-R46/index.php
+++ R40-R46/index.php
@@ -5,84 +5,62 @@
 <meta http-equiv="Content-Type" content="text/html; charset=windows-1252">
 <?php include "../scripts/header.html"; ?>
     <h6><img src="http://musicfamily.org/realm/Factions/picks/TopPageResearch.png" alt="Spellcraft" align="middle"></h6>
-	<p style="color:red"><b>All Builds are outdated as of v4.0. Use the <a target="_blank" href="https://discord.gg/cq6zmQX"><b>Realm Grinder Discord</a> for build testing and crafting.</b></p>
-	<br/>
-    <p><b>Note</b>: It is recommended to get at least 100 Oc (1e29) gems before starting stage 3 of Dragon Unlocks.</p>
+	<p><b>Optimal Tiering for R42-R46:</b></p>
+	<p>R46: Demon spells to Tier 2: 1 day 6 hours and 3 minutes (of their spells activity time in this R)</p>
+	<p>     Elf spells to Tier 2: 20 hours and 13 minutes</p>
+    <p><b>Note</b>: It is recommended to get at least 1 No (1e30) gems before ending stage 3 of Dragon unlock.</p>
     <p><b>Note</b>: Make sure to get <b>Prismatic Mana</b> trophy and <b>Fortune Teller Machine</b> as soon as possible! </p>
     <br/>
     <div class="shlisting">
         <p><b>Production</b></p>
         <div class="category">
 		    <div class="shelementwhole">
-                <p onclick="shohid($(this));"><b><a href="#" onclick="return false;">U Druidline Dwangels (R40-R46)<font color="Blue"> Good</font></a></b></p>
+                <p onclick="shohid($(this));"><b><a href="#" onclick="return false;">U Druidline Goblin (R40-R46)<font color="DarkRed"> Evil</font></a></b></p>
                 <div class="autohide">
-                    <p><b>Author</b>: Cyden</p>
-                    <p><b>Requirements</b>: Horn of the Kings</p>
+                    <p><b>Author</b>: Ensteffahn</p>
+                    <p><b>Requirements</b>: Flame of Bondelnar artifact</p>
                     <p><b>Range</b>: 0 Gems - ?</p>
-                    <p><b>Faction</b>: Angel</p>
+                    <p><b>Faction</b>: Goblin/Drow</p>
                     <p><b>Bloodline</b>: Druid</p>
                     <p>
                         <button onclick="myFunction($(this))">Copy Build</button>
-                        <input type="text" value="S135,S150,S250,S330,S500,C120,C150,C330,C340,C400,C25,C500,D175,D200,D290,D320,D435,D590,E80,E135,E145,E330,E410,A105,A120,A150,A250,A330,W150,W180,W290,W320,W400">
+                        <input type="text" value="S50,S135,S215,S330,S500,C150,C175,C340,C400,C500,D55,D200,D250,D290,D320,E1,E80,E225,E330,E590,A105,A120,A150,A250,A410,W25,W150,W180,W205,W290,E135,W320,W400">
                     </p>
-                    <p>S135,S150,S250,S330,S500,</p>
-                    <p>C120,C150,C330,C340,C400,C25,C500,</p>
-                    <p>D175,D200,D290,D320,D435,D590,</p>
-                    <p>E80,E135,E145,E330,E410,</p>
-                    <p>A105,A120,A150,A250,A330,</p>
-                    <p>W150,W180,W290,W320,W400</p>
-                    <p><b>Notes</b>: C branch research placements is intentional (reimport build after horn).</p>
-					<p><b>Notes</b>: After 1 hour this R, the NU build becomes better.</p>
+                    <p>S50,S135,S215,S330,S500,</p>
+                    <p>C150,C175,C340,C400,C500,</p>
+                    <p>D55,D200,D250,D290,D320,</p>
+                    <p>E1,E80,E225,E330,E590,</p>
+                    <p>A105,A120,A150,A250,A410,</p>
+                    <p>W25,W150,W180,W205,W290</p>
+                    <p><b>Note</b>: Pick E135,W320,W400 after Flame of Bondelnar is bought (reimport the build).</p>
                 </div>
             </div>
-            <div class="shelementwhole">
-                <p onclick="shohid($(this));"><b><a href="#" onclick="return false;">NU Druidline Dwangels (R40-R46) + Stage 1, 4 Unlock<font color="Blue"> Good</font></a></b></p>
+<div class="shelementwhole">
+                <p onclick="shohid($(this));"><b><a href="#" onclick="return false;">NU Druidline Goblin (R40-R46) - Dragon stage 4 Unlock<font color="DarkRed"> Evil</font></a></b></p>
                 <div class="autohide">
                     <p><b>Author</b>: Cyden</p>
-                    <p><b>Requirements</b>: 1 hour this R, Fortune Teller Machine, Horn of the Kings, Druid challenge 4</p>
-                    <p><b>Requirements</b>: (for Stage 4 Unlock) Mana Waste</p>
+                    <p><b>Requirements</b>: Fortune Teller Machine artifact, Flame of Bondelnar</p>
                     <p><b>Range</b>: 0 Gems - ?</p>
-                    <p><b>Faction</b>: Angel</p>
+                    <p><b>Faction</b>: Goblin/Drow</p>
                     <p><b>Bloodline</b>: Druid</p>
                     <p>
                         <button onclick="myFunction($(this))">Copy Build</button>
-                        <input type="text" value="S10,S150,S215,S330,S500,C80,C150,C330,C340,C400,C25,C500,D55,D175,D200,D250,D290,D320,E135,E145,E330,E410,E480,A105,A120,A150,A250,A330,W120,W150,W180,W330,W400">
+                        <input type="text" value="S50,S150,S215,S330,S500,C80,C150,C340,C400,C500,D55,D200,D250,D290,D320,E200,E330,E480,E495,E590,E135,A10,A105,A120,A150,A410,W25,W120,W150,W205,W330,W320,W400">
                     </p>
-                    <p>
-                        <button onclick="myFunction($(this))">Copy Build</button>
-                        <input type="text" value="S10,S150,S215,S330,S500,C80,C150,C330,C340,C400,C25,C500,D55,D175,D200,D250,D290,D320,E135,E145,E330,E410,E480,A105,A120,A150,A250,A330,W120,W150,W180,W320,W400">
-						<b>W320</b>
-                    </p>
-                    <p>
-                        <button onclick="myFunction($(this))">Copy Build</button>
-                        <input type="text" value="S10,S150,S215,S330,S500,C80,C150,C330,C340,C400,C25,C500,D55,D175,D200,D250,D290,D320,E135,E145,E330,E410,E480,A105,A120,A150,A250,A400,W120,W150,W180,W330,W400">
-						<b>A400</b>
-                    </p>
-                    <p>
-                        <button onclick="myFunction($(this))">Copy Build</button>
-                        <input type="text" value="S10,S150,S215,S330,S500,C80,C150,C330,C340,C400,C25,C500,D55,D175,D200,D250,D290,D320,E135,E145,E330,E410,E480,A105,A120,A150,A250,A400,W120,W150,W180,W320,W400">
-						<b>A400, W320</b>
-                    </p>
-                    <p>
-                        <button onclick="myFunction($(this))">Copy Build</button>
-                        <input type="text" value="S10,S150,S200,S330,S500,C80,C150,C330,C340,C400,C25,C500,D55,D175,D200,D205,D290,D320,E135,E145,E330,E410,E480,A105,A150,A250,A270,A400,W120,W150,W180,W320,W400">
-						<b>16 hours+</b>
-                    </p>
-                    <p>S10,S150,S215,S330,S500,</p>
-                    <p>C80,C150,C330,C340,C400,C25,C500,</p>
-                    <p>D55,D175,D200,D250,D290,D320,</p>
-                    <p>E135,E145,E330,E410,E480,</p>
-                    <p>A105,A120,A150,A250,A330,</p>
-                    <p>W120,W150,W180,W330,W400</p>
-                    <p><b>Notes</b>: Swap W330->W320 if higher.</p>
-                    <p><b>Notes</b>: Swap A330->A400 if higher.</p>
-                    <p><b>Notes</b>: Swap S215->S200, D250->D205, A120->A270 with at least 16 hours this R.</p>
-                    <p><b>Notes</b>: For Stage 4, requires 1e29 (100 Oc) Gems+. Estimated runtime under 12 hours.</p>
-                    <p><b>Notes</b>: C branch research placements is intentional (reimport build after horn).</p>
+                    <p>S50,S150,S215,S330,S500,</p>
+                    <p>C80,C150,C340,C400,C500,</p>
+                    <p>D55,D200,D250,D290,D320,</p>
+                    <p>E200,E330,E480,E495,E590,</p>
+                    <p>A10,A105,A120,A150,A410,</p>
+                    <p>W25,W120,W150,W205,W330</p>
+                    <p><b>Note</b>: Pick E135,W320,W400 after Flame of Bondelnar is bought (reimport the build).</p>
+			<p><b>Note</b>: Grows stronger as you spend more time in this R.</p>
+			<p><b>Note</b>: The build becomes better than U Goblin after approximately 2.5 hours spent in this R.</p>
+			<p><b>Note</b>: For R46: At 1e15 (1 Qa) gems swap E200->E290 and excavate Spiky Rough Egg lore artifact (at 1500th excavation) then immediately start Dragon unlock (Read the below section). From here onward <b>only use this build to gain gems if you need to do so in order to finish a stage of Dragon unlock<b/>, or to finish R46.</p>
                 </div>
             </div>
         </div>
-        <br />
+        <br/>
 		<p><b>Buff</b></p>
 		<div class="category">
 		    <div class="shelementwhole">
@@ -108,13 +86,13 @@
             </div>
         </div>
         <br />
-        <p><b>Dragon Unlock</b></p>
+        <p><b>Dragon unlock</b></p>
         <div class="category">
             <div class="shelementwhole">
-                <p onclick="shohid($(this));"><b><a href="#" onclick="return false;">Overview & Notes on Dragon unlock</b></a>
+                <p onclick="shohid($(this));"><b><a href="#" onclick="return false;">4 Stages of Dragon unlock</b></a>
                 </p>
                 <div class="autohide">
-                    <p><b>Overview</b>: After excavating the quest artifact "Spiky Rough Egg", Dragon Unlock is divided into 4 parts commonly refered to as "Stages". Each stages come with a compulsory 12 hours wait, meaning that Dragon Unlock is guaranteed to last at least 2 real time days</p>
+                    <p><b>Overview</b>: After excavating the quest artifact "Spiky Rough Egg", Dragon unlock is divided into 4 parts commonly refered to as "Stages". Each stages come with a compulsory unskippable 12 hours wait, meaning that Dragon unlock is guaranteed to last at least 2 days in real time.</p>
                     <p><b>Stage 0</b>: Excavate "Spiky Rough Egg"</p>
                     <p><b>Requirement</b>: R46+, 1500+ Excavations (Chance 2%)</p>
                     <br/>
@@ -123,10 +101,10 @@
                     <p><b>Cost</b>: 1e64 (10 Vg) coins, 1e7 (10 M) of each FC</p>
                     <br/>
                     <p><b>Stage 2</b>: Drakeling</p>
-                    <p><b>Requirement</b>: Hatch!, 12 hours of Hellfire Blast (start counting AFTER you buy Hatch!)</p>
+                    <p><b>Requirement</b>: Hatch!, 12 hours of Hellfire Blast activity time (start counting AFTER you buy Hatch!)</p>
                     <p><b>Cost</b>: 1e67 (10 Uvg) coins, 1e8 (100 M) of each FC</p>
                     <br/>
-                    <p><b>Stage 3</b>: Dragon (nope, you aren't getting dragon here, not yet)</p>
+                    <p><b>Stage 3</b>: Dragon (you aren't getting dragon here, it's just the name)</p>
                     <p><b>Requirement</b>: Drakeling, 12 hours since buying "Drakeling", 3750 Fairy assistants</p>
                     <p><b>Cost</b>: 1e70 (10 Dvg) coins, 1e9 (1 B) of each FC</p>
                     <br/>
@@ -134,34 +112,16 @@
                     <p><b>Requirement</b>: Dragon, 12 hours since buying "Dragon", 50000 max mana</p>
                     <p><b>Cost</b>: 1e73 (10 Tvg) coins, 1e10 (10 B) of each FC</p>
 		    <br/>
-                    <p><b>Notes</b>: Coupled with the estimated time to reach 1e30 (1 No) gems, R46 is estimated to take you around 2d 12h in total. And since patch 3.8.0.0, time requirement for tiering spells has been greatly shortened, thus I (Cyden) recommend that you tier T3 Angel spells (spells that are visible when you play as Angel) (taking 1d 16h in total) and T2 Demon spells (taking 20h) throughout the R, for the most efficient use of your time.</p>
+                    <p><b>Note</b>: Remaining time until next stage never reset, it persists through abdications, reincarnations and ascensions.</p>
                     <br/>
                 </div>
             </div>
             <div class="shelementwhole">
-                <p onclick="shohid($(this));"><b><a href="#" onclick="return false;"> Druidline Dromon (Dragon Stage 2 unlock)<font color="darkred"> Evil</font></a></b></p>
-                <div class="autohide">
-                    <p><b>Author</b>: Cyden</p>
-                    <p><b>Faction</b>: Demon</p>
-                    <p><b>Bloodline</b>: Druid</p>
-                    <p>
-                        <button onclick="myFunction($(this))">Copy Build</button>
-                        <input type="text" value="S30,S105,S200,S400,S500,C250,C305,C330,C340,C400,D200,D250,D290,D330,D560,E135,E145,E230,E250,E320,A120,A250,A270,A400,A545,W120,W135,W175,W200,W205,W250,W400,W520">
-                    </p>
-                    <p>S30,S105,S200,S400,S500,</p>
-                    <p>C250,C305,C330,C340,C400,</p>
-                    <p>D200,D250,D290,D330,D560,</p>
-                    <p>E135,E145,E230,E250,E320,</p>
-                    <p>A120,A250,A270,A400,A545,</p>
-                    <p>W120,W135,W175,W200,W205,W250,W400,W520</p>
-                </div>
-            </div>
-            <div class="shelementwhole">
-                <p onclick="shohid($(this));"><b><a href="#" onclick="return false;"> Druidline Dwairy Stage 3 unlock<font color="Blue"> Good</font></a></b></p>
+                <p onclick="shohid($(this));"><b><a href="#" onclick="return false;"> NU Druidline Fairy Stage 3 unlock<font color="Blue"> Good</font></a></b></p>
                 <div class="autohide">
                     <p><b>Author</b>: Cyden</p>
                     <p><b>Requirements</b>: Fortune Teller Machine, Horn of the Kings</p>
-                    <p><b>Range</b>: 1e29+ Gems</p>
+                    <p><b>Range</b>: 1e30+ Gems</p>
                     <p><b>Faction</b>: Fairy/Dwarf</p>
                     <p><b>Bloodline</b>: Druid</p>
                     <p>
@@ -180,11 +140,47 @@
                     <p>A105,A120,A150,A250,A270,</p>
                     <p>W120,W150,W180,W320,W400</p>
                     <p>Notes: Swap A150->A330 if higher</p>
-                    <p><b>Notes</b>: Gets to 3750 assistants instantly without Sun Force Assistants</p>
-                    <p><b>Notes</b>: Estimated time to 1e70 coins: 15 minutes (at 1e29) or 5-10 minutes (at 1e30)</p>
+                    <p><b>Notes</b>: Gets to 3750 assistants instantly without Sun Force Assistants, the hard part is the 1e70 (10 Dvg) coins cost.</p>
+                    <p><b>Notes</b>: Estimated time to 1e70 coins: 45 minutes at 1e30 gems without spell tiers.</p>
                 </div>
             </div>
-		<p><b>Notes</b>: Keep on using the Stage 2 (Demon) build until you have an activity time of 20 hours on all of your demon spells, then spend the rest of the time (until 10-15 minutes before finishing Stage 3) as Dwangel to continute build up spell time. Before going into R47 make sure you have actually unlocked the mentioned tiers by looking at the "highest tier unlocked" in the respective spell's tooltip.</p>
+            <div class="shelementwhole">
+                <p onclick="shohid($(this));"><b><a href="#" onclick="return false;">IMPORTANT: R46 - Dragon unlock & spell tiering</b></a>
+                </p>
+                <div class="autohide">
+                    <p>As you can see from above, Dragon unlock is an unskippable 2-day-long wait with nothing of significance to do. Thus, it is absolutely recommended that you use this time to tier some spells to a higher tier, which in turn drastically saves time on future unlocks in later Reincarnations.</p>
+			<p>Said spells are: Demon spells (all spells that are visible when you join Demon) and Elf spells, which you'll tier to tier 3 and tier 2 respectively according to the optimal tiering plan at the top of the page.</p>
+			<p>So, for the most efficient use of your time, you should be spending most of your dragon unlock sitting as Demon and Elf as much as possible to build up their spells activity time.</p>
+			<p>Optionally, you can check the "Detailed plan for R46 tiering" for specific calculations of your spell tiering process.</p>
+			<p><b>Note</b>: In tiering runs, focus on researches that increase mana regen, increase spells' duration and reduce spells' mana cost.</p>
+                    <p><b>For Stage 2</b>: You do not need to be Demon in order to finish the stage, use NU Goblin to gain some gems and buy the next stage.</p>
+			<p><b>For Stage 3</b>: When nearing the end of the stage (1-2 hours remaining), use NU Goblin to gain up to at least 1e30 (1 No) gems finishing the stage with Fairy.</p>
+                </div>
+            </div>
+            <div class="shelementwhole">
+                <p onclick="shohid($(this));"><b><a href="#" onclick="return false;">Detailed plan for R46 tiering</b></a>
+                </p>
+                <div class="autohide">
+                    <p><b>Note</b>: Spell tier requirement formula (need spell activity time this R):</p>
+                    <p>(2/3) * 0.97^(R - 35) * (0.4 + 0.1 * T) * (1 + T^2) / (1 + 0.1 * U)</p>
+                    <p>Where T is the tier you're aiming for (2-6), U is your unlocked tiers (0-84), R is your Reincarnation.</p>
+                    <p>Click <a target="_blank" style="color: red;" href="https://www.desmos.com/calculator/easbpgto2s">here</a> for an online calculator.</p>
+                    <p>Click <a target="_blank" style="color: red;" href="https://dox4242.github.io/misc/tiers.html">here</a> to see your unlocked tiers.</p>
+			<br/>
+			<p>1/ Start with Goblin until Spiky Rough Egg is excavated, then immediately go to the next step.</p>
+			<p>2/ (Switch to Goblin for Stage 1, 2, 4 and Fairy for Stage 3 completion whenever necessary)</p>
+			<p>3/ Run Demon (all spells on autocast) until Call to Arm (CtA), Blood Frenzy (BF), Combo Strike (CS) and Spiritual Surge (SS) hit tier 2 at 1 day, 10 hours and 21 minutes of CtA, SS, BF & CS activity time.</p>
+			<p>4/ If Hellfire Blast (HfB) has at least 1 day and 32 minutes of HfB activity time it will hit tier 2 immediately, if not then keep running Demon until it hits tier 2.</p>
+			<p>5/ Run Elf (all spells on autocast) for 19 hours and 4 minutes, where CtA and SS hit tier 3 at 2 days, 5 hours and 24 minutes of CtA & SS activity time.</p>
+			<p>6/ Run Elf for an extra 1 hour and 9 minutes, where Holy Light (HL), Moon Blessing (MB) and Diamond Pickaxe (DP) hit tier 2 at 20 hours and 13 minutes of HL, MB & DP activity time.</p>
+			<p>7/ Run Demon again for 5 hours and 42 minutes, where BF and CS hit tier 3 at 1 day, 16 hours and 3 minutes of BF & CS activity time.</p>
+			<p>8/ Run Demon until HfB hits tier 3.</p>
+			<br/>
+			<p><b>Note</b>: Click <a target="_blank" style="color: red;" href="https://cdn.discordapp.com/attachments/532080862229954563/828647252837793792/R46_Tiers.png">here</a> for a simplified version of the plan.</p>
+			<br/>
+                </div>
+            </div>
+		<p><b>NOTE</b>: The order in which you acquire the tiers doesn't matter, as long as your Demon spells have at least 1d 16h 3m and Elf spells at least 20h 13m of activity time this R you will be able to get the needed tiers, switching back and forth between the two factions a few times. Before going into R47 make sure you have actually unlocked the mentioned tiers by looking at the "highest tier unlocked" in the respective spell's tooltip.</p>
         </div>
     </div>
     <?php include "../scripts/footer.html"; ?>
