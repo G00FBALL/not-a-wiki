f7c8eae798a20d99945d053ab0c5a97700ef200f
W0lfsBlood <75559520+W0lfsBlood@users.noreply.github.com>
2022-03-31 07:10:02 +0200

    v4.2.3 (typo)

--- Changelog/index.php
+++ Changelog/index.php
@@ -5,7 +5,7 @@
 	<p>The patch notes, or changelogs, can also be accessed by clicking the Version Number on bottom right corner of the ingame options window.</p>
 	<div class="shlisting">
 		<div class="shelementwhole">
-			<p onclick="shohid($(this));"><b> <a href="#" onclick="return false;">v4.2.3, Mar 30, 2022, Valentine Event</a></b></p>
+			<p onclick="shohid($(this));"><b> <a href="#" onclick="return false;">v4.2.3, Mar 30, 2022, Easter Event</a></b></p>
 			<div class="autohide">
 				<p><b>Global Reminder</b>: The Easter Event will be starting on Apr 14th 2022, 12.01 PM UTC and will end on Apr 21st 2021, 6.59 PM UTC.</p>
 				<p>ADDED A NEW EASTER FEAT, unlocking a new cosmetic option.</p>
